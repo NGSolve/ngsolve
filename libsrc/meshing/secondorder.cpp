f99009b57bf34d06f8a8b79a406b0fd8f9b65b5b
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2009-01-18 00:51:27 +0000

    fix for SUN - compiler

--- libsrc/meshing/secondorder.cpp
+++ libsrc/meshing/secondorder.cpp
@@ -73,7 +73,7 @@ namespace netgen
 	    { 3, 2, 5 },
 	    { 0, 3, 6 },
 	    { 1, 2, 7 } };
-	int (*betw)[3] = 0;
+	int (*betw)[3] = NULL;
       
 	switch (el.GetType())
 	  {
@@ -169,7 +169,7 @@ namespace netgen
 	    { 3, 4, 10 },
 	    { 4, 5, 11 },
 	  };
-	int (*betw)[3] = 0;
+	int (*betw)[3] = NULL;
 
 	switch (el.GetType())
 	  {
