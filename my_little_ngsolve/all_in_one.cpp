55ef89ffe34ccc610d7da57f6895f59cff4e27c0
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2009-05-08 16:04:04 +0000

    all in one example

---
+++ my_little_ngsolve/all_in_one.cpp
@@ -0,0 +1,17 @@
+/*********************************************************************/
+/* File:   all_in_one.cpp                                            */
+/* Author: Joachim Schoeberl                                         */
+/* Date:   4. May. 2009                                              */
+/*********************************************************************/
+
+
+/*
+
+We write one numproc which creates and maintains all objects to solve
+the Poisson equation.
+
+*/
+
+
+
+
