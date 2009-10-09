84fda5f10b1c12e76b14e762cd0209f1f643e463
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2009-10-09 13:48:43 +0000

    remove typevariable (not available for all environments)

--- ng/menustat.tcl
+++ ng/menustat.tcl
@@ -48,7 +48,8 @@ menu .ngmenu.file
 #		{"Naomi Geometry"      {.nao} }
 #		{"New Geometry file"   {.ngg} } 
 
-	set file [tk_getOpenFile -filetypes $types -initialdir $dirname -typevariable loadgeomtypevar]
+#	set file [tk_getOpenFile -filetypes $types -initialdir $dirname -typevariable loadgeomtypevar]
+	set file [tk_getOpenFile -filetypes $types -initialdir $dirname]
 	if {$file != ""} {
 	    AddRecentFile $file
 	    Ng_LoadGeometry $file 
