967dfdc0876ae1d2871c27ff356fac5c1eb15ed7
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2009-10-14 19:51:43 +0000

    checkelement

--- fem/l2hofe.cpp
+++ fem/l2hofe.cpp
@@ -21,23 +21,6 @@ namespace ngfem
   }
 
   template <int D>
-  void L2HighOrderFiniteElement<D>::
-  SetOrder (int o)
-  {
-    for (int j = 0; j < DIM; j++)
-      order_inner[j] = o;
-    ComputeNDof();
-  }
-
-  template <int D>
-  void L2HighOrderFiniteElement<D>::
-  SetOrder (INT<DIM> oi)
-  {
-    order_inner = oi;   
-    ComputeNDof();
-  }
-
-  template <int D>
   void L2HighOrderFiniteElement<D>:: 
   GetInternalDofs (Array<int> & idofs) const
   {
@@ -45,8 +28,6 @@ namespace ngfem
     for (int i = 0; i < ScalarFiniteElement<D>::GetNDof(); i++)
       idofs.Append (i);
   }
-
-
 
 
 
@@ -102,7 +83,7 @@ namespace ngfem
 	ndof = (order_inner[0]+1) * (order_inner[1]+1); 
 	break;
       case ET_TET: 
-	ndof = ((order+1) * (order+2) * (order+3)) / 6; // P_k
+	ndof = ((order_inner[0]+1) * (order_inner[0]+2) * (order_inner[0]+3)) / 6; // P_k
         break;
       case ET_PRISM:
 	ndof = ((order_inner[0]+1) * (order_inner[0]+2) * (order_inner[2]+1)) / 2; // P_k x Q_k
@@ -168,16 +149,13 @@ namespace ngfem
     if(vnums[fav[1]] > vnums[fav[2]]) swap(fav[1],fav[2]);
     if(vnums[fav[0]] > vnums[fav[1]]) swap(fav[0],fav[1]); 	
     x = lami[fav[0]]; y=lami[fav[1]];
-    
      
-    int ii = 0;
     int n = order_inner[0];
-
     ArrayMem<Tx, 20> polx(n+1), poly(n+1);
 
     ScaledLegendrePolynomial (n, 2*x+y-1, 1-y, polx);
     
-    for (int i = 0; i <= order_inner[0]; i++)
+    for (int i = 0, ii = 0; i <= order_inner[0]; i++)
       {
         JacobiPolynomial (n-i, 2*y-1, 2*i+1, 0, poly);
         for (int j = 0; j <= order_inner[0]-i; j++)
@@ -247,24 +225,10 @@ namespace ngfem
     int n=order;
     ArrayMem<Tx, 20> polx(n+1), poly(n+1), polz(n+1);
     
-    //     LegendrePolynomial (n, 2*x-1, polx);
-    //     ScaledLegendrePolynomial (n, 2*y+x-1, 1-x, poly);
-    //     ScaledLegendrePolynomial (n, 2*z+x+y-1, 1-x-y, polz);
-    
-    //     int ii = 0;
-    //     for (int i = 0; i <= order; i++)
-    //       for (int j = 0; j <= order-i; j++)
-    //         for (int k = 0; k <= order-i-j; k++, ii++)
-    //           for (int l=0; l<3; l++)
-    //	      shape(ii++) = polx[i] * poly[j] * polz[k];
-
-    
-    
     // Polynomials orthogonal w.r.t L2 inner product
     ScaledLegendrePolynomial ( n, 2*x+y+z-1, 1-y-z, polx);
   
-    int ii = 0;
-    for (int i = 0; i <= order; i++)
+    for (int i = 0, ii = 0; i <= order; i++)
       {
 	ScaledJacobiPolynomial (n, 2*y+z-1, 1-z, 2*i+1, 0, poly);
 	for (int j = 0; j <= order-i; j++)
@@ -313,6 +277,55 @@ namespace ngfem
   }
 
 
+  /* *********************** Pyramid  **********************/
+
+  L2HighOrderFE<ET_PYRAMID> :: L2HighOrderFE (int aorder)
+  {
+    order_inner = INT<3>(aorder, aorder, aorder);
+    ComputeNDof();
+  }
+
+
+  template<typename Tx, typename TFA>  
+  void L2HighOrderFE<ET_PYRAMID> :: T_CalcShape (Tx hx[3], TFA & shape) const
+  {
+    Tx x = hx[0];
+    Tx y = hx[1];
+    Tx z = hx[2];
+
+    if (z == 1) z -= 1e-8;
+    Tx xt = 2 * (x / (1-z)) - 1;
+    Tx yt = 2 * (y / (1-z)) - 1;
+
+    VectorMem<10, Tx> polsx(order+1);
+    VectorMem<10, Tx> polsy(order+1);
+    
+    ArrayMem<Tx, 20> memz(sqr(order+1));
+    FlatMatrix<Tx> polsz(order+1, &memz[0]);
+
+    Tx fac = 1.0;
+    for (int i = 0; i <= order; i++)
+      {
+	JacobiPolynomial (order, 2*z-1, 2*i+2, 0, polsz.Row(i));
+	polsz.Row(i) *= fac;
+	fac *= (1-z);
+      }
+
+    LegendrePolynomial (order, xt, polsx);
+    LegendrePolynomial (order, yt, polsy);
+    
+    int ii = 0;
+    for (int iz = 0; iz <= order; iz++)
+      for (int ix = 0; ix <= order-iz; ix++)
+	for (int iy = 0; iy <= order-iz; iy++, ii++)
+	  shape[ii] = polsx(ix) * polsy(iy) * polsz(max(ix,iy), iz);
+  }
+
+
+
+
+
+
   /* *********************** Hex  **********************/
 
   L2HighOrderFE<ET_HEX> :: L2HighOrderFE (int aorder)
@@ -339,8 +352,7 @@ namespace ngfem
     LegendrePolynomial (q, 2*y-1, poly);
     LegendrePolynomial (r, 2*z-1, polz);
   
-    int ii = 0;
-    for (int i = 0; i <= p; i++)
+    for (int i = 0, ii = 0; i <= p; i++)
       for (int j = 0; j <= q; j++)
         for (int k = 0; k <= r; k++)
           shape[ii++] = polx[i] * poly[j] * polz[k];
