f4bfd278c3fe00fa9f36c9be35907804c5fa1ec5
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2009-11-01 10:51:22 +0000

    remove duplicated header declaration

--- libsrc/include/nginterface_v2.hpp
+++ libsrc/include/nginterface_v2.hpp
@@ -136,21 +136,6 @@ namespace netgen
   
   template <int DIM> 
   DLL_HEADER int Ng_GetElementIndex (int nr);
-
-  
-  /*  
-  /// Curved Elements:
-  /// xi..... DIM_EL local coordinates
-  /// sxi ... step xi
-  /// x ..... DIM_SPACE global coordinates
-  /// dxdxi...DIM_SPACE x DIM_EL Jacobian matrix (row major storage)
-  template <int DIM_EL, int DIM_SPACE> 
-  DLL_HEADER void Ng_MultiElementTransformation (int elnr, int npts,
-                                                 const double * xi, int sxi,
-                                                 double * x, int sx,
-                                                 double * dxdxi, int sdxdxi);
-  */
-  
 }
 #endif
 
