c65eed0e75a734e25f0c72f29255ace523f711fc
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2011-01-10 20:28:17 +0000

    geometry tcl-files

---
+++ ng/acisgeom.tcl
@@ -0,0 +1,12 @@
+
+if { [Ng_ACISCommand isACISavailable] == "yes" } {
+    .ngmenu.geometry add command -label "ACIS Topology Explorer..." \
+        -command { acisdialog; }
+
+    .ngmenu.geometry add command -label "ACIS combine all" \
+        -command { Ng_ACISCommand combineall }
+    .ngmenu.geometry add command -label "ACIS Create CT" \
+        -command { Ng_ACISCommand createct }
+}
+
+
