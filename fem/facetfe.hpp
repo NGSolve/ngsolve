6e3573c39b5033b7668a511f007efd0d7fc0cef5
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2012-04-14 11:23:04 +0000

    redesign of facetelements:  facet_fel.Facet(k).CalcShape (..)

--- fem/facetfe.hpp
+++ fem/facetfe.hpp
@@ -13,11 +13,32 @@ namespace ngfem
   /*
    * Facet Finite Elements
    */ 
-  
-
 
   template <int D>
-  class NGS_DLL_HEADER FacetVolumeFiniteElement : virtual public ScalarFiniteElement<D>
+  class NGS_DLL_HEADER FacetVolumeFiniteElement;
+  
+  
+  template <int D>
+  class FacetFEFacet : virtual public ScalarFiniteElement<D>
+  {
+    int fnr;
+    const FacetVolumeFiniteElement<D> & fe;
+  public:
+    FacetFEFacet (int afnr,
+		  const FacetVolumeFiniteElement<D> & afe) 
+      : fnr(afnr), fe(afe) { ; }
+
+    virtual void CalcShape (const IntegrationPoint & ip, 
+			    FlatVector<> shape) const
+    {
+      fe.CalcFacetShapeVolIP(fnr, ip, shape);
+    }
+  };
+
+  
+
+  template <int D>
+  class NGS_DLL_HEADER FacetVolumeFiniteElement : public FiniteElement
   {
   protected:
     int vnums[8];
@@ -25,123 +46,80 @@ namespace ngfem
     int first_facet_dof[7];
 
     // bool highest_order_dc;
-    using ScalarFiniteElement<D>::eltype;
-    using ScalarFiniteElement<D>::order;
 
-    ScalarFiniteElement<D-1> * facets[6];
-
-    mutable int facetnr;
+    using FiniteElement::eltype;
+    using FiniteElement::order;
 
   public:
-    FacetVolumeFiniteElement (ELEMENT_TYPE aeltype);
-
-    void SetVertexNumbers (FlatArray<int> & avnums);
-    // void SetHighestOrderDC(bool set){highest_order_dc=set;}
-
-    void SetOrder (int ao);
-    void SetOrder (FlatArray<int> & ao);
-    int GetFacetOrder (int j) const { return facet_order[j]; }
-    int GetVertexNumber (int j) const { return vnums[j]; }
-   
-    void CalcFacetShape(int fnr, const IntegrationPoint & ip, FlatVector<> shape) const;
-    void EvaluateFacet (int fnr, const IntegrationRule & ir, FlatVector<> coefs, FlatVector<> values) const;
-    void EvaluateFacetTrans (int fnr, const IntegrationRule & ir, FlatVector<> values, FlatVector<> coefs) const;
-    
-    
-    void SelectFacet (int afn) const { facetnr = afn; }
+    FacetVolumeFiniteElement (ELEMENT_TYPE aeltype)
+      : FiniteElement (D, aeltype,-1,-1)
+    {
+      // highest_order_dc = false;
+      for (int i=0; i<8; i++) 
+	vnums[i] = i;
+      for (int i = 0; i < 6; i++) 
+	facet_order[i] = -1;
+      for (int i=0; i < 7; i++) 
+	first_facet_dof[i] = 0;
+    }
 
 
-    void GetFacetDofNrs(int afnr, Array<int>& fdnums) const; 
-    int GetFacetNDof(int afnr) const { return first_facet_dof[afnr+1] - first_facet_dof[afnr]; };
-    int GetFirstFacetDof(int afnr) const { return first_facet_dof[afnr]; } 
-  
-    const ScalarFiniteElement<D-1> & GetFacetFE(int fnr) const { return *facets[fnr]; }
-
-    virtual void ComputeNDof () = 0;
-
-    // virtual void GetInternalDofs (Array<int> & idofs) const; 
-
-  };
-
-
-
-
-  /*
-  
-
-  template <int DIM>
-  class NGS_DLL_HEADER EdgeVolumeFiniteElement : virtual public ScalarFiniteElement<DIM>
-  {
-  protected:
-    int vnums[8];
-    mutable int edgenr;
-
-    using ScalarFiniteElement<DIM> :: order;
-    using ScalarFiniteElement<DIM> :: eltype;
-    using ScalarFiniteElement<DIM> :: ndof;
-  public:
-    EdgeVolumeFiniteElement (ELEMENT_TYPE aeltype, int aorder);
     void SetVertexNumbers (FlatArray<int> & avnums)
     {
       for (int i = 0; i < avnums.Size(); i++)
 	vnums[i] = avnums[i];
     }
-    void CalcEdgeShape(int enr, const IntegrationPoint & ip, FlatVector<> shape) const;
 
-    void SelectEdge (int nr) const { edgenr = nr; }
-  };
+    // void SetHighestOrderDC(bool set){highest_order_dc=set;}
 
-  class NGS_DLL_HEADER EdgeVolumeTet : public EdgeVolumeFiniteElement<3>,
-				       public T_ScalarFiniteElement2<EdgeVolumeTet, ET_TET>
-  {
-  public:
-    EdgeVolumeTet (int aorder)
-      : EdgeVolumeFiniteElement<3>(ET_TET, aorder) { ; }
-
-
-    template<typename Tx, typename TFA>  
-    void T_CalcShape (Tx hx[3], TFA & shape) const
+    void SetOrder (int ao)  
     {
-      Tx lam[4] = { hx[0], hx[1], hx[2], 1-hx[0]-hx[1]-hx[2] };
-
-      INT<2> e = ET_trait<ET_TET> :: GetEdgeSort (edgenr, vnums);
-
-      SetZero (shape, 0, ndof);
-
-      int base = (order+1) * edgenr;
-      LegendrePolynomial (order, 
-			  lam[e[1]] - lam[e[0]],
-			  shape.Addr(base));
+      order = ao;
+      for (int i = 0; i < 6; i++)
+	facet_order[i] = ao;
     }
-  };
-
-
-  class NGS_DLL_HEADER EdgeVolumeTrig : public EdgeVolumeFiniteElement<2>,
-					public T_ScalarFiniteElement2<EdgeVolumeTrig, ET_TRIG>
-  {
-  public:
-    EdgeVolumeTrig (int aorder)
-      : EdgeVolumeFiniteElement<2>(ET_TRIG, aorder) { ; }
-
-
-    template<typename Tx, typename TFA>  
-    void T_CalcShape (Tx hx[2], TFA & shape) const
+    
+    void SetOrder (FlatArray<int> & ao)
     {
-      Tx lam[4] = { hx[0], hx[1], 1-hx[0]-hx[1] };
-
-      INT<2> e = ET_trait<ET_TRIG> :: GetEdgeSort (edgenr, vnums);
+      for (int i=0; i<ao.Size(); i++)
+	facet_order[i] = ao[i];
       
-      SetZero (shape, 0, ndof);
-
-      int base = (order+1) * edgenr;
-      LegendrePolynomial (order, 
-			  lam[e[1]] - lam[e[0]],
-			  shape.Addr(base));
+      order = facet_order[0];        // integration order
+      for (int i = 1; i < ao.Size(); i++)
+	order = max(order, ao[i]);
     }
+    
+    
+    FacetFEFacet<D> Facet (int fnr) const { return FacetFEFacet<D> (fnr, *this); }
+    virtual void CalcFacetShapeVolIP (int fnr, const IntegrationPoint & ip, FlatVector<> shape) const = 0;
+
+
+    /*
+      // please use Facet(k).CalcShape (ip, shape)   instead
+      // ip .. vol point
+
+    void CalcFacetShape(int fnr, const IntegrationPoint & ip, FlatVector<> shape) const;
+    void EvaluateFacet (int fnr, const IntegrationRule & ir, FlatVector<> coefs, FlatVector<> values) const;
+    void EvaluateFacetTrans (int fnr, const IntegrationRule & ir, FlatVector<> values, FlatVector<> coefs) const;
+    void SelectFacet (int afn) const { facetnr = afn; }
+    */
+
+
+    IntRange GetFacetDofs(int fnr) const
+    {
+      return IntRange (first_facet_dof[fnr], first_facet_dof[fnr+1]);
+    }
+
+    void GetFacetDofNrs(int fnr, Array<int>& fdnums) const
+    {
+      fdnums = GetFacetDofs(fnr);
+    }
+
+    int GetFacetNDof(int afnr) const { return first_facet_dof[afnr+1] - first_facet_dof[afnr]; };
+    int GetFirstFacetDof(int afnr) const { return first_facet_dof[afnr]; } 
+  
+    virtual void ComputeNDof () = 0;
   };
-
-  */
-
 
 }
 
--- fem/facethofe.hpp
+++ fem/facethofe.hpp
@@ -22,13 +22,46 @@ namespace ngfem
   template <ELEMENT_TYPE ET> 
   class FacetFiniteElement_Family :
     public FacetVolumeFiniteElement<ET_trait<ET>::DIM>, 
-    public T_ScalarFiniteElement2<FacetFE<ET>, ET>,
     public ET_trait<ET> 
   {
+  protected:
+    enum { DIM = ET_trait<ET>::DIM };
+
+    // using FacetVolumeFiniteElement<ET_trait<ET>::DIM>::ndof;
+    using FiniteElement::ndof;
+    using FacetVolumeFiniteElement<ET_trait<ET>::DIM>::first_facet_dof;
+    using FacetVolumeFiniteElement<ET_trait<ET>::DIM>::facet_order;
+
   public:
     FacetFiniteElement_Family ()
-      : FacetVolumeFiniteElement<ET_trait<ET>::DIM> (ET_TET) 
+      : FacetVolumeFiniteElement<DIM> (ET_TET) 
     { ; }
+
+    
+    virtual void ComputeNDof() 
+    {
+      ndof = 0;
+      for (int i = 0; i < ET_trait<ET>::N_FACET; i++)
+	{
+	  first_facet_dof[i] = ndof;
+	  int fo = facet_order[i];
+	  switch (ElementTopology::GetFacetType (ET, i))
+	    {
+	    case ET_SEGM: ndof += fo+1; break;
+	    case ET_TRIG: ndof += ( (fo+1) * (fo+2) ) / 2; break;
+	    case ET_QUAD: ndof += sqr (fo+1); break;
+	    }
+	}
+      first_facet_dof[ET_trait<ET>::N_FACET] = ndof;
+    }
+    
+
+    virtual void CalcFacetShapeVolIP(int fnr, const IntegrationPoint & ip, FlatVector<> shape) const
+    {
+      double pt[DIM];
+      for (int i = 0; i < DIM; i++) pt[i] = ip(i);
+      static_cast<const FacetFE<ET>*>(this)->T_CalcShapeFNr (fnr, pt, shape); 
+    }
   };
 
 
@@ -36,23 +69,17 @@ namespace ngfem
   template <>
   class FacetFE<ET_TRIG> : public FacetFiniteElement_Family<ET_TRIG>
   {
-  protected:
-    L2HighOrderFE<ET_SEGM> facets2[3];
-    using FiniteElement::eltype;    
   public:
-    FacetFE(); 
-    virtual void ComputeNDof();
-
     template<typename Tx, typename TFA>  
-    void T_CalcShape (Tx x[2], TFA & shape) const
+    void T_CalcShapeFNr (int fnr, Tx x[2], TFA & shape) const
     {
       Tx lam[3] = { x[0], x[1], 1-x[0]-x[1] };
       
-      INT<2> e = GetEdgeSort (facetnr, vnums);
+      INT<2> e = GetEdgeSort (fnr, vnums);
       SetZero (shape, 0, ndof);
 
-      int p = facet_order[facetnr];
-      int ii = first_facet_dof[facetnr];
+      int p = facet_order[fnr];
+      int ii = first_facet_dof[fnr];
       LegendrePolynomial::Eval (p, lam[e[1]]-lam[e[0]], shape.Addr(ii));
     }
   };
@@ -63,52 +90,39 @@ namespace ngfem
   template <>
   class FacetFE<ET_QUAD> : public FacetFiniteElement_Family<ET_QUAD>
   {
-  protected:
-    L2HighOrderFE<ET_SEGM> facets2[4];
-    using FiniteElement::eltype;    
   public:
-    FacetFE();
-    virtual void ComputeNDof();
-
     template<typename Tx, typename TFA>  
-    void T_CalcShape (Tx hx[2], TFA & shape) const
+    void T_CalcShapeFNr (int fnr, Tx hx[2], TFA & shape) const
     {
       Tx x = hx[0], y = hx[1];
       Tx sigma[4] = {(1-x)+(1-y),x+(1-y),x+y,(1-x)+y};  
       
-      INT<2> e = GetEdgeSort (facetnr, vnums);
+      INT<2> e = GetEdgeSort (fnr, vnums);
       SetZero (shape, 0, ndof);
 
-      int p = facet_order[facetnr];
-      int ii = first_facet_dof[facetnr];
+      int p = facet_order[fnr];
+      int ii = first_facet_dof[fnr];
       LegendrePolynomial::Eval (p, sigma[e[1]]-sigma[e[0]], shape.Addr(ii));
     }
   };
-
 
 
   // --------------------------------------------------------
   template <>
   class FacetFE<ET_TET> : public FacetFiniteElement_Family<ET_TET>
   {
-  protected:
-    L2HighOrderFE<ET_TRIG> facets2[4];
-    using FiniteElement::eltype;
   public:
-    FacetFE(); 
-    virtual void ComputeNDof();
-
     template<typename Tx, typename TFA>  
-    void T_CalcShape (Tx hx[3], TFA & shape) const
+    void T_CalcShapeFNr (int fnr, Tx hx[3], TFA & shape) const
     {
       Tx lam[4] = { hx[0], hx[1], hx[2], 1-hx[0]-hx[1]-hx[2] };
 
-      INT<4> f = GetFaceSort (facetnr, vnums);
+      INT<4> f = GetFaceSort (fnr, vnums);
 
       SetZero (shape, 0, ndof);
       
-      int p = facet_order[facetnr];
-      int ii = first_facet_dof[facetnr];
+      int p = facet_order[fnr];
+      int ii = first_facet_dof[fnr];
       DubinerBasis::Eval (p, lam[f[0]], lam[f[1]], shape.Addr(ii));
     }
   };
@@ -119,17 +133,31 @@ namespace ngfem
   template <>
   class FacetFE<ET_HEX> : public FacetFiniteElement_Family<ET_HEX>
   {
-  protected:
-    L2HighOrderFE<ET_QUAD> facetsq[6]; 
-    using FiniteElement::eltype;
   public:
-    FacetFE();
-    virtual void ComputeNDof();
-
     template<typename Tx, typename TFA>  
-    void T_CalcShape (Tx x[3], TFA & shape) const
+    void T_CalcShapeFNr (int fnr, Tx hx[3], TFA & shape) const
     {
-      ;
+      Tx x = hx[0], y = hx[1], z = hx[2];
+      Tx sigma[8]={(1-x)+(1-y)+(1-z),x+(1-y)+(1-z),x+y+(1-z),(1-x)+y+(1-z),
+		   (1-x)+(1-y)+z,x+(1-y)+z,x+y+z,(1-x)+y+z}; 
+      
+      int p = facet_order[fnr];
+      int ii = first_facet_dof[fnr];
+
+      INT<4> f = GetFaceSort (fnr, vnums);	  
+	    
+      Tx xi  = sigma[f[0]] - sigma[f[1]]; 
+      Tx eta = sigma[f[0]] - sigma[f[3]];
+
+      ArrayMem<Tx,20> polx(p+1), poly(p+1);
+      
+      LegendrePolynomial::Eval(p, xi, polx);
+      LegendrePolynomial::Eval(p, eta, poly);
+
+      SetZero (shape, 0, ndof);
+      for (int i = 0; i <= p; i++)
+	for (int j = 0; j <= p; j++)
+	  shape[ii++] = polx[i] * poly[j];
     }
 
   };
@@ -138,30 +166,23 @@ namespace ngfem
   template <>
   class FacetFE<ET_PRISM> : public FacetFiniteElement_Family<ET_PRISM>
   {
-  protected:
-    L2HighOrderFE<ET_TRIG> facetst[2];
-    L2HighOrderFE<ET_QUAD> facetsq[3];
-    using FiniteElement::eltype;
   public:
-    FacetFE();
-    virtual void ComputeNDof();
-
     template<typename Tx, typename TFA>  
-    void T_CalcShape (Tx hx[3], TFA & shape) const
+    void T_CalcShapeFNr (int fnr, Tx hx[3], TFA & shape) const
     {
       Tx x = hx[0], y = hx[1], z = hx[2];
       Tx lam[6] = { x, y, 1-x-y, x, y, 1-x-y };
       Tx muz[6]  = { 1-z, 1-z, 1-z, z, z, z };
       
 
-      INT<4> f = GetFaceSort (facetnr, vnums);
+      INT<4> f = GetFaceSort (fnr, vnums);
 
       SetZero (shape, 0, ndof);
       
-      int p = facet_order[facetnr];
-      int ii = first_facet_dof[facetnr];
+      int p = facet_order[fnr];
+      int ii = first_facet_dof[fnr];
 
-      if (facetnr < 2)
+      if (fnr < 2)
 	DubinerBasis::Eval (p, lam[f[0]], lam[f[1]], shape.Addr(ii));
       else
 	{
@@ -186,23 +207,9 @@ namespace ngfem
   template <>
   class FacetFE<ET_PYRAMID> : public FacetFiniteElement_Family<ET_PYRAMID>
   {
-  protected:
-    int tnr, qnr; // active facets
-    L2HighOrderFE<ET_TRIG> trig;
-    L2HighOrderFE<ET_QUAD> quad;
-    using FiniteElement::eltype;    
   public:
-    FacetFE() : FacetFiniteElement_Family<ET_PYRAMID> () {  qnr=tnr=-1; };
-    
-    virtual void CalcFacetShape (int afnr, const IntegrationPoint & ip, FlatVector<> shape) const; 
-    virtual void SetFacet(int afnr) const;
-  
-    virtual void ComputeNDof();
-    //     virtual const FiniteElement & GetFacetFE(int fnr, LocalHeap& lh) const;
-
-
     template<typename Tx, typename TFA>  
-    void T_CalcShape (Tx hx[3], TFA & shape) const
+    void T_CalcShapeFNr (int fnr, Tx hx[3], TFA & shape) const
     {
       Tx x = hx[0], y = hx[1], z = hx[2];
       
@@ -220,14 +227,14 @@ namespace ngfem
       lam[4] = z;
 
 
-      INT<4> f = GetFaceSort (facetnr, vnums);
+      INT<4> f = GetFaceSort (fnr, vnums);
 
       SetZero (shape, 0, ndof);
       
-      int p = facet_order[facetnr];
-      int ii = first_facet_dof[facetnr];
+      int p = facet_order[fnr];
+      int ii = first_facet_dof[fnr];
 
-      if (facetnr < 4)
+      if (fnr < 4)
 	DubinerBasis::Eval (p, lam[f[0]], lam[f[1]], shape.Addr(ii));
       else
 	{
