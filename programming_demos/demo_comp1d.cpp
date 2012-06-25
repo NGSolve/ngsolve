d69403dc573ce5ff2a4b7d9bb859db4dd39b46b7
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2012-06-25 16:21:22 +0000

    0-dim elements

--- programming_demos/demo_comp1d.cpp
+++ programming_demos/demo_comp1d.cpp
@@ -17,14 +17,20 @@ int main (int argc, char **argv)
   ngs_comm = MPI_COMM_WORLD;
 #endif 
 
-  Ng_LoadGeometry ("cube.geo");
-  Ng_LoadMesh ("cube.vol");
+  // Ng_LoadGeometry ("cube.geo");
+  // Ng_LoadMesh ("cube.vol");
   
   LocalHeap lh(10000000, "main heap");
   MeshAccess ma;
 
-  // H1HighOrderFESpace fes(ma, Flags().SetFlag("order",2).SetFlag("print") );
-  NodalFESpace fes(ma, Flags().SetFlag("order",1).SetFlag("print") );
+  Array<double> dirichlet(1);
+  dirichlet[0] = 1;
+  H1HighOrderFESpace fes(ma, 
+			 Flags() .SetFlag("order",2)
+			 .SetFlag("print")
+			 .SetFlag("dirichlet", dirichlet));
+
+  // NodalFESpace fes(ma, Flags().SetFlag("order",1).SetFlag("print") );
   
   T_GridFunction<double> gfu (fes, "gfu", Flags());
 
@@ -46,6 +52,7 @@ int main (int argc, char **argv)
 
 
   fes.Update(lh);
+  fes.FinalizeUpdate(lh);
   gfu.Update();
   bfa.Assemble(lh);
   lff.Assemble(lh);
@@ -55,7 +62,8 @@ int main (int argc, char **argv)
   BaseVector & vecf = lff.GetVector();
   BaseVector & vecu = gfu.GetVector();
   
-  BaseMatrix * jacobi = dynamic_cast<const BaseSparseMatrix&> (mata).CreateJacobiPrecond();
+  cout << "freedofs = " << *fes.GetFreeDofs() << endl;
+  BaseMatrix * jacobi = dynamic_cast<const BaseSparseMatrix&> (mata).CreateJacobiPrecond(fes.GetFreeDofs());
 
   CGSolver<double> inva (mata, *jacobi);
   inva.SetPrintRates();
