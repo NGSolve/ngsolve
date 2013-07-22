f08151cb903c2fd7e0985fa4bff94b097756a2bd
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2013-07-22 11:34:46 +0000

    lambda implementation of scalar fe

--- fem/tscalarfe_impl.hpp
+++ fem/tscalarfe_impl.hpp
@@ -5,271 +5,18 @@
 namespace ngfem
 {
 
-
-  /**
-     Extracts and assigns gradient from autodiff.
-   */
   template <int DIM>
-  class DShapeElement
+  class AD2Vec : public Vec<DIM>
   {
-    FlatVec<DIM> data;
   public:
-    /// A reference to the destination
-    DShapeElement (FlatVec<DIM> adata) : data(adata) { ; }
-
-    /// Assign gradient values
-    void operator= (AutoDiff<DIM> ad) 
-    { 
-      for (int i = 0; i < DIM; i++) 
-        data(i) = ad.DValue(i); 
-    }
-  };
-
-
-  /**
-     Assign gradients from generic shape functions
-   */
-  template <int DIM>
-  class DShapeAssign
-  {
-    FlatMatrixFixWidth<DIM> dshape;
-  public:
-    /// Initialize with gradient matrix
-    DShapeAssign (FlatMatrixFixWidth<DIM> mat) : dshape(mat) { ; }
-
-    /// i-th component of gradient matrix
-    DShapeElement<DIM> operator[] (int i) const
-    { return DShapeElement<DIM> (dshape.Row(i)); }
-
-    /// sub-array
-    const DShapeAssign Addr (int i) const
-    { return DShapeAssign (dshape.Rows(i, dshape.Height())); }
-    const DShapeAssign operator+ (int i) const
-    { return DShapeAssign (dshape.Rows(i, dshape.Height())); }
-  };
-
-
-
-
- 
-
-  /**
-     Evaluate shape 
-   */
-  template <typename TSCAL>
-  class EvaluateShapeElement
-  {
-    double coef;
-    TSCAL * sum;
-  public:
-    /// initialize with coefficient and sum-reference
-    INLINE EvaluateShapeElement (double acoef, TSCAL * asum)
-      : coef(acoef), sum(asum) { ; }
-
-    /// add up
-    INLINE void operator= (TSCAL val) 
+    AD2Vec (double d) { (*this) = d; }
+    AD2Vec (AutoDiff<DIM> ad)
     {
-      *sum += coef * val;
+      for (int j = 0; j < DIM; j++)
+        (*this)(j) = ad.DValue(j);
     }
   };
-
-  template <typename TSCAL>
-  class EvaluateShapeSlave
-  {
-    const double * coefs;
-    TSCAL * sum;
-  public:
-    /// initialize with coefficient vector and value for the sum
-    INLINE EvaluateShapeSlave (const double * acoefs, TSCAL * asum)
-      : coefs(acoefs), sum(asum) { ; }
-
-    /// does the computation for i-th element
-    INLINE EvaluateShapeElement<TSCAL> operator[] (int i) const
-    { return EvaluateShapeElement<TSCAL> (coefs[i], sum); }
-
-    /// get sub-vector
-    const EvaluateShapeSlave<TSCAL> Addr (int i) const
-    { return EvaluateShapeSlave<TSCAL> (coefs+i, sum); } 
-    const EvaluateShapeSlave<TSCAL> operator+ (int i) const
-    { return EvaluateShapeSlave<TSCAL> (coefs+i, sum); } 
-  };
-
-
-  /**
-     Computes function value from generic shape functions
-   */
-  template <typename TSCAL = double>
-  class EvaluateShape
-  {
-    const double * coefs;
-    TSCAL sum;
-  public:
-    /// initialize with coefficient vector and value for the sum
-    EvaluateShape (FlatVector<> acoefs)
-      : coefs(&acoefs(0)), sum(0.0) { ; }
-    
-    /// does the computation for i-th element
-    EvaluateShapeElement<TSCAL> operator[] (int i)
-    { return EvaluateShapeElement<TSCAL> (coefs[i], &sum); }
-
-    TSCAL Sum() const { return sum; }
-
-    /// get sub-vector
-    const EvaluateShapeSlave<TSCAL> Addr (int i) // const
-    { return EvaluateShapeSlave<TSCAL> (coefs+i, &sum); } 
-    const EvaluateShapeSlave<TSCAL> operator+ (int i) // const
-    { return EvaluateShapeSlave<TSCAL> (coefs+i, &sum); } 
-  };
-
-
-
-
-
   
-  template <typename TSCAL>
-  class EvaluateShapeTransElement
-  {
-    double & data;
-    TSCAL fac;
-  public:
-    EvaluateShapeTransElement (double & adata, TSCAL afac) 
-      : data(adata), fac(afac) { ; }
-
-    ALWAYS_INLINE void operator= (const TSCAL & ad) 
-    { data += InnerProduct (ad, fac); }
-  };
-
-  template <typename TSCAL = double>
-  class EvaluateShapeTrans
-  {
-    double * coefs;
-    TSCAL fac;
-  public:
-    EvaluateShapeTrans (FlatVector<> acoefs, TSCAL afac)
-      : coefs(&acoefs(0)), fac(afac) { ; }
-
-    EvaluateShapeTrans (double * acoefs, TSCAL afac)
-      : coefs(acoefs), fac(afac) { ; }
-
-    EvaluateShapeTransElement<TSCAL> operator[] (int i) const
-    { return EvaluateShapeTransElement<TSCAL> (coefs[i], fac); }
-
-    const EvaluateShapeTrans Addr (int i) const
-    { return EvaluateShapeTrans (coefs+i, fac); }
-    const EvaluateShapeTrans operator+ (int i) const
-    { return EvaluateShapeTrans (coefs+i, fac); }
-  };
-
-
-
-
-
-
-
-
-  
-  template <int DIM>
-  class EvaluateDShapeElement
-  {
-    double data;
-    Vec<DIM> & sum;
-  public:
-    EvaluateDShapeElement (double adata, Vec<DIM> & asum) : data(adata), sum(asum) { ; }
-    void operator= (AutoDiff<DIM> ad) 
-    { 
-      for (int i = 0; i < DIM; i++) 
-        sum(i) += ad.DValue(i) * data;
-    }
-  };
-
-  template <int DIM>
-  class EvaluateDShape
-  {
-    double * coefs;
-    Vec<DIM> & sum;
-  public:
-    EvaluateDShape (FlatVector<> acoefs, Vec<DIM> & asum)
-      : coefs(&acoefs(0)), sum(asum) { ; }
-
-    EvaluateDShape (double * acoefs, Vec<DIM> & asum)
-      : coefs(acoefs), sum(asum) { ; }
-
-    EvaluateDShapeElement<DIM> operator[] (int i) const
-    { return EvaluateDShapeElement<DIM> (coefs[i], sum); }
-
-    const EvaluateDShape Addr (int i) const
-    { return EvaluateDShape (coefs+i, sum); }
-    const EvaluateDShape operator+ (int i) const
-    { return EvaluateDShape (coefs+i, sum); }
-  };
-
-
-
-
-
-  
-  
-  template <int DIM>
-  class EvaluateDShapeTransElement
-  {
-    double & data;
-    const Vec<DIM> & fac;
-  public:
-    EvaluateDShapeTransElement (double & adata, const Vec<DIM> & afac) : data(adata), fac(afac) { ; }
-    void operator= (AutoDiff<DIM> ad) 
-    { 
-      for (int i = 0; i < DIM; i++) 
-        data += ad.DValue(i) * fac(i);
-    }
-  };
-
-  /// todo
-  template <int DIM>
-  class EvaluateDShapeTrans
-  {
-    double * coefs;
-    const Vec<DIM> & fac;
-  public:
-    EvaluateDShapeTrans (FlatVector<> acoefs, const Vec<DIM> & afac)
-      : coefs(&acoefs(0)), fac(afac) { ; }
-
-    EvaluateDShapeTrans (double * acoefs, const Vec<DIM> & afac)
-      : coefs(acoefs), fac(afac) { ; }
-
-    EvaluateDShapeTransElement<DIM> operator[] (int i) const
-    { return EvaluateDShapeTransElement<DIM> (coefs[i], fac); }
-
-    const EvaluateDShapeTrans Addr (int i) const
-    { return EvaluateDShapeTrans (coefs+i, fac); }
-    const EvaluateDShapeTrans operator+ (int i) const
-    { return EvaluateDShapeTrans (coefs+i, fac); }
-  };
-
-
-
-
-  template <typename TFA>
-  inline void SetZero (TFA & shape, int first, int next)
-  {
-    for (int i = first; i < next; i++)
-      shape[i] = 0.0;
-  }
-  
-  template <typename TSCAL>
-  inline void SetZero (EvaluateShape<TSCAL> & shape, int first, int next)  { ; }
-  template <typename TSCAL>
-  inline void SetZero (EvaluateShapeSlave<TSCAL> & shape, int first, int next)  { ; }
-  
-  template<typename TSCAL>
-  inline void SetZero (EvaluateShapeTrans<TSCAL> & shape, int first, int next)
-  {
-    ;
-  }
-
-
-
-
-
 
 
   template <class FEL, ELEMENT_TYPE ET, int NDOF, int ORDER>
@@ -283,6 +30,7 @@ namespace ngfem
   {
     ;
   }
+
 
   template <class FEL, ELEMENT_TYPE ET, class BASE>
   void T_ScalarFiniteElement<FEL,ET,BASE> :: 
@@ -302,12 +50,6 @@ namespace ngfem
     double sum = 0;
     T_CalcShape (&pt(0), SBLambda ( [&](int i, double val) { sum += x(i)*val; } ));
     return sum;
-
-    /*
-    EvaluateShape<> eval(x); 
-    T_CalcShape (&pt(0), eval); 
-    return eval.Sum();
-    */
   }  
 
 
@@ -320,16 +62,11 @@ namespace ngfem
         Vec<DIM> pt = ir[i].Point();
 
         double sum = 0;
-        T_CalcShape (&pt(0), SBLambda ( [&](int i, double val) { sum += coefs(i)*val; } ));
+        T_CalcShape (&pt(0), SBLambda ( [&](int i, double shape) { sum += coefs(i)*shape; } ));
         vals(i) = sum;
-
-        /*
-	EvaluateShape<> eval(coefs);
-        T_CalcShape (&pt(0), eval); 
-        vals(i) = eval.Sum();
-        */
       }
   }
+
 
   template <class FEL, ELEMENT_TYPE ET, class BASE>
   void T_ScalarFiniteElement<FEL,ET,BASE> :: 
@@ -341,15 +78,10 @@ namespace ngfem
     for (int i = 0; i < ir.GetNIP(); i++)
       {
         Vec<DIM> pt = ir[i].Point();
-        double vali = vals(i);
-        T_CalcShape (&pt(0), SBLambda ( [&](int i, double shape) { coefs(i) += vali*shape; } ));
-        /*
-	EvaluateShapeTrans<> eval(coefs, vals(i));
-        T_CalcShape (&pt(0), eval); 
-        */
+        T_CalcShape (&pt(0), SBLambda ( [&](int j, double shape) 
+                                        { coefs(j) += vals(i)*shape; } ));
       }
   }
-
 
 
   template <class FEL, ELEMENT_TYPE ET, class BASE>
@@ -362,10 +94,10 @@ namespace ngfem
 	for (int j = 0; j < DIM; j++)
 	  adp[j] = AutoDiff<DIM> (ir[i](j), j);
 
-	Vec<DIM> val = 0;
-	EvaluateDShape<DIM> eval(coefs, val);
-        T_CalcShape (&adp(0), eval); 
-	vals.Row(i) = val;
+        Vec<DIM> sum = 0.0;
+        T_CalcShape (&adp(0), SBLambda ([&] (int j, AD2Vec<DIM> shape)
+                                        { sum += coefs(j) * shape; }));
+        vals.Row(i) = sum;
       }
   }
 
@@ -381,12 +113,11 @@ namespace ngfem
 	for (int j = 0; j < DIM; j++)
 	  adp[j] = AutoDiff<DIM> (ir[i](j), j);
 
-	Vec<DIM> val;
-	val = vals.Row(i);
-	EvaluateDShapeTrans<DIM> eval(coefs, val);
-        T_CalcShape (&adp(0), eval); 
+        T_CalcShape (&adp(0), SBLambda ([&] (int j, AD2Vec<DIM> shape)
+                                        { coefs(j) += InnerProduct (vals.Row(i), shape); }));
       }
   }
+
 
   template <class FEL, ELEMENT_TYPE ET, class BASE>
   void T_ScalarFiniteElement<FEL,ET,BASE> :: 
@@ -396,9 +127,9 @@ namespace ngfem
     Vec<DIM, AutoDiff<DIM> > adp;
     for (int i = 0; i < DIM; i++)
       adp[i] = AutoDiff<DIM> (ip(i), i);
-      
-    DShapeAssign<DIM> ds(dshape); 
-    T_CalcShape (&adp(0), ds);
+
+    T_CalcShape (&adp(0), SBLambda ([&] (int i, AD2Vec<DIM> shape)
+                                    { dshape.Row(i) = shape; }));
   }
 
 
@@ -414,9 +145,9 @@ namespace ngfem
     for (int i = 0; i < DIM; i++)
       for (int j = 0; j < DIM; j++)
 	adp[i].DValue(j) = mip.GetJacobianInverse()(i,j);
-      
-    DShapeAssign<DIM> ds(dshape); 
-    T_CalcShape (&adp(0), ds);
+
+    T_CalcShape (&adp(0), SBLambda ([&] (int i, AD2Vec<DIM> shape)
+                                    { dshape.Row(i) = shape; }));
   }
 
 
@@ -454,7 +185,7 @@ namespace ngfem
       }
 
     T_CalcShape (&po[0], orders);
-    // does not work for old tensor productelements: order cancelation for lam_e
+    // did not work for old tensor productelements: order cancelation for lam_e
   }
 }
 
