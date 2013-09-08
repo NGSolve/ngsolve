4aa105326175f9cf3561b2ecd377496b2e6478f3
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2013-09-08 11:09:40 +0000

    profiling

--- solve/ngs.cpp
+++ solve/ngs.cpp
@@ -34,5 +34,17 @@ int main(int argc, char ** argv)
                 << e.What() << std::endl;
     };
 
+
+#ifdef PARALLEL
+  int id;
+  MPI_Comm_rank(MPI_COMM_WORLD, &id);
+  char filename[100];
+  sprintf (filename, "ngs.prof.%d", id);
+  FILE *prof = fopen(filename,"w");
+  NgProfiler::Print (prof);
+  fclose(prof);
+#endif
+
+
   return 0;
 }
