8e51a2e87716aaba772a66a7d03ca5b36f0a63c5
Gerhard Kitzler <gerhard.kitzler@tuwien.ac.at>
2013-10-04 12:32:08 +0000

    removed "using typename BASE::T_IMPL;"

--- fem/l2hofefo.hpp
+++ fem/l2hofefo.hpp
@@ -40,7 +40,7 @@ namespace ngfem
   class L2HighOrderFEFO : public BASE
   {
   protected:
-    using typename BASE::T_IMPL;
+    // using typename BASE::T_IMPL;
     using typename BASE::T_SHAPES;
     typedef L2HighOrderFEFO_Shapes<ET,ORDER> SHAPES;
 
