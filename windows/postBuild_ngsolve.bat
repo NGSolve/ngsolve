3354ae4e56c1dd43e71371d22ab282ab3aeaa6f3
Gerhard Kitzler <gerhard.kitzler@tuwien.ac.at>
2013-10-04 12:48:07 +0000

    replaced "xcopy" with its full path "C:\Windows\System32\xcopy"

--- windows/postBuild_ngsolve.bat
+++ windows/postBuild_ngsolve.bat
@@ -17,6 +17,7 @@ set BUILD_ARCH=%~4
 set PROJ_DIR=%~5
 set LIB_NAME=%~6
 
+
 REM *** Change these Folders if required ***
 REM Check if the environment variable NETGENDIR exists, 
 REM and use it as the installation folder
@@ -36,18 +37,18 @@ echo POSTBUILD Script for %PROJ_NAME% ........
 
 REM *** Copy the core TCL files into the Install Folder ***
 echo Installing core TCL files into %INSTALL_FOLDER%\bin ....
-xcopy  "%NGSOLVE_TCLSRC%\*.tcl" "%INSTALL_FOLDER%\bin\" /i /d /y
+C:\Windows\System32\xcopy  "%NGSOLVE_TCLSRC%\*.tcl" "%INSTALL_FOLDER%\bin\" /i /d /y
 if errorlevel 1 goto CoreTCLFailed
 echo Installing core TCL Files: Completed OK!!
 
 REM *** Copy the primary NGSolve DLL into the Install Folder ***
 echo Installing %PROJ_EXEC% into %INSTALL_FOLDER%\bin ....
 if /i "%BUILD_ARCH%" == "win32" (
-   xcopy "%PROJ_DIR%%BUILD_TYPE%\%PROJ_EXEC%" "%INSTALL_FOLDER%\bin\" /i /d /y
+   C:\Windows\System32\xcopy "%PROJ_DIR%%BUILD_TYPE%\%PROJ_EXEC%" "%INSTALL_FOLDER%\bin\" /i /d /y
    if errorlevel 1 goto ExecInstallFailed
 )
 if /i "%BUILD_ARCH%" == "x64" (
-   xcopy "%PROJ_DIR%%BUILD_ARCH%\%BUILD_TYPE%\%PROJ_EXEC%" "%INSTALL_FOLDER%\bin\" /i /d /y
+   C:\Windows\System32\xcopy "%PROJ_DIR%%BUILD_ARCH%\%BUILD_TYPE%\%PROJ_EXEC%" "%INSTALL_FOLDER%\bin\" /i /d /y
    if errorlevel 1 goto ExecInstallFailed
 )   
 echo Installing %PROJ_EXEC%: Completed OK!!
@@ -55,25 +56,43 @@ echo Installing %PROJ_EXEC%: Completed OK!!
 REM *** Copy the NGSolve library and include files into the Install Folder ***
 echo Installing %LIB_NAME%.lib into %INSTALL_FOLDER%\lib ....
 if /i "%BUILD_ARCH%" == "win32" (
-   xcopy "%PROJ_DIR%%BUILD_TYPE%\%LIB_NAME%.lib" "%INSTALL_FOLDER%\lib\" /i /d /y
+   C:\Windows\System32\xcopy "%PROJ_DIR%%BUILD_TYPE%\%LIB_NAME%.lib" "%INSTALL_FOLDER%\lib\" /i /d /y
    if errorlevel 1 goto LibInstallFailed
 )
 if /i "%BUILD_ARCH%" == "x64" (
-   xcopy "%PROJ_DIR%%BUILD_ARCH%\%BUILD_TYPE%\%LIB_NAME%.lib" "%INSTALL_FOLDER%\lib\" /i /d /y
+   C:\Windows\System32\xcopy "%PROJ_DIR%%BUILD_ARCH%\%BUILD_TYPE%\%LIB_NAME%.lib" "%INSTALL_FOLDER%\lib\" /i /d /y
    if errorlevel 1 goto LibInstallFailed
 )   
 echo Installing %LIB_NAME%.lib: Completed OK!!
 
 echo Installing NgSolve development header files into %INSTALL_FOLDER%\include ....
-xcopy "%NGSOLVE_INCROOT%\include\*.hpp" "%INSTALL_FOLDER%\include\include\" /i /d /y
-xcopy "%NGSOLVE_INCROOT%\solve\*.hpp" "%INSTALL_FOLDER%\include\solve\" /i /d /y
-xcopy "%NGSOLVE_INCROOT%\parallel\*.hpp" "%INSTALL_FOLDER%\include\parallel\" /i /d /y
-xcopy "%NGSOLVE_INCROOT%\ngstd\*.hpp" "%INSTALL_FOLDER%\include\ngstd\" /i /d /y
-xcopy "%NGSOLVE_INCROOT%\multigrid\*.hpp" "%INSTALL_FOLDER%\include\multigrid\" /i /d /y
-xcopy "%NGSOLVE_INCROOT%\linalg\*.hpp" "%INSTALL_FOLDER%\include\linalg\" /i /d /y
-xcopy "%NGSOLVE_INCROOT%\fem\*.hpp" "%INSTALL_FOLDER%\include\fem\" /i /d /y
-xcopy "%NGSOLVE_INCROOT%\comp\*.hpp" "%INSTALL_FOLDER%\include\comp\" /i /d /y
-xcopy "%NGSOLVE_INCROOT%\basiclinalg\*.hpp" "%INSTALL_FOLDER%\include\basiclinalg\" /i /d /y
+C:\Windows\System32\xcopy "%NGSOLVE_INCROOT%\include\*.hpp" "%INSTALL_FOLDER%\include\include\" /i /d /y
+C:\Windows\System32\xcopy "%NGSOLVE_INCROOT%\solve\*.hpp" "%INSTALL_FOLDER%\include\solve\" /i /d /y
+C:\Windows\System32\xcopy "%NGSOLVE_INCROOT%\parallel\*.hpp" "%INSTALL_FOLDER%\include\parallel\" /i /d /y
+C:\Windows\System32\xcopy "%NGSOLVE_INCROOT%\ngstd\*.hpp" "%INSTALL_FOLDER%\include\ngstd\" /i /d /y
+C:\Windows\System32\xcopy "%NGSOLVE_INCROOT%\multigrid\*.hpp" "%INSTALL_FOLDER%\include\multigrid\" /i /d /y
+C:\Windows\System32\xcopy "%NGSOLVE_INCROOT%\linalg\*.hpp" "%INSTALL_FOLDER%\include\linalg\" /i /d /y
+C:\Windows\System32\xcopy "%NGSOLVE_INCROOT%\fem\*.hpp" "%INSTALL_FOLDER%\include\fem\" /i /d /y
+C:\Windows\System32\xcopy "%NGSOLVE_INCROOT%\comp\*.hpp" "%INSTALL_FOLDER%\include\comp\" /i /d /y
+C:\Windows\System32\xcopy "%NGSOLVE_INCROOT%\basiclinalg\*.hpp" "%INSTALL_FOLDER%\include\basiclinalg\" /i /d /y
+C:\Windows\System32\xcopy "%NGSOLVE_INCROOT%\basiclinalg\*.h" "%INSTALL_FOLDER%\include\basiclinalg\" /i /d /y
+
+REM *** Copy the NgSolve pde tutorials to the install folder ***
+echo Copying pde tutorials
+C:\Windows\System32\xcopy "%PROJ_DIR%..\pde_tutorial" "%INSTALL_FOLDER%\pde_tutorial\" /i /d /y /e
+
+REM *** Copy the Lapack libraries to the bin folder ***
+if /i "%BUILD_TYPE%"=="Release_Lapack" (
+if /i "%BUILD_ARCH%"=="x64"	(
+		echo Copying Lapack x64 dlls and gfortran runtime libraries
+		C:\Windows\System32\xcopy "%PROJ_DIR%..\..\ext_libs\OpenBlas\lib\x64\*.dll" "%INSTALL_FOLDER%\bin" /i /d /y
+)
+if /i "%BUILD_ARCH%"=="win32" (
+		echo Copying Lapack win32 dlls and gfortran runtimes libraries
+		C:\Windows\System32\xcopy "%PROJ_DIR%..\..\ext_libs\OpenBlas\lib\x86\*.dll" "%INSTALL_FOLDER%\bin" /i /d /y
+)	
+)
+
 
 
 REM *** Done with the installation routine ***
