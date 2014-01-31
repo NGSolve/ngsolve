a102391d668d7d415b561859c11c4f73a4329ad3
Gerhard Kitzler <gerhard.kitzler@tuwien.ac.at>
2014-01-31 12:02:07 +0000

    removed a NGS_DLL_HEADER

--- fem/diffop.hpp
+++ fem/diffop.hpp
@@ -257,7 +257,7 @@ namespace ngfem
     virtual bool Boundary() const { return int(DIM_SPACE) > int(DIM_ELEMENT); }
     virtual string Name() const { return DIFFOP::Name(); }
     
-    NGS_DLL_HEADER virtual void
+    virtual void
     CalcMatrix (const FiniteElement & bfel,
 		const BaseMappedIntegrationPoint & bmip,
 		FlatMatrix<double> mat, 
