f0e8569202eec93f102bb13ba414e9fa4b733a5e
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2014-08-15 08:31:34 +0000

    python exported a few mesh and element features

--- comp/python_comp.cpp
+++ comp/python_comp.cpp
@@ -13,16 +13,19 @@ struct PyExportComp {
 
         bp::class_<Ngs_Element>("Ngs_Element", bp::no_init)
             .add_property("vertices", FunctionPointer([](Ngs_Element &el)->Array<int>{ return Array<int>(el.Vertices());} ))
+            .add_property("edges", FunctionPointer([](Ngs_Element &el)->Array<int>{ return Array<int>(el.Edges());} ))
+            .add_property("faces", FunctionPointer([](Ngs_Element &el)->Array<int>{ return Array<int>(el.Faces());} ))
             ;
 
+
         bp::class_<MeshAccess>("MeshAccess", bp::no_init)
-            .def("GetNV", &MeshAccess::GetNV)
+          // .def("GetNV", &MeshAccess::GetNV)
             .def("GetElement", static_cast< Ngs_Element (MeshAccess::*)(int, bool)const> (&MeshAccess::GetElement),
                     (bp::arg("arg1")=NULL,bp::arg("arg2")=0))
             .def("GetElementVertices", static_cast<void (MeshAccess::*)(int, Array<int> &) const>( &MeshAccess::GetElVertices))
             .add_property ("nv", &MeshAccess::GetNV)
+            .add_property ("ne", static_cast<int(MeshAccess::*)()const> (&MeshAccess::GetNE))
             ;
-
 
     }
 };
