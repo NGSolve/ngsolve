95c233dd9207868ac2db0e7fd57b0ff9acf35221
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2014-08-16 14:08:00 +0000

    Moved Array from bla to ngstd

--- basiclinalg/python_bla.cpp
+++ basiclinalg/python_bla.cpp
@@ -24,26 +24,6 @@ struct PyExportBla {
             .def(bp::init<int>())
             ;
 
-        py_env["FlatArrayD"] = bp::class_<FlatArray<double> >("FlatArrayD")
-            .def(PyDefVector<FlatArray<double>, double>()) 
-            .def(PyDefToString<FlatArray<double> >())
-            .def(bp::init<int, double *>())
-            ;
-
-        py_env["ArrayD"] = bp::class_<Array<double>, bp::bases<FlatArray<double> > >("ArrayD")
-            .def(bp::init<int>())
-            ;
-
-        py_env["FlatArrayI"] = bp::class_<FlatArray<int> >("FlatArrayI")
-            .def(PyDefVector<FlatArray<int>, int>()) 
-            .def(PyDefToString<FlatArray<int> >())
-            .def(bp::init<int, int *>())
-            ;
-
-        py_env["ArrayI"] = bp::class_<Array<int>, bp::bases<FlatArray<int> > >("ArrayI")
-            .def(bp::init<int>())
-            ;
-
         typedef FlatMatrix<double> FMD;
         py_env["FlatMatrix"] = bp::class_<FlatMatrix<double> >("FlatMatrix")
             .def(PyDefToString<FMD>())
--- ngstd/python_ngstd.cpp
+++ ngstd/python_ngstd.cpp
@@ -32,6 +32,27 @@ PythonEnvironment::PythonEnvironment() {
     catch(bp::error_already_set const &) {
         PyErr_Print();
     }
+
+    // Export ngstd classes
+    py_env["FlatArrayD"] = bp::class_<FlatArray<double> >("FlatArrayD")
+        .def(PyDefVector<FlatArray<double>, double>()) 
+        .def(PyDefToString<FlatArray<double> >())
+        .def(bp::init<int, double *>())
+        ;
+
+    py_env["ArrayD"] = bp::class_<Array<double>, bp::bases<FlatArray<double> > >("ArrayD")
+        .def(bp::init<int>())
+        ;
+
+    py_env["FlatArrayI"] = bp::class_<FlatArray<int> >("FlatArrayI")
+        .def(PyDefVector<FlatArray<int>, int>()) 
+        .def(PyDefToString<FlatArray<int> >())
+        .def(bp::init<int, int *>())
+        ;
+
+    py_env["ArrayI"] = bp::class_<Array<int>, bp::bases<FlatArray<int> > >("ArrayI")
+        .def(bp::init<int>())
+        ;
 }
 
 #endif // NGS_PYTHON
