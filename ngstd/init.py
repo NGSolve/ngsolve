5e600acc5b4693751a120e6946df403eb3e240b0
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2014-08-21 14:17:44 +0000

    python mydir

--- ngstd/init.py
+++ ngstd/init.py
@@ -1,3 +1,8 @@
+def mydir(x=None):
+    if x==None:
+        return []
+    else:
+        return [i for i in dir(x) if not '__' in i]
 
 def startConsole():
     import readline # optional, will allow Up/Down/History in the console
