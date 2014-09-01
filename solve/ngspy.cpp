38badc038380107e8430a75fdf9d3dd64e667c6c
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2014-09-01 17:04:02 +0000

    Exception handling

--- solve/ngspy.cpp
+++ solve/ngspy.cpp
@@ -24,10 +24,22 @@ int ExportNgsolve();
 
 BOOST_PYTHON_MODULE(libngspy)
 {
-  ExportNgstd();
-  ExportNgbla();
-  ExportNgfem();
-  ExportNgla();
-  ExportNgcomp();
-  ExportNgsolve();
+    try
+    {
+        ExportNgstd();
+        ExportNgbla();
+        ExportNgfem();
+        ExportNgla();
+        ExportNgcomp();
+        ExportNgsolve();
+    }
+    catch (ngstd::Exception & e)
+    {
+        cerr << "\n\nCaught Exception:\n" << e.What() << endl;
+    }
+    catch (...)
+    {
+        cerr << "\n\nCaught Python Exception:\n" << endl;
+        PyErr_Print();
+    }
 }
