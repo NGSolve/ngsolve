a43a701ecc2133c9af1c1ee67a8f1ff1eaaf896c
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2014-09-09 08:22:38 +0000

    Flags with standard semantics

--- ngstd/flags.cpp
+++ ngstd/flags.cpp
@@ -20,10 +20,10 @@ namespace ngstd
 
   Flags :: Flags (const Flags & flags)
   {
-    const char * name;
+    string name;
     for (int i = 0; i < flags.GetNStringFlags(); i++)
       {
-	const char * str = flags.GetStringFlag (i, name);
+	string str = flags.GetStringFlag (i, name);
 	SetFlag (name, str);
       }
     for (int i = 0; i < flags.GetNNumFlags(); i++)
@@ -38,12 +38,12 @@ namespace ngstd
       }
     for (int i = 0; i < flags.GetNNumListFlags(); i++)
       {
-	const Array<double> * numa = flags.GetNumListFlag (i, name);
+	auto numa = flags.GetNumListFlag (i, name);
 	SetFlag (name, *numa);
       }
     for (int i = 0; i < flags.GetNStringListFlags(); i++)
       {
-	const Array<char*> * stra = flags.GetStringListFlag (i, name);
+	auto stra = flags.GetStringListFlag (i, name);
 	SetFlag (name, *stra);
       }
   }
@@ -74,59 +74,64 @@ namespace ngstd
   
   void Flags :: DeleteFlags ()
   {
-    for (int i = 0; i < strflags.Size(); i++)
-      delete [] strflags[i];
+    // for (int i = 0; i < strflags.Size(); i++)
+    // delete [] strflags[i];
     strflags.DeleteAll();
     numflags.DeleteAll();
     defflags.DeleteAll();
+    /*
     for(int i=0; i<strlistflags.Size(); i++)
       {
         // for (int j = 0; j < strlistflags[i]->Size(); j++)
         // delete [] (*strlistflags[i])[j];
         delete strlistflags[i];
       }
+    */
     strlistflags.DeleteAll();
-    for(int i=0; i<numlistflags.Size(); i++)
-      delete numlistflags[i];
+    // for(int i=0; i<numlistflags.Size(); i++)
+    // delete numlistflags[i];
     numlistflags.DeleteAll();
   }
   
-  Flags & Flags :: SetFlag (const char * name, const char * val)
+  Flags & Flags :: SetFlag (const string & name, const string & val)
   {
-    char * hval = new char[strlen (val) + 1];
-    strcpy (hval, val);
-    strflags.Set (name, hval);
+    // char * hval = new char[strlen (val) + 1];
+    // strcpy (hval, val);
+    strflags.Set (name, val);
     return *this;
   }
   
-  Flags & Flags :: SetFlag (const char * name, double val) &
+  Flags & Flags :: SetFlag (const string & name, double val) &
   {
     numflags.Set (name, val);
     return *this;
   }
   
-  Flags & Flags :: SetFlag (const char * name)
+  Flags & Flags :: SetFlag (const string & name)
   {
     defflags.Set (name, 1);
     return *this;
   }
 
 
-  Flags & Flags :: SetFlag (const char * name, const Array<char*> & val)
+  Flags & Flags :: SetFlag (const string & name, const Array<string> & val)
   {
-    Array<char*> * strarray = new Array<char*>;
+    auto strarray = make_shared<Array<string>>(val);
+      /*
     for (int i = 0; i < val.Size(); i++)
       {
 	strarray->Append (new char[strlen(val[i])+1]);
 	strcpy (strarray->Last(), val[i]);
       }
+      */
     strlistflags.Set (name, strarray);    
     return *this;
   }
 
-  Flags & Flags :: SetFlag (const char * name, const Array<double> & val)
+  Flags & Flags :: SetFlag (const string & name, const Array<double> & val)
   {
-    Array<double> * numarray = new Array<double>(val);
+    // Array<double> * numarray = new Array<double>(val);
+    auto numarray = make_shared<Array<double>> (val);
 
     numlistflags.Set (name, numarray);
     return *this;
@@ -134,9 +139,18 @@ namespace ngstd
 
 
 
-  
-  const char * 
-  Flags :: GetStringFlag (const char * name, const char * def) const
+  /*
+  const string & 
+  Flags :: GetStringFlag (const string & name, const string & def) const
+  {
+    if (strflags.Used (name))
+      return strflags[name];
+    else
+      return def;
+  }
+  */
+
+  const string & Flags :: GetStringFlag (const string & name, const string & def) const
   {
     if (strflags.Used (name))
       return strflags[name];
@@ -144,16 +158,8 @@ namespace ngstd
       return def;
   }
 
-  string Flags :: GetStringFlag (const char * name, const string & def) const
-  {
-    if (strflags.Used (name))
-      return strflags[name];
-    else
-      return def;
-  }
 
-
-  double Flags :: GetNumFlag (const char * name, double def) const
+  double Flags :: GetNumFlag (const string & name, double def) const
   {
     if (numflags.Used (name))
       return numflags[name];
@@ -161,7 +167,7 @@ namespace ngstd
       return def;
   }
   
-  const double * Flags :: GetNumFlagPtr (const char * name) const
+  const double * Flags :: GetNumFlagPtr (const string & name) const
   {
     if (numflags.Used (name))
       return & ((SymbolTable<double>&)numflags)[name];
@@ -169,39 +175,40 @@ namespace ngstd
       return NULL;
   }
   
-  double * Flags :: GetNumFlagPtr (const char * name) 
+  double * Flags :: GetNumFlagPtr (const string & name) 
   {
     if (numflags.Used (name))
       return & ((SymbolTable<double>&)numflags)[name];
     else
       return NULL;
   }
-  
+
+  /*
   int Flags :: GetDefineFlag (const char * name) const
   {
     return defflags.Used (name);
   }
-
+  */
   int Flags :: GetDefineFlag (const string & name) const
   {
     return defflags.Used (name);
   }
 
 
-  const Array<char*> & 
-  Flags :: GetStringListFlag (const char * name) const
+  const Array<string> & 
+  Flags :: GetStringListFlag (const string & name) const
   {
     if (strlistflags.Used (name))
       return *strlistflags[name];
     else
       {
-	static Array<char*> hstra(0);
+	static Array<string> hstra(0);
 	return hstra;
       }
   }
 
   const Array<double> & 
-  Flags ::GetNumListFlag (const char * name) const
+  Flags ::GetNumListFlag (const string & name) const
   {
     if (numlistflags.Used (name))
       return *numlistflags[name];
@@ -213,22 +220,22 @@ namespace ngstd
   }
 
 
-  int Flags :: StringFlagDefined (const char * name) const
+  bool Flags :: StringFlagDefined (const string & name) const
   {
     return strflags.Used (name);
   }
 
-  int Flags :: NumFlagDefined (const char * name) const
+  bool Flags :: NumFlagDefined (const string &name) const
   {
     return numflags.Used (name);
   }
-
-  int Flags :: StringListFlagDefined (const char * name) const
+  
+  bool Flags :: StringListFlagDefined (const string & name) const
   {
     return strlistflags.Used (name);
   }
 
-  int Flags :: NumListFlagDefined (const char * name) const
+  bool Flags :: NumListFlagDefined (const string & name) const
   {
     return numlistflags.Used (name);
   }
@@ -416,7 +423,7 @@ namespace ngstd
 	      }
 	    else
 	      {
-		Array<char *> strs;
+		Array<string> strs;
 
 		posbrack++;
 		char * hstr = new char[strlen(posbrack)+1];
@@ -428,7 +435,7 @@ namespace ngstd
 		while (*chp && *chp != ']')
 		  {
 		    if (start)
-		      strs.Append (chp);
+		      strs.Append (string(chp));
 		    start = 0;
 		    if (*chp == ',')
 		      {
