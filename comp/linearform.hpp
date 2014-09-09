6f8a53232d43e23c90d2700b25d91f79441a6ba1
Gerhard Kitzler <gerhard.kitzler@tuwien.ac.at>
2014-09-09 16:51:50 +0000

    windows work around for base class constructor

--- comp/linearform.hpp
+++ comp/linearform.hpp
@@ -181,11 +181,13 @@ namespace ngcomp
     enum { HEIGHT = mat_traits<TV>::HEIGHT };
 
     ///
-    using BASE::BASE;
-    /*
-    T_LinearForm (const FESpace & afespace, const string & aname,
-		  const Flags & flags);
-    */
+    //using BASE::BASE;   // not working on MSVC
+    
+    T_LinearForm(shared_ptr<FESpace> afespace, const string & aname,
+        const Flags & flags)
+        : BASE(afespace, aname, flags) {
+        ;
+    }
 
     ///
     virtual ~T_LinearForm ();
