d0638da82445cde5d782baa1a8fe37907299dfcd
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2014-09-09 16:20:21 +0000

    solution evaluation

--- py_tutorials/all_in_one.py
+++ py_tutorials/all_in_one.py
@@ -13,6 +13,8 @@ from libngspy.ngcomp import *
 from libngspy.ngsolve import *
 
 
+import numpy as np
+
 mesh = Mesh("square.vol")
 
 v = FESpace ("h1ho", mesh, { "order" : 3, "dirichlet" : [1] })
@@ -23,17 +25,18 @@ u.Update()
 
 f = LinearForm (v,"lff", { } )
 f.Add (CreateLFI ("source", 2, ConstantCF(1)))
-f.Assemble(1000000)
+f.Assemble()
 
 a = BilinearForm (v,"bfa", { } )
 a.Add (CreateBFI ("mass", 2, ConstantCF(1)))
 a.Add (CreateBFI ("laplace", 2, ConstantCF(1)))
-a.Assemble(1000000)
+a.Assemble()
 
 inv = a.mat.Inverse(v.FreeDofs())
 u.vec.data = inv * f.vec
 
-
+pnts = [ (x,y) for x in np.linspace(0,1,6) for y in np.linspace(0,1,6)]
+vals = [ u(p[0], p[1]) for p in pnts ]
 
 
 
