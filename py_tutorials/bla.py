5597bd49a293871f64c0067fab78a6898d3856e7
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2014-09-17 16:46:02 +0000

    Python Complex Basiclinalg

--- py_tutorials/bla.py
+++ py_tutorials/bla.py
@@ -14,31 +14,52 @@ m = 7
 x = Vector(m)
 A = Matrix(n,m)
 
-#initialize A and x
+# initialize A and x
 for i in range(m):
     x[i] = i+1
     for j in range(n):
         A[j,i] = j*j+i+1
         A[j,i] = i*i+j+1
 
-# Allocate result Vector
-y = Vector(n)
 
-# Write A*x to already existing Vector y,
-# -> no extra memory allocation!
-y.data = A*x
+# Arithmetic
+y = A*x
+z = 5*A*x + 7*y - A*5*(x-y)
 
-# Allocate new Vector with result
-z = Vector(5*A*x + 7*y)
 
-# Should be zero
-print(Vector(z-12*y))
+# Get third row as Vector
+A[2]
 
-# my_expression is of type 'SumExpr'
-# -> no calculation is done at this point
-my_expression = x+y
+# Get third row as Matrix
+A[2:3]
 
-# Assign x+y to z
-z.data = my_expression
-print(z)
+# Get rows 4 to 7
+A[4:8]
 
+# Get columns 1,3,5 (0-based)
+A[:, 1:6:2]
+
+# Set every second element in every third column to ten
+A[::2,::3] = 10
+
+# Multiply this elements by 7
+A[::2,::3] *= 7
+
+# Reset elements in last two rows and last three columns
+A[-2:,-3:] = 0
+
+# other stuff
+A[3] = x
+A[:, 2] = A*x
+
+# Complex classes
+B = Matrix(10,10, complex=True)
+C = Matrix(10,10, True)
+w = Vector(10,True) 
+
+for i in range(10):
+    w[i] = complex(i+1, i*i)
+    for k in range(10):
+        B[k,i] = complex(k*k+i+1, i+1+k)
+
+v = A*w
