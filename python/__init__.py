bc727da747602bb1ab403bebb1ae9d53709143cd
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2014-10-02 13:48:30 +0000

    expr module fix

--- python/__init__.py
+++ python/__init__.py
@@ -6,14 +6,14 @@ del path
 
 from sys import platform as __platform
 
-# from libngspy import *
-
-print ("importing ngsolve modules")
-from . import ngstd
-from . import bla
-from . import fem
-from . import la
-from . import comp
-from . import solve
-print ("done main init")
+__all__ = ['ngstd','bla','fem','la','comp','solve'];
+ 
+# print ("importing ngsolve modules")
+# from . import ngstd
+# from . import bla
+# from . import fem
+# from . import la
+# from . import comp
+# from . import solve
+# print ("done main init")
 
