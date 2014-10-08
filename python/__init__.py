f89060e26064ffdff62153e229ab9e28e2018fc7
Christoph Wintersteiger <c.wintersteiger@gmx.at>
2014-10-08 13:37:37 +0000

    windows fixes

--- python/__init__.py
+++ python/__init__.py
@@ -1,7 +1,10 @@
 from os import environ
 from sys import path
 from sys import platform as __platform
-path.append(environ['NETGENDIR']+'/../lib')
+if __platform.startswith('linux') or __platform.startswith('darwin'):
+    path.append(environ['NETGENDIR']+'/../lib')
+if __platform.startswith('win'):
+    path.append(environ['NETGENDIR'])
 
 from . import csg
 from . import meshing
--- python/csg.py
+++ python/csg.py
@@ -2,10 +2,11 @@ from netgen import __platform
 if __platform.startswith('linux') or __platform.startswith('darwin'):
     # Linux or Mac OS X
     from libcsg.csg import *
-    from libcsgvis import *
+    from libcsgvis.csgvis import *
     import libmesh.meshing
 #    from libmesh.meshing import *
 if __platform.startswith('win'):
     # Windows
     from nglib.csg import *
+    from nglib.csgvis import *
     from nglib.meshing import *
