e0b71dc5b461a1db972b4c364cf39e849b7a7b6c
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2014-10-14 18:31:56 +0000

    togl with indirct rendering

--- ng/drawing.tcl
+++ ng/drawing.tcl
@@ -9,9 +9,9 @@ set oldmousey 0
 # if { 1 } {
 
 # use this one for Togl 2.0
-# if {[catch {togl .ndraw -width 400 -height 300  -rgba true -double true -depth true -privatecmap false -stereo false -indirect true -create init  -display draw -reshape reshape  }] } {    
+# if {[catch {togl .ndraw -width 400 -height 300  -rgba true -double true -depth true -privatecmap false -stereo false -indirect false -create init  -display draw -reshape reshape  }] } {    
 
-if {[catch {togl .ndraw -width 400 -height 300  -rgba true -double true -depth true -privatecmap false -stereo false -indirect true }] } {    puts "no OpenGL" 
+if {[catch {togl .ndraw -width 400 -height 300  -rgba true -double true -depth true -privatecmap false -stereo false -indirect false }] } {    puts "no OpenGL" 
 } {
     #
     pack .ndraw -expand true -fill both -padx 10 -pady 10
