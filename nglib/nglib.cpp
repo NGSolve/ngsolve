9977e12e68b454ccfc9bd042910de738cb202b83
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2014-10-16 16:05:42 +0000

    dummy not needed also on Linux

--- nglib/nglib.cpp
+++ nglib/nglib.cpp
@@ -1226,11 +1226,12 @@ namespace netgen
 } // End of namespace netgen
 
 
+/*
+
 #ifndef WIN32
 void Ng_Redraw () { ; }
 void Ng_ClearSolutionData() { ; }
 #endif
-/*
 void Ng_SetSolutionData (Ng_SolutionData * soldata) 
 { 
   delete soldata->solclass;
