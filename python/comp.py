60098c37fd89c2ea8051751a55d2b74a7640f4c8
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2014-10-24 20:33:29 +0000

    ngslib also on Linux and Mac

--- python/comp.py
+++ python/comp.py
@@ -1,14 +1,16 @@
 from . import fem
 from . import la
 
-from ngsolve import __platform
-if __platform.startswith('linux') or __platform.startswith('darwin'):
-    # Linux or Mac OS X
-    from libngcomp.comp import *
+##from ngsolve import __platform
+##if __platform.startswith('linux') or __platform.startswith('darwin'):
+##    # Linux or Mac OS X
+##    from libngcomp.comp import *
 
-if __platform.startswith('win'):
-    # Windows
-    from ngslib.comp import *
+##if __platform.startswith('win'):
+##    # Windows
+##    from ngslib.comp import *
+
+from ngslib.comp import *
 
 __all__ = ['BND', 'BilinearForm', 'COUPLING_TYPE', 'CompoundFESpace', 'ElementId', 'FESpace', 'GridFunction', 'LinearForm', 'Mesh', 'Preconditioner', 'VOL', 'NumProc', 'PDE', 'PyNumProc']
 
--- python/solve.py
+++ python/solve.py
@@ -1,13 +1,15 @@
 from . import comp
 
-from ngsolve import __platform
-if __platform.startswith('linux') or __platform.startswith('darwin'):
-    # Linux or Mac OS X
-    from libsolve.solve import *
+##from ngsolve import __platform
+##if __platform.startswith('linux') or __platform.startswith('darwin'):
+##    # Linux or Mac OS X
+##    from libsolve.solve import *
 
-if __platform.startswith('win'):
-    # Windows
-    from ngslib.solve import *
+##if __platform.startswith('win'):
+##    # Windows
+##    from ngslib.solve import *
+
+from ngslib.solve import *
 
 __all__ = ['Redraw']
 
