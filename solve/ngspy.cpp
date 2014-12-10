3961eab6c705989e686c7a96703eebfeee71fbcd
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2014-12-10 10:11:36 +0000

    lapack load & test

--- solve/ngspy.cpp
+++ solve/ngspy.cpp
@@ -23,8 +23,20 @@ void ExportNgcomp();
 void ExportNgsolve();
 
 
+// char * libargv = (char*) { "libngs" };
+// MyMPI mpiinit (1, &libargv);
+
+
+
 BOOST_PYTHON_MODULE(ngslib)
 {
+
+  cout << "do some lapack stuff" << endl;
+  Matrix<> a(1000), b(1000), c(1000);
+  a = 1; b = 2;
+  c = a*b | Lapack;
+
+
     try
     {
         ExportNgstd();
