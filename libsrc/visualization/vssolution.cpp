325e1e315c79ea76fb6c2a14644124eb6d65e1ca
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2015-01-09 16:03:35 +0000

    openmp3.1 check

--- libsrc/visualization/vssolution.cpp
+++ libsrc/visualization/vssolution.cpp
@@ -2466,7 +2466,9 @@ namespace netgen
               double hminv = numeric_limits<double>::max();
               double hmaxv = -numeric_limits<double>::max();
               bool hhasit = false;
+#if defined _OPENMP && _OPENMP >= 201107
 #pragma omp parallel for reduction (max : hmaxv) reduction (min : hminv) reduction (|| : hhasit)
+#endif
               for (int i = 0; i < ne; i++) 
                 {
                   double val;
