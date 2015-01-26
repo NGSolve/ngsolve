1b10dc6b9bf4567fe3d0fc074bc91fc969b35c45
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2015-01-26 14:17:33 +0000

    visual_dummy for no-gui

---
+++ libsrc/visualization/visual_dummy.cpp
@@ -0,0 +1,15 @@
+#include <mystdlib.h>
+
+#include <meshing.hpp>
+#include "../include/nginterface.h"
+
+void Ng_ClearSolutionData () { ; }
+void Ng_InitSolutionData (Ng_SolutionData * soldata) { ; }
+void Ng_SetSolutionData (Ng_SolutionData * soldata) { ; }
+void Ng_Redraw (bool blocking) { ; }
+
+
+
+
+
+
