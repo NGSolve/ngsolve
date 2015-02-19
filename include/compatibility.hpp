c55796c95e222e1a6a51518c6acfade30327e8e5
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2015-02-19 16:41:18 +0000

    removed outdated compatibility function

--- include/compatibility.hpp
+++ include/compatibility.hpp
@@ -13,18 +13,18 @@ different versions of ngsolve.
 namespace ngcomp
 {
 
-  // argument MeshAccess ma  was removed since it is available in GridFunction u
-  // change: ngsolve-5.1 - r1177, 20130404
-  NGS_DLL_HEADER 
-  inline void SetValues (const MeshAccess & ma,
-                         const CoefficientFunction & coef,
-                         GridFunction & u,
-                         bool vb,
-                         DifferentialOperator * diffop,   // NULL is FESpace evaluator
-                         LocalHeap & clh)
-  {
-    SetValues (coef, u, vb, diffop, clh);
-  }
+  // // argument MeshAccess ma  was removed since it is available in GridFunction u
+  // // change: ngsolve-5.1 - r1177, 20130404
+  // NGS_DLL_HEADER 
+  // inline void SetValues (const MeshAccess & ma,
+  //                        const CoefficientFunction & coef,
+  //                        GridFunction & u,
+  //                        bool vb,
+  //                        DifferentialOperator * diffop,   // NULL is FESpace evaluator
+  //                        LocalHeap & clh)
+  // {
+  //   SetValues (coef, u, vb, diffop, clh);
+  // }
 }
   
 
