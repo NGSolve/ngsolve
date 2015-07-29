2669a920ae522a95cb872ac2030367e19da022d5
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2015-07-29 22:56:12 +0200

    argument packs

--- python/utils.py
+++ python/utils.py
@@ -19,7 +19,12 @@ def Neumann (coef):
     return LFI("neumann", coef=coef)
 
 
-def H1(mesh, order=1, dirichlet=[], dim=1, flags={}):
+def H1(mesh, order=1, complex=False, dirichlet=[], dim=1, flags={}):
     """ Create H1 finite element space """
-    return FESpace("h1ho", mesh, flags=flags, order=order, dirichlet=dirichlet, dim=dim)
+    return FESpace("h1ho", mesh, flags=flags, order=order, complex=complex,
+                   dirichlet=dirichlet, dim=dim)
+
+# def H1(mesh, **args):
+#     """ Create H1 finite element space """
+#     return FESpace("h1ho", mesh, **args)
 
