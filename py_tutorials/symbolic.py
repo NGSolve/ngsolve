e1b8d9fde7980d0ee6a06340910fce66e4ba8c1e
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2015-08-08 19:08:07 +0200

    new py-tutorials

---
+++ py_tutorials/symbolic.py
@@ -0,0 +1,34 @@
+from ngsolve.fem import *
+from ngsolve.comp import *
+from ngsolve.utils import *
+
+import netgen
+
+ngsglobals.msg_level = 1
+mesh = Mesh (netgen.csg.unit_cube.GenerateMesh(maxh=0.2))
+
+import timeit
+
+
+V = H1(mesh, order=3)
+
+u = V.TrialFunction()
+v = V.TestFunction()
+
+gradu = u.Deriv()
+gradv = v.Deriv()
+dvdx = gradv[0]
+dvdy = gradv[1]
+dvdz = gradv[2]
+
+a = BilinearForm (V, symmetric=True)
+a += SymbolicBFI (u*v+gradu*gradv)
+a.Assemble(heapsize=10000000)
+
+
+f = LinearForm (V)
+f += SymbolicLFI (x*v)
+f.Assemble()
+
+print (a.mat)
+print (f.vec)
