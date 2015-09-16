50e5a9b9ec334f30de40070fc1b163cd46b9031d
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2015-09-16 07:19:56 -0700

    elasticity-py example

---
+++ pde_tutorial_py/d5_beam.py
@@ -0,0 +1,29 @@
+from ngsolve import *
+
+mesh = Mesh("beam.vol")
+
+V = H1(mesh, order=3, dim=3, dirichlet=[1])
+u = GridFunction(V)
+
+E = 2.1E11
+nu = 0.2
+a = BilinearForm(V)
+a += BFI("elasticity", coef=[E,nu])
+
+c = Preconditioner(a,"local")
+a.Assemble()
+
+
+traction = DomainConstantCF ([0,1e5])
+f = LinearForm(V)
+f += BlockLFI (Neumann(traction), dim=3, comp=2)
+f.Assemble()
+
+
+# u.vec.data = a.mat.Inverse(V.FreeDofs()) * f.vec
+
+BVP(bf=a, gf=u, lf=f, pre=c, maxsteps=1000).Do()
+
+Draw (u)
+
+
