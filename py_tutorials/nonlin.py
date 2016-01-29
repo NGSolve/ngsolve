59541c5169c3484bb03416175c0595f2d11d2c73
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2016-01-29 17:16:43 +0100

    non-linear SymbolicBFI implementation and example

---
+++ py_tutorials/nonlin.py
@@ -0,0 +1,29 @@
+from netgen.geom2d import unit_square
+from ngsolve import *
+
+m = Mesh (unit_square.GenerateMesh(maxh=0.3))
+
+V = H1(m, order=3, dirichlet=[1,2,3,4])
+u = V.TrialFunction()
+v = V.TestFunction()
+
+a = BilinearForm(V)
+a += SymbolicBFI( grad(u) * grad(v) + 5*u*u*v- 1 * v)
+
+u = GridFunction(V)
+r = u.vec.CreateVector()
+w = u.vec.CreateVector()
+
+for it in range(5):
+    print ("Iterateion",it)
+    a.Apply(u.vec, r)
+    a.AssembleLinearization(u.vec)
+    
+    w.data = a.mat.Inverse(V.FreeDofs()) * r.data
+    print ("|w| =", w.Norm())
+    u.vec.data -= w
+
+    Draw(u)
+    input("<press a key>")
+    
+    
