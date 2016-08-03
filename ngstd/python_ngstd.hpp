051e3a1bbb16418fd1a7b2f01c831dbc9a9e712d
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2016-08-03 18:23:38 +0200

    boost-python 1.60 fix should not by applied for bp 1.61

--- ngstd/python_ngstd.hpp
+++ ngstd/python_ngstd.hpp
@@ -3,7 +3,7 @@
 #ifdef NGS_PYTHON
 
 #pragma clang diagnostic push
-#pragma clang diagnostic ignored "-W#pragma-messages"
+// #pragma clang diagnostic ignored "-W#pragma-messages"
 #pragma clang diagnostic ignored "-Wunused-local-typedefs"
 #include <boost/python.hpp>
 
@@ -14,7 +14,7 @@
 #include <iostream>
 
 
-#if BOOST_VERSION >= 106000
+#if (BOOST_VERSION >= 106000) && (BOOST_VERSION < 106100)
   // Boost Python 1.60 does not automatically register shared_ptr<T> with T
   #define REGISTER_PTR_TO_PYTHON_BOOST_1_60_FIX(type) bp::register_ptr_to_python<type>();
 #else
