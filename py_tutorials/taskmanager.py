4d7cd28aa63c3fc496d187cf919caee674987f4a
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2016-08-17 11:48:04 +0200

    add python tutorials to CMake tests

--- py_tutorials/taskmanager.py
+++ py_tutorials/taskmanager.py
@@ -2,7 +2,7 @@ from netgen.csg import unit_cube
 from ngsolve import *
 
 mesh = Mesh (unit_cube.GenerateMesh(maxh=0.4))
-for l in range(4):
+for l in range(3):
     mesh.Refine()
 
 fes = H1(mesh, order=3)
