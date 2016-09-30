25469241729a1f5542c10af2f7c115d3ef9d457b
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2016-09-30 14:14:45 +0200

    first attempt to use AutoDiffRec

--- fem/intrule.hpp
+++ fem/intrule.hpp
@@ -1562,6 +1562,63 @@ namespace ngstd
 
 namespace ngfem
 {
+  template<int DIMS, int DIMR>
+  TIP<DIMS,AutoDiffRec<DIMR,SIMD<double>>> GetTIP(const SIMD<MappedIntegrationPoint<DIMS,DIMR>> & mip);
+
+    template<int DIMR>
+    TIP<0,AutoDiffRec<DIMR,SIMD<double>>> GetTIP(const SIMD<MappedIntegrationPoint<0,DIMR>> & mip) 
+    {
+      TIP<0, AutoDiffRec<DIMR, SIMD<double>> > adp;
+      return adp;
+    }
+
+    template<int DIMR>
+    TIP<1,AutoDiffRec<DIMR,SIMD<double>>> GetTIP(const SIMD<MappedIntegrationPoint<1,DIMR>> & mip) 
+    {
+      TIP<1, AutoDiffRec<DIMR, SIMD<double>> > adp;
+      Mat<1,DIMR,SIMD<double>> ijac = mip.GetJacobianInverse();
+      const auto &ip = mip.IP();
+      adp.x.Value() = ip(0);
+      for (int j = 0; j < DIMR; j++)
+        adp.x.DValue(j) = ijac(0,j);
+      return adp;
+    }
+
+  
+    template<int DIMR>
+    TIP<2,AutoDiffRec<DIMR,SIMD<double>>> GetTIP(const SIMD<MappedIntegrationPoint<2,DIMR>> & mip) 
+    {
+      TIP<2, AutoDiffRec<DIMR, SIMD<double>> > adp;
+      Mat<2,DIMR,SIMD<double>> ijac = mip.GetJacobianInverse();
+      const auto &ip = mip.IP();
+      adp.x.Value() = ip(0);
+      adp.y.Value() = ip(1);
+      for (int j = 0; j < DIMR; j++)
+        adp.x.DValue(j) = ijac(0,j);
+      for (int j = 0; j < DIMR; j++)
+        adp.y.DValue(j) = ijac(1,j);
+      return adp;
+    }
+
+  
+    template<int DIMR>
+    TIP<3,AutoDiffRec<DIMR,SIMD<double>>> GetTIP(const SIMD<MappedIntegrationPoint<3,DIMR>> & mip) 
+    {
+      TIP<3, AutoDiffRec<DIMR, SIMD<double>> > adp;
+      Mat<3,DIMR,SIMD<double>> ijac = mip.GetJacobianInverse();
+      const auto &ip = mip.IP();
+      adp.x.Value() = ip(0);
+      adp.y.Value() = ip(1);
+      adp.z.Value() = ip(2);
+      for (int j = 0; j < DIMR; j++)
+        adp.x.DValue(j) = ijac(0,j);
+      for (int j = 0; j < DIMR; j++)
+        adp.y.DValue(j) = ijac(1,j);
+      for (int j = 0; j < DIMR; j++)
+        adp.z.DValue(j) = ijac(2,j);
+      return adp;
+    }
+
   
   class SIMD_IntegrationRule : public Array<SIMD<IntegrationPoint>,size_t>
   {
