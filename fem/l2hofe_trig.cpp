72f83da2a4ec3f5f4d542b2d614b6b3edaf83c9f
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2016-10-17 18:45:45 +0200

    l2-trig diagmass

--- fem/l2hofe_trig.cpp
+++ fem/l2hofe_trig.cpp
@@ -4,6 +4,7 @@
 /* Date:   6. Feb. 2003                                              */
 /*********************************************************************/
 
+// #define FILE_L2HOFE_CPP
 
 #include <fem.hpp>
 #include <tscalarfe_impl.hpp>
@@ -12,6 +13,16 @@
 
 namespace ngfem
 {
+
+  template <> inline void L2HighOrderFE<ET_TRIG> ::
+  GetDiagMassMatrix(FlatVector<> mass) const
+  {
+    for (int ix = 0, ii = 0; ix <= order; ix++)
+      for (int iy = 0; iy <= order - ix; iy++, ii++)
+	mass(ii) = 1.0 / ((2 * ix + 1) * (2 * ix + 2 * iy + 2));
+  }
+
+  
   template class L2HighOrderFE<ET_TRIG>;  
   template class T_ScalarFiniteElement<L2HighOrderFE_Shape<ET_TRIG>, ET_TRIG, DGFiniteElement<2> >;
   
