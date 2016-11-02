08a662ae7731eefd40224807a2e7d3eea7a0914a
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2016-11-02 06:05:08 +0100

    optional use of opengl-buffers (not supported on WIN without additional library)

--- libsrc/visualization/vssolution.hpp
+++ libsrc/visualization/vssolution.hpp
@@ -43,9 +43,12 @@ class DLL_HEADER VisualSceneSolution : public VisualScene
     Point<3> p;
   };
 
+  // #define USE_BUFFERS
+#ifdef USE_BUFFERS
   bool has_surfel_vbo = false;
   GLuint surfel_vbo[4]; //
   size_t surfel_vbo_size;
+#endif
   int surfellist;
   int linelist;
   int element1dlist;
