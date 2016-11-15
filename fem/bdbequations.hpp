4aa7af53ca69aae6f401fa0791113b7588ab179a
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2016-11-15 13:57:41 +0100

    fixes for matrix<simd>

--- fem/bdbequations.hpp
+++ fem/bdbequations.hpp
@@ -109,7 +109,7 @@ namespace ngfem
 
     using DiffOp<DiffOpGradient<D, FEL> >::ApplySIMDIR;    
     static void ApplySIMDIR (const FiniteElement & fel, const SIMD_BaseMappedIntegrationRule & mir,
-                             BareSliceVector<double> x, ABareSliceMatrix<double> y)
+                             BareSliceVector<double> x, BareSliceMatrix<SIMD<double>> y)
     {
       Cast(fel).EvaluateGrad (mir, x, y);
     }
