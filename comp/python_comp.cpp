d33d3c53b0202ae81cd12d4a5f6c803ce1c84ca4
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2016-11-28 10:57:37 +0100

    use py::keep_alive to ensure the iterated object is not destroyed while iterating over it

--- comp/python_comp.cpp
+++ comp/python_comp.cpp
@@ -242,12 +242,16 @@ void NGS_DLL_HEADER ExportNgcomp(py::module &m)
 
   py::class_<ElementRange, IntRange> (m, "ElementRange")
     .def(py::init<const MeshAccess&,VorB,IntRange>())
-    .def("__iter__", [] (ElementRange &er) { return py::make_iterator(er.begin(), er.end()); }) 
-    ;
+    .def("__iter__", [] (ElementRange &er)
+      { return py::make_iterator(er.begin(), er.end()); },
+      py::keep_alive<0,1>()
+    );
 
   py::class_<FESpace::ElementRange,shared_ptr<FESpace::ElementRange>, IntRange> (m, "FESpaceElementRange")
-    .def("__iter__", [] (FESpace::ElementRange &er) { return py::make_iterator(er.begin(), er.end()); } )
-    ;
+    .def("__iter__", [] (FESpace::ElementRange &er)
+      { return py::make_iterator(er.begin(), er.end()); },
+      py::keep_alive<0,1>()
+    );
 
 
   //////////////////////////////////////////////////////////////////////////////////////////
