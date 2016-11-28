b953db44d51d5c1c4b59962e439e46e1c1f06154
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2016-11-29 00:10:42 +0100

    pybind11: Add NumPy buffer support again for (Flat)Matrix/Vector

---
+++ tests/pytest/test_vector.py
@@ -0,0 +1,31 @@
+import pytest
+from ngsolve import *
+
+def test_vector():
+    n = 10
+    a = Vector(n)
+    assert len(a) == n
+    for i in range(n):
+        a[i] = i
+    assert list(a) == list(range(n))
+    assert list(a-a) == [0]*n
+
+    b = Vector(n)
+    b[:] = 42
+    b[1:8:3] = 99
+
+    assert list(b)== ([42,99,42]*3)+[42]
+
+def test_vector_numpy():
+    n = 10
+    a = Vector(n)
+    for i in range(len(a)):
+        a[i] = i
+
+    v = a.NumPy()
+    v[0] = 234
+    assert a[0] == 234
+    a[1] = 100
+    assert v[1] == 100
+
+    assert list(v) == list(a)
