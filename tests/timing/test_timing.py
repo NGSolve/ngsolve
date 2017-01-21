7ff0aa9b2c7ef4efd910851699c2ad53f6d76f10
Christopher Lackner <christopher.lackner@gmx.at>
2017-01-21 12:12:09 +0100

    make timings parallel

--- tests/timing/test_timing.py
+++ tests/timing/test_timing.py
@@ -6,8 +6,9 @@ mesh = Mesh(unit_square.GenerateMesh(maxh=0.3))
 
 h1 = H1(mesh=mesh,order=3)
 hcurl = HCurl(mesh=mesh,order=3)
-h1timing = h1.__timing__()
-hcurltiming = hcurl.__timing__()
+with TaskManager():
+    h1timing = h1.__timing__()
+    hcurltiming = hcurl.__timing__()
 print("h1: ", h1timing)
 print("hcurl: ", hcurltiming)
 
