5d6d3300531c5ffd621de7cc43041f229b2bee8e
Christopher Lackner <christopher.lackner@gmx.at>
2017-01-21 16:16:51 +0100

    add a bit of functionality

--- tests/timing/test_timing.py
+++ tests/timing/test_timing.py
@@ -1,14 +1,29 @@
 
 from ngsolve import *
 from netgen.geom2d import unit_square
+import subprocess
 
 mesh = Mesh(unit_square.GenerateMesh(maxh=0.3))
 
+def Timing(obj):
+    name = type(obj)
+    timing = obj.__timing__()
+    commit = subprocess.check_output(["git", "log"]).decode("utf-8").split("\n")[0]
+    return {"name" : name, "commit" : commit, "timing" : timing}
+
+def PrintTiming(obj):
+    timing = Timing(obj)
+    print("Print timing for ", timing["name"],", in", timing["commit"],":")
+    for key in timing["timing"]:
+        print(key,": ", timing["timing"][key])
+
 h1 = H1(mesh=mesh,order=3)
 hcurl = HCurl(mesh=mesh,order=3)
+
+PrintTiming(h1)
+PrintTiming(hcurl)
+
 with TaskManager():
-    h1timing = h1.__timing__()
-    hcurltiming = hcurl.__timing__()
-print("h1: ", h1timing)
-print("hcurl: ", hcurltiming)
+    PrintTiming(h1)
+    PrintTiming(hcurl)
 
