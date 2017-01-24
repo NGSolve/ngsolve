17347a2777bbfd1f14c203b164843bf611f67813
Markus Wess <markus.wess@tuwien.ac.at>
2017-01-24 10:18:50 +0100

    cleaned up createdim, added halfspace, added orig

--- comp/pmltrafo.hpp
+++ comp/pmltrafo.hpp
@@ -141,19 +141,22 @@ namespace ngcomp
 
   };
 
-
-
-  
-
   template <int DIM>
   class RadialPML_Transformation : public PML_TransformationDim<DIM>
   {
     Complex alpha;
     double rad;
+    Vector<double> origin;
+    Vec<DIM> dimorigin;
   public:
 
-    RadialPML_Transformation(double _rad, Complex _alpha) 
-      : PML_TransformationDim<DIM>(), rad(_rad), alpha(_alpha) { ; }
+    RadialPML_Transformation(double _rad, Complex _alpha, FlatVector<double> _origin) 
+      : PML_TransformationDim<DIM>(), rad(_rad), alpha(_alpha), origin(_origin) 
+    { 
+      if (origin.Size()>=DIM)
+        for (int i : Range(DIM))
+          dimorigin(i)=origin(i);
+    }
     
     ~RadialPML_Transformation() {;}
     
@@ -161,33 +164,38 @@ namespace ngcomp
     {
       cout << "alpha: " << alpha << endl;
       cout << "radius: " << rad << endl;
+      cout << "origin: " << origin << endl;
     }
 
     shared_ptr<PML_Transformation> CreateDim(int dim)
     {
-      switch (dim)
-      {
-        case 0:    
-          return make_shared<RadialPML_Transformation<0>> (rad,alpha);
+      if (origin.Size()>=dim)
+        switch (dim)
+        {
+          case 0:    
+            return make_shared<RadialPML_Transformation<0>> (rad,alpha,origin);
 
-        case 1:    
-          return make_shared<RadialPML_Transformation<1>> (rad,alpha);
+          case 1:    
+            return make_shared<RadialPML_Transformation<1>> (rad,alpha,origin);
 
-        case 2:    
-          return make_shared<RadialPML_Transformation<2>> (rad,alpha);
+          case 2:    
+            return make_shared<RadialPML_Transformation<2>> (rad,alpha,origin);
 
-        case 3:    
-          return make_shared<RadialPML_Transformation<3>> (rad,alpha);
+          case 3:    
+            return make_shared<RadialPML_Transformation<3>> (rad,alpha,origin);
 
-        default:
-          throw Exception ("No valid Dimension");
-      }
+          default:
+            throw Exception ("RadialPML_Transformation::CreateDim: No valid Dimension");
+        }
+      else
+         throw Exception ("RadialPML_Transformation::CreateDim: No valid Dimension");
     }
     
     void MapPoint (Vec<DIM> & hpoint, Vec<DIM,Complex> & point,
                    Mat<DIM,DIM,Complex> & jac) const
     {
-      double abs_x = L2Norm (hpoint);
+      Vec<DIM,double> rel_point = hpoint-dimorigin;
+      double abs_x = L2Norm (rel_point);
       if (abs_x <= rad)  
       {
         point = hpoint;
@@ -196,12 +204,12 @@ namespace ngcomp
       else
       {
         Complex g = 1.+alpha*(1.0-rad/abs_x);
-        point = g * hpoint;
+        point = origin + g * rel_point;
         // SZ: sollte da nicht abs_x * abs_x anstelle  abs_x*abs_x * abs_x stehen? 
         // JS: das hat schon so gestimmt
         jac =
           g * Id<DIM>() + (rad*alpha/(abs_x*abs_x*abs_x)) * 
-          (hpoint * Trans(hpoint));
+          (rel_point * Trans(rel_point));
       }
 
     }
@@ -213,7 +221,7 @@ namespace ngcomp
     Complex alpha;
     public:
 
-    CartesianPML_Transformation(Matrix<double> _bounds, Complex _alpha) 
+    CartesianPML_Transformation(FlatMatrix<double> _bounds, Complex _alpha) 
       : PML_TransformationDim<DIM>(), bounds(_bounds), alpha(_alpha) { ; }
     
     ~CartesianPML_Transformation() {;}
@@ -230,16 +238,16 @@ namespace ngcomp
         switch (dim)
         {
           case 0:     
-            return make_shared<CartesianPML_Transformation<0>> (abounds,alpha);
+            return make_shared<CartesianPML_Transformation<0>> (bounds,alpha);
 
           case 1:     
-            return make_shared<CartesianPML_Transformation<1>> (abounds,alpha);
+            return make_shared<CartesianPML_Transformation<1>> (bounds,alpha);
           
           case 2:
-            return make_shared<CartesianPML_Transformation<2>> (abounds,alpha);
+            return make_shared<CartesianPML_Transformation<2>> (bounds,alpha);
           
           case 3:
-            return make_shared<CartesianPML_Transformation<3>> (abounds,alpha);
+            return make_shared<CartesianPML_Transformation<3>> (bounds,alpha);
 
           default:
             throw Exception ("CartesianPML_Transformation::CreateDim: No valid Dimension");
@@ -272,14 +280,91 @@ namespace ngcomp
 
 
   template <int DIM>
+  class HalfSpacePML_Transformation : public PML_TransformationDim<DIM>
+  {
+    Vector<double> point;
+    Vec<DIM> dimpoint;
+    Vector<double> normal;
+    Vec<DIM> dimnormal;
+    Complex alpha;
+    public:
+    HalfSpacePML_Transformation(FlatVector<double> _point, FlatVector<double> _normal, Complex _alpha) 
+      : PML_TransformationDim<DIM>(), point(_point), normal(_normal), alpha(_alpha)
+    {
+      if (point.Size()>=DIM)
+        for (int i : Range(DIM))
+          dimpoint(i)=point(i);
+      else
+        throw Exception("HalfSpacePML_Transformation::HalfSpacePML_Transformation: point has too feq dimensions");
+      if (normal.Size()>=DIM)
+        for (int i : Range(DIM))
+          dimnormal(i)=normal(i)/L2Norm(normal);
+      else
+        throw Exception("HalfSpacePML_Transformation::HalfSpacePML_Transformation: normal has too feq dimensions");
+    }
+    
+    ~HalfSpacePML_Transformation() {;}
+
+    void PrintParameters()
+    {
+      cout << "point: " << point << endl;
+      cout << "normal: " << normal << endl;
+    }
+
+    shared_ptr<PML_Transformation> CreateDim(int dim)
+    {
+      if (point.Size()>=dim && normal.Size()>=dim)
+        switch (dim)
+        {
+          case 0:     
+            return make_shared<HalfSpacePML_Transformation<0>> (point,normal,alpha);
+
+          case 1:     
+            return make_shared<HalfSpacePML_Transformation<1>> (point,normal,alpha);
+          
+          case 2:
+            return make_shared<HalfSpacePML_Transformation<2>> (point,normal,alpha);
+          
+          case 3:
+            return make_shared<HalfSpacePML_Transformation<3>> (point,normal,alpha);
+
+          default:
+            throw Exception ("HalfSpacePML_Transformation::CreateDim: No valid Dimension");
+
+        }
+      else
+        throw Exception("HalfSpacePML_Transformation::CreateDim: not enough bounds");
+    }
+
+    void MapPoint (Vec<DIM> & hpoint, Vec<DIM,Complex> & out,
+                    Mat<DIM,DIM,Complex> & jac) const 
+    {
+      out = hpoint;
+      jac = Id<DIM>();
+      double dot = InnerProduct(hpoint-dimpoint,dimnormal); //dimnormal has already norm 1
+      if (dot>0)
+      {
+        out += alpha*dot*dimnormal;
+        for (int i : Range(DIM))
+          jac(i,i)+= alpha*dimnormal(i);        
+      }
+    }
+  };
+  template <int DIM>
   class BrickRadialPML_Transformation : public PML_TransformationDim<DIM>
   {
     Matrix<double> bounds;
     Complex alpha;
+    Vector<double> origin;
+    Vec<DIM> dimorigin;
     public:
-
-    BrickRadialPML_Transformation(Matrix<double> _bounds, Complex _alpha) 
-      : PML_TransformationDim<DIM>(), bounds(_bounds), alpha(_alpha) { ; }
+    BrickRadialPML_Transformation(FlatMatrix<double> _bounds, Complex _alpha, FlatVector<double> _origin) 
+      : PML_TransformationDim<DIM>(), bounds(_bounds), alpha(_alpha), origin(_origin) 
+    {
+      if (origin.Size()>=DIM)
+        for (int i : Range(DIM))
+          dimorigin(i)=origin(i);
+    }
     
     ~BrickRadialPML_Transformation() {;}
 
@@ -287,31 +372,32 @@ namespace ngcomp
     {
       cout << "alpha: " << alpha << endl;
       cout << "bounds: " << bounds << endl;
+      cout << "origin: " << origin << endl;
     }
 
     shared_ptr<PML_Transformation> CreateDim(int dim)
     {
-      if (bounds.Height()>=dim)
+      if (bounds.Height()>=dim && origin.Size()>=dim)
         switch (dim)
         {
           case 0:     
-            return make_shared<BrickRadialPML_Transformation<0>> (bounds,alpha);
+            return make_shared<BrickRadialPML_Transformation<0>> (bounds,alpha,origin);
 
           case 1:     
-            return make_shared<BrickRadialPML_Transformation<1>> (bounds,alpha);
+            return make_shared<BrickRadialPML_Transformation<1>> (bounds,alpha,origin);
           
           case 2:
-            return make_shared<BrickRadialPML_Transformation<2>> (bounds,alpha);
+            return make_shared<BrickRadialPML_Transformation<2>> (bounds,alpha,origin);
           
           case 3:
-            return make_shared<BrickRadialPML_Transformation<3>> (bounds,alpha);
+            return make_shared<BrickRadialPML_Transformation<3>> (bounds,alpha,origin);
 
           default:
             throw Exception ("BrickRadialPML_Transformation::CreateDim: No valid Dimension");
 
         }
       else
-        throw Exception("BrickRadialPML_Transformation::CreateDim: not enough bounds")
+        throw Exception("BrickRadialPML_Transformation::CreateDim: not enough bounds");
     }
 
     void MapPoint (Vec<DIM> & hpoint, Vec<DIM,Complex> & point,
@@ -321,12 +407,13 @@ namespace ngcomp
       double tmp = 0;
       double scal = 0;
       int maxind = 0;
+      Vec<DIM> rel_point = hpoint - origin;
       for (int j : Range(DIM))
       {
           if (hpoint(j)<bounds(j,0))
-            tmp=(hpoint(j)-bounds(j,0))/hpoint(j);
+            tmp=(hpoint(j)-bounds(j,0))/rel_point(j);
           else if (hpoint(j)>bounds(j,1))
-            tmp=(hpoint(j)-bounds(j,1))/hpoint(j);
+            tmp=(hpoint(j)-bounds(j,1))/rel_point(j);
           if (tmp>scal)
           {
             scal=tmp;
@@ -334,9 +421,9 @@ namespace ngcomp
           }
       }
       Vec<DIM> tmpvec;
-      tmpvec[maxind]=1/hpoint(maxind)-scal/hpoint(maxind);
-      point += alpha*scal*hpoint;
-      jac =(1. + alpha * scal)*Id<DIM>() + alpha * hpoint * Trans(tmpvec);
+      tmpvec[maxind]=1/rel_point(maxind)-scal/rel_point(maxind);
+      point += alpha*scal*rel_point;
+      jac =(1. + alpha * scal)*Id<DIM>() + alpha * rel_point * Trans(tmpvec);
     }
   };
 
