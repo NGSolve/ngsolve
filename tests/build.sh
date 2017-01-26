9efeef13fc82db879a7e1e6285762bc5b912d9cc
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2017-01-26 16:24:53 +0100

    Include code for package generation, update Netgen
    This commit provides the CMake-code to generate binary distribution
    packages for Windows, MacOS and Ubuntu. These packages are generated
    during automated builds in gitlab-ci and are uploaded to our server
    (vector).
    It also provides the 'deploy' job to upload the generated packages to
    ngsolve.org (not properly tested yet).

--- tests/build.sh
+++ tests/build.sh
@@ -4,6 +4,18 @@ git submodule update --init --recursive
 cd
 mkdir -p build/ngsolve
 cd build/ngsolve
-cmake ../../src/ngsolve -DUSE_CCACHE=ON -DUSE_MKL=ON -DUSE_UMFPACK=ON
+cmake ../../src/ngsolve -DUSE_CCACHE=ON -DUSE_MKL=ON -DUSE_UMFPACK=ON -DINSTALL_DEPENDENCIES=ON -DMKL_STATIC=ON
 make -j12
 make install
+make package
+
+# Run ssh-agent (inside the build environment)
+eval $(ssh-agent -s)
+# Add the SSH key stored in SSH_PRIVATE_KEY variable to the agent store
+ssh-add <(echo "$SSH_PRIVATE_KEY")
+mkdir -p ~/.ssh
+[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+
+ssh tester@vector.asc.tuwien.ac.at "mkdir -p /home/tester/deploy/ubuntu/$CI_BUILD_REF/$UBUNTU_VERSION_NAME"
+scp *.deb tester@vector.asc.tuwien.ac.at:/home/tester/deploy/ubuntu/$CI_BUILD_REF/$UBUNTU_VERSION_NAME/
+        
