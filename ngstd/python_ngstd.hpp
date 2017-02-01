f519ac310cc1caac5b4ed8cc4d6142881fe9097a
Christopher Lackner <christopher.lackner@gmx.at>
2017-02-01 21:00:45 +0100

    use environment variable to set docu string, revert some old changes

--- ngstd/python_ngstd.hpp
+++ ngstd/python_ngstd.hpp
@@ -397,7 +397,6 @@ template <> inline void PyExportSymbolTable<shared_ptr<double>, shared_ptr<doubl
 }
 
 // replace docu links with plain text for help function
-extern bool rst_docu;
 const char* docu_string(const char* str);
 
 PYBIND11_DECLARE_HOLDER_TYPE(T, std::shared_ptr<T>);
