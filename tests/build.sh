60b2209241b38a60627f898159eb0ecf45d48e29
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2017-02-16 16:26:21 +0100

    Some changes for deployment, update Netgen

--- tests/build.sh
+++ tests/build.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 cd
 mkdir -p build/ngsolve
 cd build/ngsolve
-cmake ../../src/ngsolve -DUSE_CCACHE=ON -DUSE_MKL=ON -DUSE_UMFPACK=ON -DINSTALL_DEPENDENCIES=ON -DMKL_STATIC=ON
+cmake ../../src/ngsolve -DUSE_CCACHE=ON -DUSE_MKL=ON -DUSE_UMFPACK=ON -DINSTALL_PROFILES=ON -DMKL_STATIC=ON -DCMAKE_BUILD_TYPE=Release
 make -j12
 make install
 make package
@@ -16,6 +16,6 @@ ssh-add <(echo "$SSH_PRIVATE_KEY")
 mkdir -p ~/.ssh
 [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
-ssh tester@vector.asc.tuwien.ac.at "mkdir -p /home/tester/deploy/ubuntu/$CI_BUILD_REF/$UBUNTU_VERSION_NAME"
-scp *.deb tester@vector.asc.tuwien.ac.at:/home/tester/deploy/ubuntu/$CI_BUILD_REF/$UBUNTU_VERSION_NAME/
+ssh tester@vector.asc.tuwien.ac.at "mkdir -p /home/tester/deploy/ubuntu/$CI_BUILD_REF/${UBUNTU_VERSION_NAME}_amd64"
+scp *.deb tester@vector.asc.tuwien.ac.at:/home/tester/deploy/ubuntu/$CI_BUILD_REF/${UBUNTU_VERSION_NAME}_amd64/
         
