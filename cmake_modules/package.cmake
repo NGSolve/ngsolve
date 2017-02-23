e46f7d1b303034f19ce72234cc13e23843192bca
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2017-02-23 11:52:18 +0100

    gitlab-ci: change package name ("_nightly" will be passed as PACKAGE_NAME_SUFFIX by nightly builds)

--- cmake_modules/package.cmake
+++ cmake_modules/package.cmake
@@ -3,19 +3,11 @@ set(CPACK_INSTALL_CMAKE_PROJECTS
   "${CMAKE_BINARY_DIR}/ngsolve;NGSolve;ALL;/"
 )
 
-if(NGSOLVE_VERSION_TWEAK)
-  # nightly build
-  set(CPACK_PACKAGE_VERSION ${NGSOLVE_VERSION_LONG})
-  set(CPACK_SOURCE_PACKAGE_FILE_NAME ngsolve_v${NGSOLVE_VERSION_LONG})
-  set(CPACK_DEBIAN_PACKAGE_NAME ngsolve_nightly)
-else(NGSOLVE_VERSION_TWEAK)
-  # release
-  set(CPACK_PACKAGE_NAME ngsolve)
-  set(CPACK_DEBIAN_PACKAGE_NAME ngsolve)
-  set(CPACK_PACKAGE_VERSION ${NGSOLVE_VERSION})
-  set(CPACK_SOURCE_PACKAGE_FILE_NAME ngsolve_v${NGSOLVE_VERSION})
-endif(NGSOLVE_VERSION_TWEAK)
+set(CPACK_PACKAGE_NAME ngsolve CACHE STRING "package name")
+set(CPACK_PACKAGE_VERSION ${NGSOLVE_VERSION} CACHE STRING "package version")
 
+set(CPACK_SOURCE_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}_v${NGSOLVE_VERSION} CACHE STRING "source package file name")
+set(CPACK_PACKAGE_FILE_NAME ${CPACK_PACKAGE_NAME}_${NGSOLVE_VERSION} CACHE STRING "package file name")
 
 # set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "automatic 3d tetrahedral mesh generator")
 # set(CPACK_PACKAGE_DESCRIPTION "NETGEN is an automatic 3d tetrahedral mesh generator. It accepts input from constructive solid geometry (CSG) or boundary representation (BRep) from STL file format. The connection to a geometry kernel allows the handling of IGES and STEP files. NETGEN contains modules for mesh optimization and hierarchical mesh refinement. Netgen is open source based on the LGPL license. It is available for Unix/Linux and Windows.")
