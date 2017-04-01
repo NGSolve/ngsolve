6ea3eea773c3c04b1a85e3a07a76dcc96d86d6a4
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2017-04-01 22:10:51 +0200

    element_types - next attempt for msvc

--- fem/elementtopology.hpp
+++ fem/elementtopology.hpp
@@ -25,10 +25,10 @@ namespace ngfem
 	ET_TET = 20, ET_PYRAMID = 21, ET_PRISM = 22, ET_HEX = 24 };
   
 
-  static constexpr auto element_types =
+  static constexpr initializer_list<ELEMENT_TYPE> element_types =
     { ET_POINT, ET_SEGM, ET_TRIG, ET_QUAD,
       ET_TET, ET_PYRAMID, ET_PRISM, ET_HEX };
-
+  
 
   /**
      Type of node. 
