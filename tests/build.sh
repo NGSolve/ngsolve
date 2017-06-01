d371fe264d269d08c2320e9f8b388dbee4bb90c8
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2017-06-01 14:38:12 +0200

    change install path, build and upload docu only on zesty

--- tests/build.sh
+++ tests/build.sh
@@ -1,23 +1,10 @@
+set -e
+
 if [ "$UBUNTU_VERSION_NAME" == "debug" ]
 then
   /root/src/ngsolve/tests/build_debug.sh
   exit
 fi
-
-set -e
-cd 
-cd src/ngsolve
-git submodule update --init --recursive
-cd
-mkdir -p build/ngsolve
-cd build/ngsolve
-cmake ../../src/ngsolve -DUSE_NATIVE_ARCH=OFF -DUSE_OCC=ON -DUSE_CCACHE=ON -DUSE_MKL=ON -DUSE_UMFPACK=ON -DINSTALL_PROFILES=OFF -DMKL_STATIC=ON -DENABLE_UNIT_TESTS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DCPACK_PACKAGING_INSTALL_PREFIX=/usr $CMAKE_ARGS
-make -j12
-make install
-make package
-cd ngsolve
-make docs
-cd ..
 
 # Run ssh-agent (inside the build environment)
 eval $(ssh-agent -s)
@@ -32,11 +19,39 @@ rsync -ztrl --del -e ssh \
   *.deb \
   gitlab-runner@vector.asc.tuwien.ac.at:$UPLOAD_DIR/
 
-if [ "yakkety" = "$UBUNTU_VERSION_NAME" ]
+cd 
+cd src/ngsolve
+git submodule update --init --recursive
+cd
+mkdir -p build/ngsolve
+cd build/ngsolve
+cmake ../../src/ngsolve \
+  -DUSE_NATIVE_ARCH=OFF \
+  -DUSE_OCC=ON \
+  -DUSE_CCACHE=ON \
+  -DUSE_MKL=ON \
+  -DUSE_UMFPACK=ON \
+  -DINSTALL_PROFILES=OFF \
+  -DMKL_STATIC=ON \
+  -DENABLE_UNIT_TESTS=ON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DNG_INSTALL_DIR_LIB=lib/netgen \
+  -DNG_INSTALL_DIR_INCLUDE=include/netgen \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCPACK_PACKAGING_INSTALL_PREFIX=/usr \
+  $CMAKE_ARGS
+
+make -j12
+make install
+make package
+cd ngsolve
+if [ "zesty" = "$UBUNTU_VERSION_NAME" ]
 then
+  echo "build docu"
+  make docs
   echo "upload docu"
   rsync -ztrl --del -e ssh \
-    ngsolve/docs/html/* \
+    docs/html/* \
     gitlab-runner@vector.asc.tuwien.ac.at:deploy/builds/$CI_PIPELINE_ID/docu/
 fi
 
