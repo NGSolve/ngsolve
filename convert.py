c82dc1f06e75614ef63e00a3b0de1493b081fb26
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2017-06-21 10:51:31 +0200

    copy htmls to own folder and zip ipynb files

--- convert.py
+++ convert.py
@@ -14,3 +14,16 @@ for p in procs:
 # replace links to .ipynb files by .html
 for f in glob.iglob('**/*.html', recursive=True):
     subprocess.run(['sed','-i','s/\.ipynb/\.html/g',f])
+
+# create dir to copy html files to
+subprocess.run(['mkdir','../html_i-tutorials'])
+
+# copy all html files to dir for upload
+htmls = [f for f in glob.iglob('**/*.html', recursive=True)]
+print(htmls)
+for f in htmls:
+    subprocess.run(['cp',f,'--parents','../html_i-tutorials'])
+
+# zip all ipynb files
+nbs = [f for f in glob.iglob('**/*.ipynb', recursive=True)]
+subprocess.run(['zip','../html_i-tutorials/i-tutorials.zip',*nbs])
