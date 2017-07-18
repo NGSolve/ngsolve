ef31ba46fcc141b1b655f76f25d11d176a08ae0b
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2017-07-18 09:35:24 +0200

    next trial for MSVC

--- comp/h1hofespace.cpp
+++ comp/h1hofespace.cpp
@@ -726,11 +726,14 @@ namespace ngcomp
                  Ngs_Element ngel = ma->GetElement<et.DIM,BND> (ei.Nr());
 
                  // auto hofe =  new (alloc) H1HighOrderFE<et> ();
+                 constexpr ELEMENT_TYPE ET = et.ElementType();
+                 auto hofe =  new (alloc) H1HighOrderFE<ET> ();
+                 /*
                  auto hofe =  new (alloc)
                    H1HighOrderFE<et.ElementType(),
                                  H1HighOrderFE_Shape<et.ElementType()>,
                                  T_ScalarFiniteElement< H1HighOrderFE_Shape<et.ElementType()>,et.ElementType()> >();
-    
+                 */
                  hofe -> SetVertexNumbers (ngel.vertices);
 
                  if (et.DIM >= 1)
