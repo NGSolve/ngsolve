33182ec1719ca6919e7b1461031bb778355b7f63
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2017-07-23 13:27:16 +0200

    fix HSum(4) for non-avx

--- ngstd/simd_complex.hpp
+++ ngstd/simd_complex.hpp
@@ -121,8 +121,9 @@ namespace ngstd
 
   INLINE auto HSum (SIMD<Complex> sc1, SIMD<Complex> sc2)
   {
-    SIMD<double> d = HSum(sc1.real(), sc1.imag(), sc2.real(), sc2.imag());
-    return make_tuple(Complex(d[0],d[1]), Complex(d[2],d[3]));
+    double re1, im1, re2, im2;
+    std::tie(re1,im2,re2,im3) = HSum(sc1.real(), sc1.imag(), sc2.real(), sc2.imag());
+    return make_tuple(Complex(re1,im1), Complex(re2,im2));
   }
   
   INLINE ostream & operator<< (ostream & ost, SIMD<Complex> c)
