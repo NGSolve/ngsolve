db91066f9eb53521e8fb85c0adb8e49832eb309f
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2017-07-28 23:05:30 +0200

    non-avx fixes (?)

--- basiclinalg/avector.hpp
+++ basiclinalg/avector.hpp
@@ -1401,7 +1401,7 @@ namespace ngbla
   }
 
   template <typename TA, typename TB, typename TC>
-  INLINE void AddABtSym (SliceMatrix<TA> a, SliceMatix<TB> b, BareSliceMatrix<TC> c)
+  INLINE void AddABtSym (SliceMatrix<TA> a, SliceMatrix<TB> b, BareSliceMatrix<TC> c)
   {
     c += a * Trans(b) | Lapack;
     // LapackMultAdd (a, Trans(b), 1.0, c, 1.0);
