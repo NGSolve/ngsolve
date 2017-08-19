7bf3fd31b960c56712a92fced2025ae91327ad1b
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2017-08-19 15:28:07 +0200

    parallel zeroing

--- comp/fespace.cpp
+++ comp/fespace.cpp
@@ -557,7 +557,10 @@ lot of new non-zero entries in the matrix!\n" << endl;
 
         while (found < cnt)
           {
-            mask = 0;
+            // mask = 0;
+            ParallelForRange
+              (mask.Size(), [&] (IntRange myrange)
+               { for (auto i : myrange) mask[i] = 0; });
 
             size_t ne = ma->GetNE(vb);
 
