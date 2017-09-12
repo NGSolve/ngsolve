19b25b9dca57b6117a783d282d56c7e222b01cbb
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2017-09-12 14:29:25 +0200

    [code generation] Fix MeshSizeCF
     - Fix MeshSizeCF::GenerateCode() to take into account boundary evaluation
     - In addition to adding tests, adds a parameter 'wait' to
       CoefficientFunction::Compiler() to block execution until compilation
       is finished.
     - Increase timeout for pytests due to compile time

--- tests/pytest/test_coefficient.py
+++ tests/pytest/test_coefficient.py
@@ -1,7 +1,31 @@
 import pytest
-import ngsolve
+from netgen.geom2d import unit_square
+from netgen.csg import unit_cube
+from ngsolve import *
 
 def test_ParameterCF():
-    p = ngsolve.fem.Parameter(23)
-    assert type(p) == ngsolve.fem.Parameter
+    p = Parameter(23)
+    assert type(p) == Parameter
                     
+def test_mesh_size_cf():
+    for mesh in [ Mesh(unit_cube.GenerateMesh(maxh=0.2)), Mesh(unit_square.GenerateMesh(maxh=0.2))]:
+        dim = mesh.dim
+        fes = L2(mesh, order=0)
+        gfu = GridFunction(fes)
+        cf = specialcf.mesh_size 
+        if dim==2:
+            gfu.Set(cf*cf/2)
+        else:
+            gfu.Set(cf*cf*cf/6)
+        assert abs( sum(gfu.vec)-1 ) < 1e-12
+
+        bfi = BilinearForm(fes)
+        bfi += SymbolicBFI( fes.TrialFunction()*(specialcf.mesh_size - specialcf.mesh_size.Compile(True, wait=True))*fes.TestFunction(), element_boundary=True)
+        bfi.Assemble()
+        v = bfi.mat.AsVector().FV()
+        for val in v:
+            assert abs(val) < 1e-14
+
+if __name__ == "__main__":
+    test_ParameterCF()
+    test_mesh_size_cf()
