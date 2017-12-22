02da723717aaf0764d3b672cf0fd5d4894aad1c4
Michael Neunteufel <michael@michael-ThinkPad-T460p.home>
2017-12-22 13:05:04 +0100

    preparation for merge part2

--- fem/scalarfe.cpp
+++ fem/scalarfe.cpp
@@ -411,12 +411,14 @@ namespace ngfem
                                               FlatMatrix<> ddshape) const
   {
     int nd = GetNDof();
-    int sdim = D;
+    double eps = 1e-3;
 
-    double eps = 1e-7;
-    Matrix<> dshape1(nd, sdim), dshape2(nd, sdim);
+    STACK_ARRAY(double, mem1, nd*D);
+    STACK_ARRAY(double, mem2, nd*D);    
+    FlatMatrixFixWidth<D> dshape1(nd, &mem1[0]);  
+    FlatMatrixFixWidth<D> dshape2(nd, &mem2[0]);
 
-    for (int i = 0; i < sdim; i++)
+    for (int i = 0; i < D; i++)
       {
 	IntegrationPoint ip1 = ip;
 	IntegrationPoint ip2 = ip;
@@ -426,11 +428,27 @@ namespace ngfem
 
 	CalcDShape (ip1, dshape1);
 	CalcDShape (ip2, dshape2);
-	dshape2 -= dshape1;
-	dshape2 *= (0.5 / eps);
+
 	for (int j = 0; j < nd; j++)
-	  for (int k = 0; k < sdim; k++)
-	    ddshape(j,sdim*i+k) = dshape2(j,k);
+	  for (int k = 0; k < D; k++)
+	    // ddshape(j,D*i+k) = (0.5/eps) * (dshape2(j,k) - dshape1(j,k));
+            ddshape(j,D*i+k) = (2.0/(3.0*eps)) * (dshape2(j,k) - dshape1(j,k));
+      }
+    
+    for (int i = 0; i < D; i++)
+      {
+	IntegrationPoint ip1 = ip;
+	IntegrationPoint ip2 = ip;
+        
+        ip1(i) -= 2*eps;
+        ip2(i) += 2*eps;
+
+	CalcDShape (ip1, dshape1);
+	CalcDShape (ip2, dshape2);
+
+	for (int j = 0; j < nd; j++)
+	  for (int k = 0; k < D; k++)
+	    ddshape(j,D*i+k) -= (1.0/(12*eps)) * (dshape2(j,k) - dshape1(j,k));
       }  
   }
 
