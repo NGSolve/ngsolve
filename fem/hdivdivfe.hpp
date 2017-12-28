d6f973451dda218121df2728b69f58231490d355
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2017-12-28 10:22:43 +0100

    better capture ?

--- fem/hdivdivfe.hpp
+++ fem/hdivdivfe.hpp
@@ -272,21 +272,40 @@ namespace ngfem
                  {
                    auto jac = mir[i].GetJacobian();
                    auto d2 = sqr(mir[i].GetJacobiDet());
+
+                   Vec<DIM_STRESS,SIMD<double>> hv;
+                   Mat<DIM,DIM,SIMD<double>> mat;
+                   Mat<DIMSPACE*DIMSPACE, DIM_STRESS,SIMD<double>> trans;
+                   for (int i = 0; i < DIM_STRESS; i++)
+                     {
+                       hv = SIMD<double>(0.0);
+                       hv(i) = SIMD<double>(1.0);
+                       VecToSymMat<DIM> (hv, mat);
+                       Mat<DIMSPACE,DIMSPACE,SIMD<double>> physmat =
+                         1/d2 * (jac * mat * Trans(jac));
+                       for (int j = 0; j < DIMSPACE*DIMSPACE; j++)
+                         trans(j,i) = physmat(j);
+                     }
                    
           
                    Vec<DIM,AutoDiff<DIM,SIMD<double>>> adp = bmir.IR()[i];
                    TIP<DIM,AutoDiffDiff<DIM,SIMD<double>>> addp(adp);
 
                    this->Cast() -> T_CalcShape (addp,
-                                                SBLambda ([i,shapes,jac,d2] (size_t j, auto val)
+                                                SBLambda ([i,shapes,trans] (size_t j, auto val)
                                                     {
+                                                      /*
                                                       Mat<DIM,DIM,SIMD<double>> mat;
                                                       VecToSymMat<DIM> (val.Shape(), mat);
                                                       Mat<DIMSPACE,DIMSPACE,SIMD<double>> physmat =
                                                         1/d2 * (jac * mat * Trans(jac));
-
                                                       for (size_t k = 0; k < sqr(DIMSPACE); k++)
                                                         shapes(j*sqr(DIMSPACE)+k,i) = physmat(k);
+                                                      */
+                                                      Vec<DIMSPACE*DIMSPACE,SIMD<double>> transvec;
+                                                      transvec = trans * val.Shape();
+                                                      for (size_t k = 0; k < sqr(DIMSPACE); k++)
+                                                        shapes(j*sqr(DIMSPACE)+k,i) = transvec(k);
                                                     }));
                  }
              }
