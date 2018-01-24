59bc84a89a1818d420e1974615d70ec48df6e905
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2018-01-24 08:36:31 +0100

    performance fix: Vector -> FlatVector in Integrate

--- tests/pytest/test_code_generation.py
+++ tests/pytest/test_code_generation.py
@@ -43,14 +43,15 @@ def test_code_generation_volume_terms_complex():
 
     for cf in functions:
         gfu.Set(cf)
+        print ("norm gfu.vec = ", Norm(gfu.vec))
         # should all give the same results
         cfs = [ cf.Compile(), cf.Compile(True, wait=True), gfu, gfu.Compile(), gfu.Compile(True, wait=True) ]
 
         for f in cfs:
-            print (Integrate(f, mesh))
-            print (Integrate(cf, mesh))
-            print (Integrate(cf-f, mesh))
-            print (Integrate(Conj(cf-f), mesh))
+            print ("integral f =", Integrate(f, mesh))
+            print ("integral cf =", Integrate(cf, mesh))
+            print ("integral cf-f =", Integrate(cf-f, mesh))
+            print ("integral Conj(cf-f) =", Integrate(Conj(cf-f), mesh))
             error = (cf-f)*Conj(cf-f)
             assert (abs(Integrate ( error, mesh))<1e-13)
 
