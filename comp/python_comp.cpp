67cfd7c1252549ed7624fb3c6e50a821681f341d
Michael Neunteufel <michael@michael-ThinkPad-T460p.home>
2018-01-30 14:35:56 +0100

    added vectorfacet to python_comp

--- comp/python_comp.cpp
+++ comp/python_comp.cpp
@@ -1942,6 +1942,29 @@ kwargs : For a description of the possible kwargs have a look a bit further down
                   return flags_doc;
                 })
     ;
+
+  py::class_<VectorFacetFESpace, shared_ptr<VectorFacetFESpace>,FESpace>
+    (m, "VectorFacet")
+    .def("__init__", [] (py::object self, shared_ptr<MeshAccess> ma, py::kwargs kwargs)
+         {
+           auto myclass = self.attr("__class__");
+           py::list info;
+           info.append(ma);
+           auto flags = CreateFlagsFromKwArgs(myclass, kwargs, info);
+           auto instance = py::cast<VectorFacetFESpace*>(self);
+           new (instance) VectorFacetFESpace(ma, flags);
+           self.attr("__initialize__")(**kwargs);
+         })
+    .def(py::pickle(fesPickle,(shared_ptr<VectorFacetFESpace>(*)(py::tuple))
+                    fesUnpickle<VectorFacetFESpace>))
+    .def_static("__flags_doc__", [] ()
+                {
+                  auto flags_doc = py::cast<py::dict>(py::module::import("ngsolve").
+                                                  attr("FESpace").
+                                                  attr("__flags_doc__")());
+                  return flags_doc;
+                })
+    ;
   
   py::class_<HDivHighOrderSurfaceFESpace, shared_ptr<HDivHighOrderSurfaceFESpace>,FESpace>
     (m, "HDivSurface")
