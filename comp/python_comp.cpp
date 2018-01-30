8659fdb71cb29ca615a7a0ae7ade8210bebbe38b
Michael Neunteufel <michael@michael-ThinkPad-T460p.home>
2018-01-30 14:39:30 +0100

    added facetspace to python_comp

--- comp/python_comp.cpp
+++ comp/python_comp.cpp
@@ -1965,6 +1965,29 @@ kwargs : For a description of the possible kwargs have a look a bit further down
                   return flags_doc;
                 })
     ;
+
+  py::class_<FacetFESpace, shared_ptr<FacetFESpace>,FESpace>
+    (m, "FacetFESpace")
+    .def("__init__", [] (py::object self, shared_ptr<MeshAccess> ma, py::kwargs kwargs)
+         {
+           auto myclass = self.attr("__class__");
+           py::list info;
+           info.append(ma);
+           auto flags = CreateFlagsFromKwArgs(myclass, kwargs, info);
+           auto instance = py::cast<FacetFESpace*>(self);
+           new (instance) FacetFESpace(ma, flags);
+           self.attr("__initialize__")(**kwargs);
+         })
+    .def(py::pickle(fesPickle,(shared_ptr<FacetFESpace>(*)(py::tuple))
+                    fesUnpickle<FacetFESpace>))
+    .def_static("__flags_doc__", [] ()
+                {
+                  auto flags_doc = py::cast<py::dict>(py::module::import("ngsolve").
+                                                  attr("FESpace").
+                                                  attr("__flags_doc__")());
+                  return flags_doc;
+                })
+    ;
   
   py::class_<HDivHighOrderSurfaceFESpace, shared_ptr<HDivHighOrderSurfaceFESpace>,FESpace>
     (m, "HDivSurface")
