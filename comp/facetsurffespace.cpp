3950c2817afa3997ed96921666f88aa3124294ec
Michael Neunteufel <michael@michael-ThinkPad-T460p.home>
2018-01-30 13:33:32 +0100

    fix

--- comp/facetsurffespace.cpp
+++ comp/facetsurffespace.cpp
@@ -118,7 +118,7 @@ namespace ngcomp
     : FESpace(ama, flags)
   {
     name="FacetSurfaceFESpace(facet)";
-    type = "facetsurface"
+    type = "facetsurface";
     // defined flags
     DefineNumFlag("relorder");
     DefineDefineFlag("variableorder"); 
