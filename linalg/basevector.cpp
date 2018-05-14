4b60a850bc3d99f98e7601425d1bedeeb8c62c18
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2018-05-14 10:52:14 +0200

    BlocVector/Matrix

--- linalg/basevector.cpp
+++ linalg/basevector.cpp
@@ -687,6 +687,114 @@ namespace ngla
 
 
 
+  BlockVector & dynamic_cast_BlockVector (BaseVector & x)
+  {
+    AutoVector * ax = dynamic_cast<AutoVector*> (&x);
+    if (ax) return dynamic_cast<BlockVector&> (**ax);
+    return dynamic_cast<BlockVector&> (x);
+  }
+  
+  const BlockVector & dynamic_cast_BlockVector (const BaseVector & x)
+  {
+    const AutoVector * ax = dynamic_cast<const AutoVector*> (&x);
+    if (ax) return dynamic_cast<const BlockVector&> (**ax);
+    return dynamic_cast<const BlockVector&> (x);
+  }
+
+
+  BlockVector :: BlockVector (const Array<shared_ptr<BaseVector>> & avecs)
+    : vecs(avecs), ispar(vecs.Size())
+  {
+    ispar.Clear();
+    
+    size = 0;
+    for(auto & vec:vecs)
+      size += vec->Size();
+    
+    for(size_t k = 0; k<vecs.Size(); k++) {
+      auto stat = vecs[k]->GetParallelStatus();
+      if( (stat==CUMULATED) || (stat==DISTRIBUTED) )
+        ispar.Set(k);
+    }
+    
+  }
+
+  void * BlockVector :: Memory () const
+  { throw Exception("BlockVector::Memory is not useful"); }
+  FlatVector<double> BlockVector :: FVDouble () const 
+  { throw Exception("BlockVector::FVDouble is not useful"); }
+  FlatVector<Complex> BlockVector :: FVComplex () const
+  { throw Exception("BlockVector::FVComplex is not useful"); }
+  void BlockVector :: GetIndirect (FlatArray<int> ind, 
+                                   FlatVector<double> v) const
+  { throw Exception("BlockVector::GetIndirect is not useful"); }      
+  void BlockVector :: GetIndirect (FlatArray<int> ind, 
+                                   FlatVector<Complex> v) const 
+  { throw Exception("BlockVector::GetIndirect is not useful"); }      
+  
+  // not yet implemented properly for complex components!!
+  bool BlockVector :: IsComplex() const
+  { return false; }
+  
+  AutoVector BlockVector :: CreateVector () const
+  {
+    Array<shared_ptr<BaseVector>> v2;
+    for (auto & v : vecs)
+      v2 += v->CreateVector();
+    return make_shared<BlockVector> (v2);
+  }
+  
+  double BlockVector :: InnerProductD (const BaseVector & v2) const
+  {
+    double pp = 0;
+    double ps = 0;
+    const auto & v2b = dynamic_cast_BlockVector(v2);
+    for(size_t k = 0; k<vecs.Size(); k++) {
+      auto p = vecs[k]->InnerProductD(*v2b[k]);
+      if(ispar.Test(k)) pp += p;
+      else ps += p;
+    }
+    return pp+MyMPI_AllReduce(ps);
+  }
+  
+  BaseVector & BlockVector :: Scale (double scal)
+  {
+    for (auto i : ::Range(vecs))
+      *vecs[i] *= scal;
+    return *this;
+  }
+  
+  BaseVector & BlockVector :: SetScalar (double scal)
+  {
+    for (auto i : ::Range(vecs))
+      vecs[i]->SetScalar(scal);
+    return *this;
+  }
+  
+  ostream & BlockVector :: Print (ostream & ost) const
+  {
+    for (auto i : ::Range(vecs))
+      vecs[i] -> Print(ost);
+    return ost;
+  }
+  
+  BaseVector & BlockVector :: Set (double scal, const BaseVector & v)
+  {
+    auto & bv = dynamic_cast_BlockVector(v);
+    for (size_t i : ::Range(vecs))
+      vecs[i] -> Set(scal, *bv[i]);
+    return *this;
+  }
+  
+  BaseVector & BlockVector :: Add (double scal, const BaseVector & v)
+  {
+    auto & bv = dynamic_cast_BlockVector(v);
+    for (size_t i : ::Range(vecs))
+      vecs[i] -> Add(scal, *bv[i]);
+    return *this;
+  }
+  
+  
 
 
   template <typename TSCAL>
