057656db9e4291ce994c236701ba23d87b46b083
Lukas <lukas.kogler@tuwien.ac.at>
2018-06-15 17:16:04 +0200

    superimportant blockvector fix

--- linalg/basevector.cpp
+++ linalg/basevector.cpp
@@ -727,6 +727,7 @@ namespace ngla
     for (size_t k = 0; k<vecs.Size(); k++) {
       auto stat = vecs[k]->GetParallelStatus();
       if ( stat==NOT_PARALLEL ) continue;
+      ispar.Set(k);
       auto * pv = dynamic_cast_ParallelBaseVector(vecs[k].get());
       auto vcomm = pv->GetParallelDofs()->GetCommunicator();
       if (comm==MPI_COMM_NULL)
