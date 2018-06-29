4e97177d51deb4b6708d50002dad145f226594de
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-06-29 06:34:18 -0700

    cleanup

--- tests/gitlab-ci/win/set_vars.bat
+++ tests/gitlab-ci/win/set_vars.bat
@@ -1,4 +1,3 @@
-call "%VS140COMNTOOLS%\..\..\VC\vcvarsall" x64
 set CMAKE_GENERATOR=Ninja
 set NETGEN_BUILD_DIR=%CI_DIR%\build
 set CMAKE_INSTALL_PREFIX=%CI_DIR%\install
--- tests/gitlab-ci/win/set_vars_avx.bat
+++
@@ -1,9 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall" x64
-set CMAKE_GENERATOR=Ninja
-set NETGEN_BUILD_DIR=%CI_DIR%\build
-set CMAKE_INSTALL_PREFIX=%CI_DIR%\install
-set NETGENDIR=%CMAKE_INSTALL_PREFIX%\bin
-set PATH=%NETGENDIR%;%PATH%
-set PYTHONPATH=%CMAKE_INSTALL_PREFIX%\lib\site-packages
-set CLCACHE_BASEDIR=%CI_DIR%
-set CMAKE_CONFIG=%CMAKE_CONFIG% -DUSE_OCC=ON -DUSE_CCACHE=ON -DUSE_MKL=ON -DMKL_STATIC=ON -DMKL_ROOT="C:/Intel/compilers_and_libraries/windows/mkl"
