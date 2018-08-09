dd5ca4ca8426d03bc07b4ec5eb4702641c962107
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2018-08-09 17:10:20 +0200

    simplify examples

--- py_tutorials/navierstokes.py
+++ py_tutorials/navierstokes.py
@@ -7,7 +7,6 @@ nu = 0.001
 tau = 0.001
 tend = 10
 
-# mesh = Mesh("cylinder.vol")
 from netgen.geom2d import SplineGeometry
 geo = SplineGeometry()
 geo.AddRectangle( (0, 0), (2, 0.41), bcs = ("wall", "outlet", "wall", "inlet"))
@@ -16,18 +15,15 @@ mesh = Mesh( geo.GenerateMesh(maxh=0.08))
 
 mesh.Curve(3)
 
-V = H1(mesh,order=3, dirichlet="wall|cyl|inlet")
+V = VectorH1(mesh,order=3, dirichlet="wall|cyl|inlet")
 Q = H1(mesh,order=2)
 
-X = FESpace([V,V,Q])
+X = FESpace([V,Q])
 
-ux,uy,p = X.TrialFunction()
-vx,vy,q = X.TestFunction()
+u,p = X.TrialFunction()
+v,q = X.TestFunction()
 
-div_u = grad(ux)[0]+grad(uy)[1]
-div_v = grad(vx)[0]+grad(vy)[1]
-
-stokes = nu*grad(ux)*grad(vx)+nu*grad(uy)*grad(vy)+div_u*q+div_v*p - 1e-10*p*q
+stokes = nu*InnerProduct(grad(u), grad(v))+div(u)*q+div(v)*p - 1e-10*p*q
 a = BilinearForm(X)
 a += SymbolicBFI(stokes)
 a.Assemble()
@@ -39,8 +35,8 @@ f.Assemble()
 # gridfunction for the solution
 gfu = GridFunction(X)
 
-# parabolic inflow at bc=1:
-uin = 1.5*4*y*(0.41-y)/(0.41*0.41)
+# parabolic inflow at inlet:
+uin = CoefficientFunction( (1.5*4*y*(0.41-y)/(0.41*0.41), 0) )
 gfu.components[0].Set(uin, definedon=mesh.Boundaries("inlet"))
 
 # solve Stokes problem for initial conditions:
@@ -53,17 +49,16 @@ gfu.vec.data += inv_stokes * res
 
 # matrix for implicit Euler 
 mstar = BilinearForm(X)
-mstar += SymbolicBFI(ux*vx+uy*vy + tau*stokes)
+mstar += SymbolicBFI(u*v + tau*stokes)
 mstar.Assemble()
 inv = mstar.mat.Inverse(X.FreeDofs(), inverse="sparsecholesky")
 
 # the non-linear term 
 conv = BilinearForm(X, nonassemble = True)
-conv += SymbolicBFI( CoefficientFunction( (ux,uy) ) * (grad(ux)*vx+grad(uy)*vy) )
+conv += SymbolicBFI( (grad(u) * u) * v)
 
 # for visualization
-velocity = CoefficientFunction (gfu.components[0:2])
-Draw (Norm(velocity), mesh, "velocity", sd=3)
+Draw (Norm(gfu.components[0]), mesh, "velocity", sd=3)
 
 # implicit Euler/explicit Euler splitting method:
 t = 0
