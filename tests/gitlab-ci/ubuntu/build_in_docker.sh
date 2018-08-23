dd92ca407eee196469fe8056f54f91d6ea071b29
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2018-08-23 13:51:34 +0200

    copy jupyter files and zipped i-tutorials to upload folder, fix download links

--- tests/gitlab-ci/ubuntu/build_in_docker.sh
+++ tests/gitlab-ci/ubuntu/build_in_docker.sh
@@ -68,6 +68,9 @@ then
   then
     echo "build docu"
     make docs
+    find ~/src/ngsolve/docs/i-tutorials -name '*.ipynb' -print0 | xargs -0 nbstripout
+    cp -r ~/src/ngsolve/docs/i-tutorials docs/html/jupyter-files
+    zip -r docs/html/i-tutorials.zip docs/html/jupyter-files
     echo "upload docu"
     rsync -ztrl --del -e ssh \
       --rsync-path="mkdir -p deploy/builds/$CI_PIPELINE_ID/docu/ && rsync" \
