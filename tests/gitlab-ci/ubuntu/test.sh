9f6235b7ca275007cfb04e6b138e6e367462cf86
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-09-10 12:22:38 +0200

    [gitlab-ci] Some cleanup

--- tests/gitlab-ci/ubuntu/test.sh
+++ tests/gitlab-ci/ubuntu/test.sh
@@ -7,5 +7,5 @@ docker run \
       -e MKL_DOMAIN_THREADS="MKL_DOMAIN_PARDISO=6" \
       -e LD_LIBRARY_PATH=/opt/intel/mkl/lib/intel64 \
       -v /opt/intel:/opt/intel \
-      ngsolve_${CI_PIPELINE_ID}_installed:$IMAGE_VERSION \
+      ngsolve_${CI_PIPELINE_ID}_installed:$IMAGE_NAME \
       bash -c 'cd /root/build/ngsolve && make test_ngsolve ARGS="--output-on-failure"'
