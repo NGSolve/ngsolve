844ab57667ddcafabfef6a5a2db5127f97f21a38
Michael Neunteufel <michael.neunteufel@tuwien.ac.at>
2018-09-13 18:58:39 +0200

    added first_update and set/get order

--- comp/hdivhofespace.hpp
+++ comp/hdivhofespace.hpp
@@ -65,7 +65,10 @@ namespace ngcomp
 
     virtual void UpdateDofTables() override;  
 
-    virtual void UpdateCouplingDofArray() override;   
+    virtual void UpdateCouplingDofArray() override;
+
+    virtual void SetOrder (NodeId ni, int order) override;
+    virtual int GetOrder (NodeId ni) const override;
     
     virtual string GetClassName () const override
     {
