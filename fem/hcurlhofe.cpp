35443cf42083a3d4bce7fd357da96b7107cadeb6
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2018-09-25 18:44:31 +0200

    no iterate here ...

--- fem/hcurlhofe.cpp
+++ fem/hcurlhofe.cpp
@@ -12,16 +12,17 @@
 namespace ngfem
 {
   // template class HCurlHighOrderFE<ET_POINT>;
-  template class HCurlHighOrderFE<ET_SEGM>;
+  // template class HCurlHighOrderFE<ET_SEGM>;
   template class HCurlHighOrderFE<ET_TRIG>;
-  template class HCurlHighOrderFE<ET_QUAD>;
+  // template class HCurlHighOrderFE<ET_QUAD>;
 
   // template class
   // T_HCurlHighOrderFiniteElement<ET_POINT, HCurlHighOrderFE_Shape<ET_POINT>>;
-  template class
-  T_HCurlHighOrderFiniteElement<ET_SEGM, HCurlHighOrderFE_Shape<ET_SEGM>>;
+  // template class
+  // T_HCurlHighOrderFiniteElement<ET_SEGM, HCurlHighOrderFE_Shape<ET_SEGM>>;
   template class
   T_HCurlHighOrderFiniteElement<ET_TRIG, HCurlHighOrderFE_Shape<ET_TRIG>>;
-  template class
-  T_HCurlHighOrderFiniteElement<ET_QUAD, HCurlHighOrderFE_Shape<ET_QUAD>>;
+  // template class
+  // T_HCurlHighOrderFiniteElement<ET_QUAD, HCurlHighOrderFE_Shape<ET_QUAD>>;
+
 }
