fa88cd155d12787b4c9c08d358a36a1ea92b1f7a
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2018-09-27 14:11:07 +0200

    more dll-header

--- linalg/basevector.cpp
+++ linalg/basevector.cpp
@@ -875,10 +875,6 @@ namespace ngla
     return make_shared<S_BaseVectorPtr<TSCAL>> (range.Size(), es, pdata+range.First()*es);
   }
 
-  
-  template class S_BaseVectorPtr<double>;
-  template class S_BaseVectorPtr<Complex>;
-
   template class S_BaseVector<double>;
   template class S_BaseVector<Complex>;
   
