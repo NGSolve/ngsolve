2b2936dcb607c876cbbe71a4619711c8ecbca015
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2018-10-08 13:49:19 +0200

    number-spaces for vol and bnd ...

--- comp/numberfespace.cpp
+++ comp/numberfespace.cpp
@@ -22,7 +22,9 @@ namespace ngcomp
     enum { DIM_ELEMENT = 0 };
     enum { DIM_DMAT = 1 };
     enum { DIFFORDER = 0 };
-  
+    
+    virtual bool SupportsVB (VorB checkvb) const { return true; }
+
     template <typename MIP, typename MAT>
     static void GenerateMatrix (const FiniteElement & fel, const MIP & mip,
 				MAT && mat, LocalHeap & lh)
