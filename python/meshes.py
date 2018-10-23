a1f7cba8ad0cdebdcbdb4f91ffb174e005e9a846
PWS <p.schroeder@math.uni-goettingen.de>
2018-10-23 13:57:42 +0200

    also periodic meshes in 2D and 3D

--- python/meshes.py
+++ python/meshes.py
@@ -2,24 +2,46 @@ from netgen.meshing import *
 from netgen.csg import *
 import ngsolve
 
-def MakeQuadMesh(nx=10, ny=10, mapping = lambda x,y : (x,y) ):
+def MakeQuadMesh(nx=10, ny=10, periodic_x=False, periodic_y=False, mapping = lambda x,y : (x,y) ):
     mesh = Mesh()
     mesh.dim=2
 
     pids = []
+    if periodic_y:
+        slavei = []
+        masteri = []
+    if periodic_x:        
+        slavej = []
+        masterj = []
     for i in range(ny+1):
         for j in range(nx+1):
             x,y = mapping (j/nx, i/ny)
-            pids.append (mesh.Add (MeshPoint(Pnt(x,y,0))))
+            pids.append(mesh.Add (MeshPoint(Pnt(x,y,0))))
+            if periodic_y:
+                if i == 0:
+                    slavei.append(pids[-1])
+                if i == ny:
+                    masteri.append(pids[-1])  
+            if periodic_x:                       
+                if j == 0:
+                    slavej.append(pids[-1])
+                if j == nx:
+                    masterj.append(pids[-1])        
+    if periodic_y:
+        for i in range(len(slavei)):   
+            mesh.AddPointIdentification(masteri[i],slavei[i],identnr=1,type=2)
+    if periodic_x:            
+        for j in range(len(slavej)):        
+            mesh.AddPointIdentification(masterj[j],slavej[j],identnr=2,type=2)                                       
 
-    mesh.Add (FaceDescriptor(surfnr=1,domin=1,bc=1))
+    mesh.Add(FaceDescriptor(surfnr=1,domin=1,bc=1))
             
     for i in range(ny):
         for j in range(nx):
             base = i * (nx+1) + j
             pnum = [base,base+1,base+nx+2,base+nx+1]
             elpids = [pids[p] for p in pnum]
-            mesh.Add (Element2D(1,elpids))
+            mesh.Add(Element2D(1,elpids))
 
     for i in range(nx):
         mesh.Add(Element1D([pids[i], pids[i+1]], index=1))
@@ -29,6 +51,7 @@ def MakeQuadMesh(nx=10, ny=10, mapping = lambda x,y : (x,y) ):
         mesh.Add(Element1D([pids[ny*(nx+1)+i+1], pids[ny*(nx+1)+i]], index=3))
     for i in range(ny):
         mesh.Add(Element1D([pids[(i+1)*(nx+1)], pids[i*(nx+1)]], index=4))
+
     mesh.SetBCName(0, "bottom")        
     mesh.SetBCName(1, "right")        
     mesh.SetBCName(2, "top")        
@@ -37,7 +60,7 @@ def MakeQuadMesh(nx=10, ny=10, mapping = lambda x,y : (x,y) ):
     return ngsolve.Mesh(mesh)
 
 
-def MakeStructuredMesh(hexes = True, nx=10, ny=None, nz=None, mapping = lambda x,y,z : (x,y,z), cuboid_mapping = True):
+def MakeStructuredMesh(hexes = True, nx=10, ny=None, nz=None, periodic_x=False, periodic_y=False, periodic_z=False, mapping = lambda x,y,z : (x,y,z), cuboid_mapping = True):
     if nz == None:
         if ny == None:
             nz = nx
@@ -58,11 +81,44 @@ def MakeStructuredMesh(hexes = True, nx=10, ny=None, nz=None, mapping = lambda x
         netmesh.SetGeometry(geom)
 
     pids = []
+    if periodic_x:
+        slavei = []
+        masteri = []
+    if periodic_y:        
+        slavej = []
+        masterj = []
+    if periodic_z:        
+        slavek = []
+        masterk = []        
     for i in range(nx+1):
         for j in range(ny+1):
             for k in range(nz+1):
                 x,y,z = mapping(i / nx, j / ny, k / nz)
                 pids.append(netmesh.Add(MeshPoint(Pnt( x,y,z ))))
+                if periodic_x:
+                    if i == 0:
+                        slavei.append(pids[-1])
+                    if i == nx:
+                        masteri.append(pids[-1])  
+                if periodic_y:           
+                    if j == 0:
+                        slavej.append(pids[-1])
+                    if j == ny:
+                        masterj.append(pids[-1]) 
+                if periodic_z:                    
+                    if k == 0:
+                        slavek.append(pids[-1])
+                    if k == nz:
+                        masterk.append(pids[-1])
+    if periodic_x:
+        for i in range(len(slavei)):   
+            netmesh.AddPointIdentification(masteri[i],slavei[i],identnr=1,type=2)     
+    if periodic_y:        
+        for j in range(len(slavej)):            
+            netmesh.AddPointIdentification(masterj[j],slavej[j],identnr=2,type=2) 
+    if periodic_z:        
+        for k in range(len(slavek)):            
+            netmesh.AddPointIdentification(masterk[k],slavek[k],identnr=3,type=2)                                                      
 
     for i in range(nx):
         for j in range(ny):
@@ -123,7 +179,7 @@ def MakeStructuredMesh(hexes = True, nx=10, ny=None, nz=None, mapping = lambda x
         netmesh.SetBCName(2,"bottom")
         netmesh.SetBCName(3,"right")
         netmesh.SetBCName(4,"top")
-        netmesh.SetBCName(5,"behind")
+        netmesh.SetBCName(5,"back")
     
     netmesh.Compress()
     ngsmesh = ngsolve.Mesh(netmesh)
@@ -132,8 +188,8 @@ def MakeStructuredMesh(hexes = True, nx=10, ny=None, nz=None, mapping = lambda x
     return ngsmesh
 
 
-def MakeHexMesh(nx=10, ny=10, nz=10, mapping = lambda x,y,z : (x,y,z)):
-    return MakeStructuredMesh(hexes = True, nx=nx, ny=ny, nz=nz, mapping = mapping)
+def MakeHexMesh(nx=10, ny=10, nz=10, periodic_x = False, periodic_y = False, periodic_z = False, mapping = lambda x,y,z : (x,y,z)):
+    return MakeStructuredMesh(hexes = True, nx=nx, ny=ny, nz=nz, periodic_x = periodic_x, periodic_y = periodic_y, periodic_z = periodic_z, mapping = mapping)
 
 from math import pi
 if __name__ == "__main__":
@@ -141,6 +197,10 @@ if __name__ == "__main__":
     mesh = MakeQuadMesh(nx = 4, ny = 4)
     Draw(mesh)
     input("simple quad mesh -- press any key to continue -- ")
+
+    mesh = MakeQuadMesh(nx = 4, ny = 4, periodic_x = True, periodic_y = False)
+    Draw(mesh)
+    input("x-periodic quad mesh -- press any key to continue -- ")    
     
     mesh = MakeQuadMesh(nx = 32, ny = 16,
                         mapping = lambda x,y : ((1.1+sin(pi*(y-0.5)))*sin(pi*x),
@@ -151,6 +211,10 @@ if __name__ == "__main__":
     mesh = MakeHexMesh(nx=8)
     Draw(mesh)
     input("simple cube mesh -- press any key to continue -- ")
+
+    mesh = MakeHexMesh(nx=8, periodic_x = True, periodic_y = True, periodic_z = True)
+    Draw(mesh)
+    input("periodic cube mesh -- press any key to continue -- ")    
     
     mesh = MakeStructuredMesh(hexes = False, nx=3, ny=6, nz=10,
                            mapping = lambda x,y,z : (x,0.5*y*(y+x),exp(z)),
@@ -162,4 +226,10 @@ if __name__ == "__main__":
                            mapping = lambda x,y,z : (5*x*x*(0.5-x/3),10*y*y*(0.5-y/3),5*z*z*(0.5-z/3)),
                            cuboid_mapping=True )
     Draw(mesh)
-    print("mapped, anisotropic non-linear cuboid mesh -- finished.")
+    input("mapped, anisotropic non-linear cuboid mesh -- press any key to continue --")
+
+    mesh = MakeStructuredMesh(hexes = True, nx=8, ny=16, nz=8, periodic_x=True,
+                           mapping = lambda x,y,z : (5*x*x*(0.5-x/3),10*y*y*(0.5-y/3),5*z*z*(0.5-z/3)),
+                           cuboid_mapping=True )
+    Draw(mesh)
+    print("x-periodic, mapped, anisotropic non-linear cuboid mesh -- finished.")    
