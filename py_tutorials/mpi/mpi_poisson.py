6a6d22d3da5527d464f563c0fcd71ae4237a27f6
Lukas <lukas.kogler@tuwien.ac.at>
2019-02-25 15:01:35 +0100

    python mpi-tutrial files moved over to new syntax. subcomm-python-test modified (no more global comm)

--- py_tutorials/mpi/mpi_poisson.py
+++ py_tutorials/mpi/mpi_poisson.py
@@ -27,7 +27,7 @@ print("Hello from rank "+str(rank)+" of "+str(np))
 
 if rank==0:
     # master-proc generates mesh
-    mesh = unit_cube.GenerateMesh(maxh=0.1)
+    mesh = unit_cube.GenerateMesh(maxh=0.3)
     # and saves it to file
     mesh.Save("some_mesh.vol")
 
@@ -35,11 +35,17 @@ if rank==0:
 comm.Barrier()
 
 # now load mesh from file
-ngmesh = netgen.Mesh(dim=3)
-ngmesh.Load("some_mesh.vol")
+if comm.rank==0:
+    ngmesh = netgen.Mesh(dim=3)
+    ngmesh.Load("some_mesh.vol")
+    ngmesh.Distribute(comm)
+else:
+    ngmesh = netgen.Mesh.Receive(comm)
 
 #refine once?
 # ngmesh.Refine()
+
+print("points: ", len(list(ngmesh.Points())))
 
 mesh = Mesh(ngmesh)
 
