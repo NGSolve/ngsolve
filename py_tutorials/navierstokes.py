367fdbc85f1eeeed59425c8da438bac637a1d2bb
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2019-02-28 16:17:05 -0800

    compensate for coarser meshing

--- py_tutorials/navierstokes.py
+++ py_tutorials/navierstokes.py
@@ -10,8 +10,8 @@ tend = 10
 from netgen.geom2d import SplineGeometry
 geo = SplineGeometry()
 geo.AddRectangle( (0, 0), (2, 0.41), bcs = ("wall", "outlet", "wall", "inlet"))
-geo.AddCircle ( (0.2, 0.2), r=0.05, leftdomain=0, rightdomain=1, bc="cyl")
-mesh = Mesh( geo.GenerateMesh(maxh=0.08))
+geo.AddCircle ( (0.2, 0.2), r=0.05, leftdomain=0, rightdomain=1, bc="cyl", maxh=0.02)
+mesh = Mesh( geo.GenerateMesh(maxh=0.07))
 
 mesh.Curve(3)
 
