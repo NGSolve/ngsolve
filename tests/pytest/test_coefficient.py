272f9afeb9762c04ad914421fbfadf32468c7d93
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2019-04-23 17:31:54 +0200

    speed up coefficient pytest using fixtures

--- tests/pytest/test_coefficient.py
+++ tests/pytest/test_coefficient.py
@@ -1,22 +1,15 @@
+
 import pytest
-from netgen.geom2d import unit_square
-from netgen.csg import unit_cube
+from pytest import approx
 from ngsolve import *
-from netgen.geom2d import SplineGeometry
-
-
-def CompareCfs2D(c1, c2):
-    mesh = Mesh(unit_square.GenerateMesh(maxh=0.2))
-    c_err = c1-c2
-    error = Integrate(c_err*c_err, mesh)
-    return abs(error) < 1e-14
+from meshes import *
 
 def test_ParameterCF():
     p = Parameter(23)
     assert type(p) == Parameter
                     
-def test_mesh_size_cf():
-    for mesh in [ Mesh(unit_cube.GenerateMesh(maxh=0.2)), Mesh(unit_square.GenerateMesh(maxh=0.2))]:
+def test_mesh_size_cf(unit_mesh_3d, unit_mesh_2d):
+    for mesh in [ unit_mesh_3d, unit_mesh_2d]:
         dim = mesh.dim
         fes = L2(mesh, order=0)
         gfu = GridFunction(fes)
@@ -25,62 +18,56 @@ def test_mesh_size_cf():
             gfu.Set(cf*cf/2)
         else:
             gfu.Set(cf*cf*cf/6)
-        assert abs( sum(gfu.vec)-1 ) < 1e-12
+        assert sum(gfu.vec) == approx(1,rel=1e-12)
 
         bfi = BilinearForm(fes)
         bfi += SymbolicBFI( fes.TrialFunction()*(specialcf.mesh_size - specialcf.mesh_size.Compile(True, wait=True))*fes.TestFunction(), element_boundary=True)
         bfi.Assemble()
-        v = bfi.mat.AsVector().FV()
-        for val in v:
-            assert abs(val) < 1e-14
+        v = bfi.mat.AsVector().FV().NumPy()
+        assert v == approx(0,abs=1e-14)
 
-def test_real():
-    mesh = Mesh(unit_square.GenerateMesh(maxh=0.2))
+def test_real(unit_mesh_2d):
     cf = CoefficientFunction(1+2j)
-    assert cf.real(mesh(0.4,0.4)) == 1
-    assert cf.imag(mesh(0.2,0.6)) == 2
+    assert cf.real(unit_mesh_2d(0.4,0.4)) == 1
+    assert cf.imag(unit_mesh_2d(0.2,0.6)) == 2
 
-def test_pow():
+def test_pow(unit_mesh_2d):
     base = (x+0.1)
+    CompareCfs = lambda c1, c2, mesh: Integrate((c1-c2)*(c1-c2), mesh) == approx(0,abs=1e-12)
     for p in range(10):
         c = CoefficientFunction(1.0)
         for i in range(p):
             c = c*base
-        assert CompareCfs2D(base**p,c)
-        assert CompareCfs2D(base**(-p),1.0/c)
-        assert CompareCfs2D(base**float(p),c)
-        assert CompareCfs2D(base**CoefficientFunction(p),c)
+        assert CompareCfs(base**p,c,unit_mesh_2d)
+        assert CompareCfs(base**(-p),1.0/c,unit_mesh_2d)
+        assert CompareCfs(base**float(p),c,unit_mesh_2d)
+        assert CompareCfs(base**CoefficientFunction(p),c,unit_mesh_2d)
 
-def test_domainwise_cf():
-    geo = SplineGeometry()
-    geo.AddCircle ( (0, 0), r=1, leftdomain=1, rightdomain=2)
-    geo.AddCircle ( (0, 0), r=1.4, leftdomain=2, rightdomain=0)
-    mesh = Mesh(geo.GenerateMesh(maxh=0.1))
+def test_domainwise_cf(domain2_mesh_2d):
     c_vec = CoefficientFunction([(x,y),(1,3)])
     c = c_vec[0]*c_vec[1]
 
+    assert Integrate(c_vec, domain2_mesh_2d, definedon=domain2_mesh_2d.Materials("inner")) == approx([4,12])
+
     c_false = c.Compile(False);
-    error_false = Integrate((c-c_false)*(c-c_false), mesh)
-    assert abs(error_false) < 1e-14
+    error_false = Integrate((c-c_false)*(c-c_false), domain2_mesh_2d)
+    assert error_false == approx(0)
 
     c_true = c.Compile(True, wait=True);
-    error_true = Integrate((c-c_true)*(c-c_true), mesh)
-    assert abs(error_true) < 1e-14
+    error_true = Integrate((c-c_true)*(c-c_true), domain2_mesh_2d)
+    assert error_true == approx(0)
 
-def test_evaluate():
-    from netgen.geom2d import unit_square
-    import ngsolve as ngs
+def test_evaluate(unit_mesh_2d):
     import numpy as np
-    mesh = ngs.Mesh(unit_square.GenerateMesh(maxh=0.2))
     pnts = np.linspace(0.1,0.9,9)
-    cf = ngs.CoefficientFunction((ngs.x,ngs.y))
-    cf2 = ngs.CoefficientFunction((ngs.y, ngs.x * 1J))
-    mips = mesh(pnts,0.5)
+    cf = CoefficientFunction((x,y))
+    cf2 = CoefficientFunction((y, x * 1J))
+    mips = unit_mesh_2d(pnts,0.5)
     vals = cf(mips)
     vals2 = cf2(mips)
-    assert np.linalg.norm(vals-np.array(list(zip(pnts,[0.5]*10)))) < 1e-10
-    assert np.linalg.norm(vals2-np.array(list(zip([0.5 + 0J] * 10, pnts*1J)))) < 1e-10
-    assert ngs.x(mesh(0.5,0.5)) - 0.5 < 1e-10
+    assert vals == approx(np.array(list(zip(pnts,[0.5]*10))))
+    assert vals2 == approx(np.array(list(zip([0.5 + 0J] * 10, pnts*1J))))
+    assert x(unit_mesh_2d(0.5,0.5)) == approx(0.5)
 
 if __name__ == "__main__":
     test_pow()
