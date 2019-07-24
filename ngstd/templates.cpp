2e2bb16878afdd2ed6bde03adfbbe09253528f35
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2019-07-24 18:36:47 +0200

    Use MKL sequentially by default
    Only when we call PARDISO from MKL, we set mkl_num_threads_local back to
    it's original value for the factorization/solve function call

--- ngstd/templates.cpp
+++ ngstd/templates.cpp
@@ -4,6 +4,9 @@
 /* Date:   25. Mar. 2000                                             */
 /*********************************************************************/
 
+#ifdef USE_MKL
+#include <mkl.h>
+#endif // USE_MKL
 
 #include <ngstd.hpp>
 #include <ngsolve_version.hpp>
@@ -15,5 +18,15 @@ namespace ngstd
   int printmessage_importance = 5;
   bool NGSOStream :: glob_active = true;
   const string ngsolve_version = NGSOLVE_VERSION;
-}
 
+
+#ifdef USE_MKL
+  int mkl_max_threads = [] ()
+  {
+      auto mkl_max = mkl_get_max_threads();
+      mkl_set_num_threads_local(1);
+      return mkl_max;
+  }();
+#endif // USE_MKL
+
+}
