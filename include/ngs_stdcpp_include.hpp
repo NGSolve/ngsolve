964eda0102aebb487109f23f4950afad94af1df6
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2019-08-16 13:18:32 +0200

    Move alignment new to Netgen, update Netgen

--- include/ngs_stdcpp_include.hpp
+++ include/ngs_stdcpp_include.hpp
@@ -158,50 +158,6 @@ namespace std
 }
 #endif
 
-#ifdef __MAC_OS_X_VERSION_MIN_REQUIRED
-#if __MAC_OS_X_VERSION_MIN_REQUIRED < 101300
-#include <mm_malloc.h>
-#ifdef __clang__
-#pragma clang diagnostic ignored "-Winline-new-delete"
-#endif
-inline void * operator new (size_t s, std::align_val_t al)
-{
-  if (int(al) > __STDCPP_DEFAULT_NEW_ALIGNMENT__)
-    return _mm_malloc(s, int(al));
-  else
-    return new char[s];
-}
-
-inline void * operator new[] (size_t s, std::align_val_t al)
-{
-  if (int(al) > __STDCPP_DEFAULT_NEW_ALIGNMENT__)
-    return _mm_malloc(s, int(al));
-  else
-    return new char[s];
-}
-
-inline void operator delete  ( void* ptr, std::align_val_t al ) noexcept
-{
-  if (int(al) > __STDCPP_DEFAULT_NEW_ALIGNMENT__)
-     _mm_free(ptr);
-  else
-    delete (char*)ptr;
-}
-
-inline void operator delete[]( void* ptr, std::align_val_t al ) noexcept
-{
-  if (int(al) > __STDCPP_DEFAULT_NEW_ALIGNMENT__)
-     _mm_free(ptr);
-  else
-    delete[] (char*)ptr;
-}
-
-#endif
-#endif
-
-
-
-
 #ifdef PARALLEL
 #include <unistd.h>  // for usleep (only for parallel)
 #endif
