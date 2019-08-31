a9af7cb59b22880900eca0eb6a8232e155f626e7
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2019-08-31 11:46:36 +0200

    added missing files

---
+++ comp/normalfacetfespace.hpp
@@ -0,0 +1,87 @@
+#ifndef NORMAL_FACET_FESPACE_HPP
+#define NORMAL_FACET_FESPACE_HPP
+
+/*********************************************************************/
+/* File:   normalfacetfespace.hpp                                    */
+/* Author: Joachim Schoeberl                                         */
+/* Date:   2019                                                      */
+/*********************************************************************/
+
+
+namespace ngcomp
+{
+
+  class NGS_DLL_HEADER NormalFacetFESpace : public FESpace
+  {
+  protected:
+    int level;
+    Array<int> first_facet_dof;
+    Array<int> first_inner_dof;  // for highest_order_dc
+
+    int rel_order;
+
+    Array<INT<2> > order_facet;
+    Array<bool> fine_facet;
+
+    int ndof;
+    Array<int> ndlevel;
+    bool var_order;
+    bool print;
+
+    bool highest_order_dc;
+    bool hide_highest_order_dc;
+    
+  public:
+    NormalFacetFESpace (shared_ptr<MeshAccess> ama, const Flags & flags, 
+			bool parseflags = false );
+
+    virtual ~NormalFacetFESpace () { ; }
+    static DocInfo GetDocu ();
+
+    virtual string GetClassName () const override
+    {
+      return "NormalFacetFESpace";
+    }
+
+    void Update() override;
+    virtual void UpdateCouplingDofArray() override;
+
+    virtual void SetOrder (NodeId ni, int order) override;
+    virtual int GetOrder (NodeId ni) const override;
+    
+    virtual size_t GetNDof() const throw() override { return ndof; }
+
+    virtual size_t GetNDofLevel ( int i ) const override { return ndlevel[i]; }
+
+    virtual FiniteElement & GetFE(ElementId ei, Allocator & lh) const override;
+    
+    virtual void GetFacetDofNrs (int felnr, Array<DofId> & dnums) const;
+
+    virtual int GetNFacetDofs (int felnr) const;
+
+    virtual void GetDofNrs (ElementId ei, Array<DofId> & dnums) const override;
+
+    virtual shared_ptr<Table<int>> CreateSmoothingBlocks (const Flags & precflags) const override;
+    ///
+    virtual shared_ptr<Array<int>> CreateDirectSolverClusters (const Flags & precflags) const override;
+  
+    // some utility functions for convenience
+    ///
+    // virtual void GetVertexNumbers(int elnr, Array<int>& vnums) const;
+    ///
+    virtual INT<2> GetFacetOrder(int fnr) const;
+
+    virtual int GetFirstFacetDof(int fanr) const;
+
+    virtual bool UsesHighestOrderDiscontinuous() const {return highest_order_dc;};
+
+    virtual void GetVertexDofNrs (int elnum, Array<DofId> & dnums) const override;
+    virtual void GetEdgeDofNrs (int elnum, Array<DofId> & dnums) const override;
+    virtual void GetFaceDofNrs (int felnr, Array<DofId> & dnums) const override;
+    virtual void GetInnerDofNrs (int felnr, Array<DofId> & dnums) const override;
+  };
+
+}
+
+#endif
+
---
+++ fem/normalfacetfe.hpp
@@ -0,0 +1,213 @@
+#ifndef NORMAL_FACET_FE_HPP__
+#define NORMAL_FACET_FE_HPP__
+
+/*********************************************************************/
+/* File:   normalfacetfe.hpp                                         */
+/* Author: J. Schoeberl                                              */
+/* Date:   2019                                                      */
+/*********************************************************************/
+
+
+#include <fem.hpp>
+#include "thdivfe.hpp"
+
+namespace ngfem 
+{
+  /*
+    facet element with normal facet components.
+  */
+  
+  template <ELEMENT_TYPE ET>
+  class NormalFacetFacetFE :
+    public HDivFiniteElement<ET_trait<ET>::DIM>,
+    public VertexOrientedFE<ET>,
+    public ET_trait<ET>
+  {
+  protected:
+    INT<2> order_inner;
+    using VertexOrientedFE<ET>::vnums;
+    using ET_trait<ET>::DIM;
+    using HDivFiniteElement<ET_trait<ET>::DIM>::order;
+ 
+  public:
+    using VertexOrientedFE<ET>::SetVertexNumber;
+    using VertexOrientedFE<ET>::SetVertexNumbers;
+
+    NormalFacetFacetFE (int aorder)
+    {
+      order = aorder;
+      order_inner = INT<2>(aorder,aorder);
+      ComputeNDof();
+    }
+
+    NormalFacetFacetFE () { ; }
+
+    HD virtual ELEMENT_TYPE ElementType() const override { return ELEMENT_TYPE(ET); }
+
+    INLINE void SetOrder (int aorder)
+    {
+      order = aorder;
+      order_inner = INT<2>(aorder,aorder);
+      ComputeNDof();
+    }
+  
+    INLINE void SetOrder (INT<2> oi)
+    {
+      order = max2 (oi[0], oi[1]);
+      order_inner = oi;
+      ComputeNDof();
+    }
+
+    virtual void ComputeNDof ();
+
+    virtual void CalcShape(const IntegrationPoint & ip,
+         		    SliceMatrix<> shape) const override;
+
+    template<typename Tx, typename TFA>  
+    void T_CalcShape (TIP<DIM,Tx> tip, TFA & shape) const;
+  };
+
+
+  
+
+  template <ELEMENT_TYPE ET> class NormalFacetVolumeFE_Shape;
+  
+  template <ELEMENT_TYPE ET>
+  class NormalFacetVolumeFE : public T_HDivFiniteElement<NormalFacetVolumeFE_Shape<ET>, ET>,
+                              public ET_trait<ET>, public VertexOrientedFE<ET>
+  {
+  protected:
+    using ET_T = ET_trait<ET>;
+    INT<2> facet_order[ET_T::N_FACET];
+    int first_facet_dof[ET_T::N_FACET+1];
+    bool highest_order_dc;
+    using HDivFiniteElement<ET_trait<ET>::DIM>::order;
+    using VertexOrientedFE<ET>::vnums;
+    enum { DIM = ET_trait<ET>::DIM };
+    
+  public:
+    using VertexOrientedFE<ET>::SetVertexNumbers;
+    
+    NormalFacetVolumeFE () { highest_order_dc=false; }
+    
+    HD virtual ELEMENT_TYPE ElementType() const override { return ELEMENT_TYPE(ET); }
+
+    void SetHighestOrderDC(bool set) { highest_order_dc=set; }
+
+    void SetOrder(int ao)
+    {
+      order = ao;
+      for ( int i = 0; i < ET_T::N_FACET; i++ )
+        facet_order[i] = INT<2> (ao, ao);
+      ComputeNDof();
+    }
+
+    void SetOrder(FlatArray<int> & ao)
+    {
+      order = 0;
+      assert(ao.Size()==ET_T::N_FACET);
+      for ( int i = 0; i < ET_T::N_FACET; i++ )
+        {
+          order = max2 ( order, ao[i] );
+          facet_order[i] = INT<2> (ao[i], ao[i]);
+        }
+      ComputeNDof();
+    }
+
+    void SetOrder(FlatArray<INT<2> > & ao)
+    {
+      order = 0;
+      assert(ao.Size()==ET_T::N_FACET);
+      for ( int i = 0; i < ET_T::N_FACET; i++ )
+        {
+          order = max3 ( order, ao[i][0], ao[i][1] );
+          facet_order[i] = ao[i];
+        }
+      ComputeNDof();
+    }
+
+    INT<2> GetFacetOrder(int j) const { return facet_order[j]; }
+    int GetVertexNumber(int j) const { return vnums[j]; }
+
+    /*
+    virtual void CalcShape (const IntegrationPoint & ip, SliceMatrix<> shape) const override
+    {
+      int fnr = ip.FacetNr();
+      if (fnr >= 0)
+        {
+          CalcShape (ip, fnr, shape);
+          return;
+        }
+      throw Exception("NormalFacetVolumeFiniteElement<D>::CalcShape in global coordinates disabled");
+    }
+
+    virtual void CalcMappedShape (const SIMD_BaseMappedIntegrationRule & mir, 
+                                  BareSliceMatrix<SIMD<double>> shapes) const override;
+    
+    virtual void Evaluate (const SIMD_BaseMappedIntegrationRule & ir, BareSliceVector<> coefs,
+                           BareSliceMatrix<SIMD<double>> values) const override;
+    
+    virtual void AddTrans (const SIMD_BaseMappedIntegrationRule & ir, BareSliceMatrix<SIMD<double>> values,
+                           BareSliceVector<> coefs) const override;
+    */
+    template<typename Tx, typename TFA>  
+    void T_CalcShape (Tx hx[DIM], int fnr, TFA & shape) const;
+    
+    virtual void CalcShape (const IntegrationPoint & ip, int facet, SliceMatrix<> shape) const;
+
+    virtual int GetNExtraShapes( int facet) const {return 0;}
+    virtual void CalcExtraShape (const IntegrationPoint & ip, int facet, FlatMatrixFixWidth<ET_T::DIM> xshape) const {xshape = 0.0;}
+
+    virtual void GetFacetDofNrs(int afnr, Array<int>& fdnums) const
+    {
+      int first = first_facet_dof[afnr];
+      int next = first_facet_dof[afnr+1];
+      assert(next > first);
+      fdnums.SetSize(next-first);
+      for (auto i : Range(next-first))
+        fdnums[i] = first+i;
+    }
+
+    virtual int GetFacetNDof(int afnr) const
+    { return first_facet_dof[afnr+1] - first_facet_dof[afnr]; };
+    virtual int GetFirstFacetDof(int afnr) const {
+      return first_facet_dof[afnr];};
+
+    virtual void ComputeNDof ();
+
+    /// degrees of freedom sitting inside the element, used for static condensation
+    virtual void GetInternalDofs (Array<int> & idofs) const
+    {
+      idofs.SetSize(0);
+      if (highest_order_dc)
+        {
+          if (ET_T::DIM==2)
+            {
+              for (int i=0; i < ET_T::N_FACET; i++)
+                idofs.Append(first_facet_dof[i+1]-1);
+            }
+          else
+            {
+              throw Exception ("NormalFacetFE with hodc not ready in 3D");
+              for (int i=0; i < ET_T::N_FACET; i++)
+                {
+                  int pos = first_facet_dof[i]-2;
+                  int fac = 4 - ElementTopology::GetNVertices(ElementTopology::GetFaceType(ET,i));
+                  for (int k = 0; k <= facet_order[i][0]; k++){
+                    pos += 2*(facet_order[i][0]+1-fac*k);
+                    idofs.Append(pos);
+                    idofs.Append(pos+1);
+                  }
+                }
+            }//end if Dimension
+        }
+    }
+  };
+
+}
+
+
+
+#endif
+
+
