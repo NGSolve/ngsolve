4b97de0222fecb143aacf7be8686c2e98e8c291a
Matthias Hochsteger <mhochsteger@cerbsim.com>
2019-10-16 15:33:42 +0200

    GetVisData: skip integration rules with size 0

--- solve/python_solve.cpp
+++ solve/python_solve.cpp
@@ -646,13 +646,16 @@ void ExportVisFunctions(py::module &m) {
                     if(el.is_curved) {
                         ei.data.Append(vertices.Size()/3);
                         HeapReset hr(lh);
-                        auto & mir = GetMappedIR(ma, el, ir, lh);
-                        // mapped coordinates of edge midpoints (for P2 interpolation)
-                        for (auto &ip : mir) {
-                          auto p = ip.GetPoint();
-                          for (auto i : Range(3))
-                              vertices.Append(p[i]);
-                        }
+                        if(ir.Size()>0)
+                          {
+                            auto & mir = GetMappedIR(ma, el, ir, lh);
+                            // mapped coordinates of edge midpoints (for P2 interpolation)
+                            for (auto &ip : mir) {
+                                auto p = ip.GetPoint();
+                                for (auto i : Range(3))
+                                    vertices.Append(p[i]);
+                            }
+                          }
                     }
                 }
                 element_data[VOL].append(types_and_numbering);
