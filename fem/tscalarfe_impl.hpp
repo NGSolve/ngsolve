5bcf1895643feb2b0a1d0534654fef473490e292
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2019-11-11 10:45:30 +0100

    fix warning

--- fem/tscalarfe_impl.hpp
+++ fem/tscalarfe_impl.hpp
@@ -827,8 +827,8 @@ namespace ngfem
              for (size_t i = 0; i < mir.Size(); i++)
                {
                  // Directional derivative
-                 // [[maybe_unused]]
-                 Vec<DIM, SIMD<double>> jac_dir = mir[i].GetJacobianInverse() * values.Col(i);
+                 [[maybe_unused]]
+                   Vec<DIM, SIMD<double>> jac_dir = mir[i].GetJacobianInverse() * values.Col(i);
 
                  const auto &ip = mir[i].IP();
                  TIP<DIM,AutoDiffRec<1,SIMD<double>>>adp;
