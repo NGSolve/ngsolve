c12b11900c826b0d171401d5ee9ab93d4223c385
Christopher Lackner <clackner@cerbsim.com>
2019-11-15 13:23:43 +0100

    use a.Data() instead of &a(0,0) to avoid range check exceptions for 0 size

--- fem/tscalarfe_impl.hpp
+++ fem/tscalarfe_impl.hpp
@@ -188,7 +188,7 @@ namespace ngfem
         TIP<DIM,SIMD<double>> pt = hir[i].TIp<DIM>();
         SIMD<double> sum = 0;
         // T_CalcShape (&pt(0), SBLambda ( [&](int j, MultiSIMD<2,double> shape) { sum += coefs(j)*shape; } ));
-        double * pcoefs = &coefs(0);
+        double * pcoefs = coefs.Data();
         size_t dist = coefs.Dist();
         T_CalcShape (pt, // TIP<DIM,SIMD<double>> (hir[i]), 
                      SBLambda ( [&](int j, SIMD<double> shape)
