6c6db17fe4f91d453f0c32576980f73c2f52ce45
Christopher Lackner <clackner@cerbsim.com>
2019-11-25 09:35:07 +0100

    newton solver as class

--- python/nonlinearsolvers.py
+++ python/nonlinearsolvers.py
@@ -2,6 +2,90 @@ from ngsolve.la import InnerProduct
 from math import sqrt
 from ngsolve import Projector, Norm
 
+class NewtonSolver:
+    def __init__(self, a, u, rhs=None, freedofs=None,
+                 inverse="umfpack", solver=None):
+        self.a, self.u, self.inverse = a, u, inverse
+        self.freedofs = freedofs or u.space.FreeDofs(a.condense)
+        self.w = u.vec.CreateVector()
+        self.r = u.vec.CreateVector()
+        self.rhs = rhs
+        self.uh = u.vec.CreateVector()
+        self.inv = None if solver is None else solver
+        if solver:
+            self.inverse = "given"
+
+    def Solve(self, maxit=100, maxerr=1e-11, dampfactor=1,
+              printing=False, callback=None, linesearch=False,
+              printenergy=False):
+        numit = 0
+        err = 1.
+        a, u, w, r,uh = self.a, self.u, self.w, self.r, self.uh
+        for it in range(maxit):
+            numit += 1
+            if printing:
+                print("Newton iteration ", it)
+                if printenergy:
+                    print("Energy: ", a.Energy(u.vec))
+
+            a.AssembleLinearization(u.vec)
+            a.Apply(u.vec, r)
+
+            if self.inverse in ("sparsecholesky", "given") and self.inv:
+                self.inv.Update()
+            else:
+                self.inv = a.mat.Inverse(self.freedofs,
+                                         inverse=self.inverse)
+
+            if self.rhs is not None:
+                r.data -= self.rhs.vec
+            if a.condense:
+                r.data += a.harmonic_extension_trans * r
+                w.data = self.inv * r
+                w.data += a.harmonic_extension * w
+                w.data += a.inner_solve * r
+            else:
+                w.data = self.inv * r
+                print("n its = ", self.inv.iterations)
+
+            err2 = InnerProduct(w,r)
+            if err2 < 0:
+                print("wrong direction")
+            err = sqrt(abs(err2))
+            if printing:
+                print("err = ", err)
+
+            tau = min(1, numit*dampfactor)
+
+            if linesearch:
+                uh.data = u.vec - tau*w
+                energy = a.Energy(u.vec)
+                while a.Energy(uh) > energy+(max(1e-14*abs(energy),maxerr)) and tau > 1e-10:
+                    tau *= 0.5
+                    uh.data = u.vec - tau * w
+                    if printing:
+                        print ("tau = ", tau)
+                        print ("energy uh = ", a.Energy(uh))
+                u.vec.data = uh
+
+            else:
+                u.vec.data -= tau * w
+            if callback is not None:
+                callback(it, err)
+            if abs(err) < maxerr: break
+        else:
+            print("Warning: Newton might not converge! Error = ", err)
+            return (-1,numit)
+        return (0,numit)
+
+    def SetDirichlet(self, dirichletvalues):
+        a.AssembleLinearization(u.vec)
+        self.inv = a.mat.Inverse(self.freedofs, inverse=self.inverse)
+
+        w.data = dirichletvalues-u.vec
+        r.data = a.mat * w
+        w.data -= self.inv*r
+        u.vec.data += w
 
 def Newton(a, u, freedofs=None, maxit=100, maxerr=1e-11, inverse="umfpack", \
                dirichletvalues=None, dampfactor=1, printing=True, callback=None):
@@ -40,64 +124,11 @@ def Newton(a, u, freedofs=None, maxit=100, maxerr=1e-11, inverse="umfpack", \
       List of two integers. The first one is 0 if Newton's method did converge, -1 otherwise. The second one gives the number of Newton iterations needed.
 
     """
-    w = u.vec.CreateVector()
-    r = u.vec.CreateVector()
-
-    err   = 1
-    numit = 0
-    inv   = None
-    
-    if freedofs is None:
-        freedofs = u.space.FreeDofs(a.condense)
-
-        
+    solver = NewtonSolver(a, u, freedofs, inverse)
     if dirichletvalues is not None:
-        a.AssembleLinearization(u.vec)
-        inv = a.mat.Inverse(freedofs, inverse=inverse)
-        
-        w.data = dirichletvalues-u.vec
-        r.data = a.mat * w
-        w.data -= inv*r
-        u.vec.data += w
-
-        
-    for it in range(maxit):
-        numit += 1
-        if printing:
-            print("Newton iteration ", it)
-
-        a.AssembleLinearization(u.vec)
-        a.Apply(u.vec, r)
-
-        if inverse == "sparsecholesky" and inv:
-            inv.Update()
-        else:
-            inv = a.mat.Inverse(freedofs, inverse=inverse)
-
-        if a.condense:
-            r.data += a.harmonic_extension_trans * r
-            w.data = inv * r
-            w.data += a.harmonic_extension * w
-            w.data += a.inner_solve * r
-        else:
-            w.data = inv * r
-            
-        err = sqrt(abs(InnerProduct(w,r)))
-        if printing:
-            print("err = ", err)
-
-        u.vec.data -= min(1, numit*dampfactor)*w
-
-        if callback is not None:
-            callback(it, err)
-        
-        if abs(err) < maxerr: break
-    else:
-        print("Warning: Newton might not converge! Error = ", err)
-        return (-1,numit)
-    return (0,numit)
-
-
+        solver.SetDirichlet(dirichletvalues)
+    return solver.Solve(maxit, maxerr, dampfactor,
+                        printing, callback, False, False)
 
 
 def NewtonMinimization(a, u, freedofs=None, maxit=100, maxerr=1e-11, inverse="umfpack", dampfactor=1, linesearch=False, printing=True, callback=None):
@@ -140,55 +171,6 @@ def NewtonMinimization(a, u, freedofs=None, maxit=100, maxerr=1e-11, inverse="um
       List of two integers. The first one is 0 if Newton's method did converge, -1 otherwise. The second one gives the number of Newton iterations needed.
 
     """
-    w = u.vec.CreateVector()
-    r = u.vec.CreateVector()
-    uh = u.vec.CreateVector()
-
-    err   = 1
-    numit = 0
-    inv   = None
-    
-    for it in range(maxit):
-        numit += 1
-        if printing:
-            print("Newton iteration ", it)
-            print ("energy = ", a.Energy(u.vec))
-        a.AssembleLinearization(u.vec)
-        a.Apply(u.vec, r)
-
-        if inverse == "sparsecholesky" and inv:
-            inv.Update()
-        else:
-            inv = a.mat.Inverse(freedofs if freedofs else u.space.FreeDofs(a.condense), inverse=inverse)
-
-        if a.condense:
-            r.data += a.harmonic_extension_trans * r
-            w.data = inv * r
-            w.data += a.harmonic_extension * w
-            w.data += a.inner_solve * r
-        else:
-            w.data = inv * r
-            
-        err = sqrt(abs(InnerProduct(w,r)))
-        if printing:
-            print("err = ", err)
-
-        energy = a.Energy(u.vec)
-        uh.data = u.vec - min(1, numit*dampfactor)*w
-            
-        tau = min(1, numit*dampfactor)
-        if linesearch:
-            while a.Energy(uh) > energy+(max(1e-14*abs(energy),maxerr)) and tau > 1e-10:
-                tau *= 0.5
-                uh.data = u.vec - tau * w
-                if printing:
-                    print ("tau = ", tau)
-                    print ("energy uh = ", a.Energy(uh))
-        u.vec.data = uh
-        if callback: callback()
-        if abs(err) < maxerr: break
-    else:
-        print("Warning: Newton might not converge! Error = ", err)
-        return (-1,numit)
-    return (0,numit)
-
+    solver = NewtonSolver(a, u, freedofs, inverse)
+    return solver.Solve(maxit, maxerr, dampfactor,
+                        printing, callback, linesearch, printing)
