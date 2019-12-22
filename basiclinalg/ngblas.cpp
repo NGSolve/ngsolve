b3183ff6d919a71f05a76f090f4a2e255b0eaf69
Christopher Lackner <clackner@cerbsim.com>
2019-12-22 12:21:19 +0100

    fix some more range checks for 0 size matrices

--- basiclinalg/ngblas.cpp
+++ basiclinalg/ngblas.cpp
@@ -1290,9 +1290,9 @@ namespace ngbla
   {
     constexpr size_t bs = 256; // inner-product loop
     size_t wa = a.Width();
-    double *pa = &a(0);
-    double *pb = &b(0);
-    double *pc = &c(0);
+    double *pa = a.Data();
+    double *pb = b.Data();
+    double *pc = c.Data();
     for (size_t i = 0; i < wa; i += bs, pa+=bs, pb+=bs)
       TAddABt2 (min2(bs,wa-i), a.Height(), b.Height(),
                 pa, a.Dist(), pb, b.Dist(), pc, c.Dist(), func);
