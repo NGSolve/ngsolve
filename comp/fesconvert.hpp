5b6b21634e7ed9080e4401105c097002f9de47a8
Lukas <lukas.kogler@tuwien.ac.at>
2020-04-04 12:46:57 +0200

    Can Project Dirichlet DOFs out of range of ConvertOperator. Also hopefully fix ConvertOperator with gcc 7

--- comp/fesconvert.hpp
+++ comp/fesconvert.hpp
@@ -6,7 +6,7 @@ namespace ngcomp
 
   shared_ptr<BaseMatrix> ConvertOperator (shared_ptr<FESpace> spacea, shared_ptr<FESpace> spaceb, VorB vb, LocalHeap & lh,
 					  shared_ptr<DifferentialOperator> diffop = nullptr, const Region * reg = NULL,
-					  bool localop = false, bool parmat = true, bool use_simd = true,
+					  shared_ptr<BitArray> range_dofs = nullptr, bool localop = false, bool parmat = true, bool use_simd = true,
 					  int bonus_intorder_ab = 0, int bonus_intorder_bb = 0);
 
 } // namespace ngcomp
