07ded69b8ef2d17008e0098ed8c70e3009cbf3b3
Michael Neunteufel <michael.neunteufel@live.de>
2020-04-15 11:54:31 +0000

    Voxelcf trafocf

--- fem/voxelcoefficientfunction.hpp
+++ fem/voxelcoefficientfunction.hpp
@@ -12,15 +12,17 @@ namespace ngfem
     Array<size_t> dim_vals;
     Array<SCAL> values;
     bool linear;
+    shared_ptr<CoefficientFunction> trafocf;
   public:
     VoxelCoefficientFunction(const Array<double>& _start,
                              const Array<double>& _end,
                              const Array<size_t>& _dim_vals,
                              Array<SCAL>&& _values,
-                             bool _linear)
+                             bool _linear,
+                             shared_ptr<CoefficientFunction> trafo=nullptr)
       : CoefficientFunctionNoDerivative(1, is_same_v<SCAL, Complex>),
         start(_start), end(_end), dim_vals(_dim_vals),
-        values(move(_values)), linear(_linear)
+        values(move(_values)), linear(_linear), trafocf(trafo)
     { ; }
 
     using CoefficientFunctionNoDerivative::Evaluate;
