8c4620b46ed5f55c79bc0918ef71be52933a0f8d
Matthias Hochsteger <mhochsteger@cerbsim.com>
2020-06-16 15:46:42 +0200

    [gitlab-ci] fix Windows builds

--- tests/gitlab-ci/win/upload.bat
+++ tests/gitlab-ci/win/upload.bat
@@ -2,7 +2,8 @@ cd %NETGEN_BUILD_DIR%
 IF %errorlevel% NEQ 0 exit /b %errorlevel%
 cmake --build . --target package --config Release
 IF %errorlevel% NEQ 0 exit /b %errorlevel%
-rsync -ztrl --del -e ssh ^
+set HOME=/cygdrive/c/tools
+rsync -ztrl --del -e "ssh -o StrictHostKeyChecking=no" ^
       --rsync-path="mkdir -p deploy/builds/%CI_PIPELINE_ID%/windows && rsync" ^
       *.msi ^
       gitlab-runner@vector.asc.tuwien.ac.at:deploy/builds/%CI_PIPELINE_ID%/windows/
