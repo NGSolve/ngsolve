87b32e6680e1b2809713359ef9fd681084b81d08
Thomas Lutonsky <e1621437@student.tuwien.ac.at>
2020-08-14 15:08:27 +0000

    Edit InnerProductD Applied cache blocking and MultiVecScalAB kernels

--- linalg/basevector.cpp
+++ linalg/basevector.cpp
@@ -1004,17 +1004,60 @@ namespace ngla
       static Timer t("BaseVector-MultiVector::InnerProductD");
       RegionTimer reg(t);
       t.AddFlops (Size()*v2.Size()*this->RefVec()->FVDouble().Size());
-      
-      Matrix<double> res(Size(), v2.Size());
 
-      // shared memory parallelization stays here
-      ParallelFor (Size(), [&] (int i)
-                   {
-                     // should call ngblas - multivector operation:
-                     for (int j = 0; j < v2.Size(); j++)
-                       res(i,j) = ngbla::InnerProduct ((*this)[i]->FVDouble(), v2[j]->FVDouble());
-                   });
+      int n = this->RefVec()->FVDouble().Size();
+
+      Matrix<double> res(Size(), v2.Size());
+      res = 0;
+
+      constexpr size_t BBH = 512;
+      constexpr size_t BH = 256;
+
+      ParallelFor ( 1 + n / BBH, [&] (int i) {
+
+        int i0 = BBH * i;
+        int is;
+        if (i0 + BBH < n) {
+          is = BBH;
+        }
+        else {
+          is = n - i0;
+        }
+
+        // store pointers to vectors of first multivector
+        double* ppx [Size()];
+        for (int ell=0; ell < Size(); ell++) {
+          ppx[ell] = (*this)[ell]->FVDouble().Addr(i0);
+        }
+
+        for (int k = 0; k < 1 + v2.Size() / BH; k++) {
+          int k0 = BH * k;
+          int ks = min(BH, v2.Size() - k0);
+
+          // store pointers to vectors of second multivector
+          double* ppy [ks];
+          for(int ell=0; ell < ks; ell++) {
+            ppy[ell] = v2[k0 + ell]->FVDouble().Addr(i0);
+          }
+
+          // calculate result
+          Matrix<double> res_sub(Size(), ks);
+
+          ngbla::PairwiseInnerProduct(is, FlatArray(Size(), ppx), FlatArray(ks, ppy), res_sub);
+
+          // add the results to the matrix res
+          for (int ell_j = 0; ell_j < Size(); ell_j++) {
+            for (int ell_k = 0; ell_k < ks; ell_k++) {
+              AtomicAdd(res(ell_j, k0 + ell_k), res_sub(ell_j, ell_k));
+            }
+          }
+
+        }
+
+      });
+
       return res;
+
     }
 
   };
