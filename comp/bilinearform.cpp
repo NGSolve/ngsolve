eaae0c9e0e5771590105630e374b5c5791319005
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2020-08-21 22:23:05 +0200

    geomfree also for 1D elements

--- comp/bilinearform.cpp
+++ comp/bilinearform.cpp
@@ -745,13 +745,13 @@ namespace ngcomp
             has_vb = true;
         if (!has_vb)
           continue;
-        
+
         Array<short> classnr(ma->GetNE(vb));
         ma->IterateElements
           (vb, lh, [&] (auto el, LocalHeap & llh)
            {
              classnr[el.Nr()] = 
-               SwitchET<ET_TRIG,ET_TET>
+               SwitchET<ET_SEGM, ET_TRIG,ET_TET>
                (el.GetType(),
                 [el] (auto et) { return ET_trait<et.ElementType()>::GetClassNr(el.Vertices()); });
            });
