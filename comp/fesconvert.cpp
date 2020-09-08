be6d384fb2bda1ea1aedc04634faf5ccdc203382
Lukas <lukas.kogler@tuwien.ac.at>
2020-09-08 11:27:48 +0200

    geometry free convertoperator

--- comp/fesconvert.cpp
+++ comp/fesconvert.cpp
@@ -10,7 +10,6 @@ namespace ngcomp
   template<int BSA, int BSB, typename SCAL> using TM = typename TM_TRAIT<BSA, BSB, SCAL>::type;
   template<int BSA, int BSB, typename SCAL> using TSPM = SparseMatrix<TM<BSA, BSB, SCAL>>;
 
-
   template<class SCAL, int DIMA, int DIMB>
   shared_ptr<BaseMatrix> ConvertOperator (shared_ptr<FESpace> space_a, shared_ptr<FESpace> space_b,
 					  // int inda, int indb,
@@ -276,38 +275,292 @@ namespace ngcomp
   } // ConvertOperator
 
 
+
+  template<class SCAL>
+  shared_ptr<BaseMatrix> ConvertOperatorGF (shared_ptr<FESpace> space_a, shared_ptr<FESpace> space_b,
+					  // int inda, int indb,
+					  shared_ptr<DifferentialOperator> diffop, shared_ptr<CoefficientFunction> trial_cf,
+					  VorB vb, const Region * reg, LocalHeap & clh,
+					  shared_ptr<BitArray> range_dofs = nullptr,
+					  bool localop = false, bool parmat = true, bool use_simd = true,
+					  int bonus_intorder_ab = 0, int bonus_intorder_bb = 0)
+  {
+
+    static Timer t ("ConvertOperatorGF");
+    RegionTimer regt(t);
+
+    auto ma = space_b->GetMeshAccess();
+
+    if ( parmat && (space_a->IsParallel() != space_b->IsParallel()) )
+      { throw Exception("Cannot form ConvertOperator between a parallel and a local space!"); }
+
+    if ( parmat && space_a->IsParallel() && space_b->IsParallel() ) {
+      MPI_Comm comma = space_a->GetParallelDofs()->GetCommunicator(), commb = space_b->GetParallelDofs()->GetCommunicator();
+      if (comma != commb)
+	{ throw Exception("Cannot convert between spaces defined on different Communicators!"); }
+    }
+
+    /** Proxies and Integrators **/
+
+    shared_ptr<CoefficientFunction> trial_a;
+    if (trial_cf != nullptr)
+      { trial_a = trial_cf; }
+    else if ( diffop != nullptr ) {
+      trial_a = make_shared<ProxyFunction>(space_a, false, false, diffop,
+					   nullptr, nullptr, nullptr, nullptr, nullptr);
+    }
+    else { // Probably most of the time
+      trial_a = make_shared<ProxyFunction>(space_a, false, false, space_a->GetEvaluator(vb),
+					   nullptr, nullptr, nullptr, nullptr, nullptr);
+    }
+
+    auto trial_b = make_shared<ProxyFunction>(space_b, false, false, space_b->GetEvaluator(vb),
+					      nullptr, nullptr, nullptr, nullptr, nullptr);
+
+    if (!space_b->GetAdditionalEvaluators().Used("dual"))
+      throw Exception(string("Dual diffop does not exist for ") + space_b->GetClassName() + string("!"));
+    auto dual_evaluator = space_b->GetAdditionalEvaluators()["dual"];
+    for (VorB avb = dual_evaluator->VB(); avb < vb; avb++) {
+      dual_evaluator = dual_evaluator->GetTrace();
+      if ( dual_evaluator == nullptr )
+	{ throw Exception(space_b->GetClassName() + string(" has no dual trace operator for vb = ")
+			  + to_string(avb) + string(" -> ") + to_string(avb + 1) + string("!")); }
+    }
+    auto dual_b = make_shared<ProxyFunction>(space_b, true, false, dual_evaluator,
+					     nullptr, nullptr, nullptr, nullptr, nullptr);
+    
+    if ( trial_a->Dimension() != trial_b->Dimension() )
+      { throw Exception(string("Cannot convert from ") + space_a->GetClassName() + string(" to ") + space_b->GetClassName() +
+			string(" - dimensions mismatch: ") + to_string(trial_a->Dimension()) +
+			string(" != ") + to_string(trial_b->Dimension()) + string("!")); }
+
+    Array<shared_ptr<BilinearFormIntegrator>> ab_bfis, bb_bfis;
+    for (auto element_vb : space_b->GetDualShapeNodes(vb)) {
+      shared_ptr<CoefficientFunction> ab, bb;
+      if (trial_b->Dimension() == 1) {
+	ab = trial_a * dual_b;
+	bb = trial_b * dual_b;
+      }
+      else {
+	ab = InnerProduct(trial_a, dual_b);
+	bb = InnerProduct(trial_b, dual_b);
+      }
+      auto ab_bfi = make_shared<SymbolicBilinearFormIntegrator>(ab, vb, element_vb);
+      ab_bfi->SetSimdEvaluate(use_simd);
+      ab_bfi->SetBonusIntegrationOrder(bonus_intorder_ab);
+      ab_bfis.Append(ab_bfi);
+      auto bb_bfi = make_shared<SymbolicBilinearFormIntegrator>(bb, vb, element_vb);
+      bb_bfi->SetSimdEvaluate(use_simd);
+      bb_bfis.Append(bb_bfi);
+      bb_bfi->SetBonusIntegrationOrder(bonus_intorder_bb);
+    }
+ 
+    /** Create Matrix Graph **/
+    int dima = space_a->GetDimension(), dimb = space_b->GetDimension();
+
+    /** element equivalence classes **/
+    LocalHeap lh(10000000);
+
+    // Array<short> classnr(ma->GetNE());
+    // ma->IterateElements
+    //   (vb, lh, [&] (auto el, LocalHeap & llh)
+    //    {
+    //      classnr[el.Nr()] = 
+    //        SwitchET<ET_TRIG,ET_TET>
+    //        (el.GetType(),
+    //         [el] (auto et) { return ET_trait<et.ElementType()>::GetClassNr(el.Vertices()); });
+    //    });
+
+    /** #of equivalence classes per element type **/
+    Array<short> et_eqc_cnt(ET_HEX+1); et_eqc_cnt = 0;
+    ma->IterateElements
+      (vb, lh, [&] (auto el, LocalHeap & llh)
+       {
+	 short anum = 1 +
+	   SwitchET (el.GetType(),
+		     [&] (auto et) { return ET_trait<et.ElementType()>::GetClassNr(el.Vertices()); });
+         et_eqc_cnt[int(el.GetType())] = max(et_eqc_cnt[int(el.GetType())], anum);
+       });
+    /** prefix-sum so we have a unique nr for every element-type/element-eqc pair **/
+    Array<short> et_firsti(ET_HEX+2); et_firsti = 0;
+    for (auto k : Range(et_eqc_cnt))
+      { et_firsti[k+1] = et_eqc_cnt[k] + et_firsti[k]; }
+    /** actual class nrs **/
+    Array<short> classnr(ma->GetNE());
+    ma->IterateElements
+      (vb, lh, [&] (auto el, LocalHeap & llh)
+       {
+         classnr[el.Nr()] = 
+           SwitchET
+           (el.GetType(),
+            [&] (auto et) { return et_firsti[int(et)] + ET_trait<et.ElementType()>::GetClassNr(el.Vertices()); });
+       });
+    
+    TableCreator<size_t> creator;
+    for ( ; !creator.Done(); creator++)
+      for (auto i : Range(classnr))
+        creator.Add (classnr[i], i);
+    Table<size_t> table = creator.MoveTable();
+
+    /** assemble element matrix for every equivalence class **/
+    Array<int> cnt_b(space_b->GetNDof()); cnt_b = 0;
+
+    shared_ptr<BaseMatrix> op;
+
+    auto simd_guard = [&](auto some_lam) {
+      try { some_lam(); }
+      catch (ExceptionNOSIMD e) { /** Turn off SIMD and continue **/
+	for (auto bfi : ab_bfis)
+	  { bfi->SetSimdEvaluate(false); }
+	for (auto bfi : bb_bfis)
+	  { bfi->SetSimdEvaluate(false); }
+	some_lam();
+      }
+    };
+
+    for (auto elclass_inds : table)
+      {
+        HeapReset hr(lh);
+        if (elclass_inds.Size() == 0) continue;
+
+	ElementId ei(vb, elclass_inds[0]);
+
+        auto & eltrans = ma->GetTrafo(ei, lh);
+
+	auto & fela = space_a->GetFE (ei, lh); int nda = fela.GetNDof();
+        auto & felb = space_b->GetFE (ei, lh); int ndb = felb.GetNDof();
+        MixedFiniteElement felab(fela, felb);
+
+	FlatMatrix<SCAL> bamat(ndb*dimb, nda*dima, lh), bbmat(ndb*dimb, ndb*dimb, lh),
+	  elmat(ndb*dimb, nda*dima, lh);
+
+	simd_guard([&]() {
+	    bamat = 0.0; bbmat = 0.0;
+	    bool symmetric_so_far = true; // will be set to false here
+	    for (auto bfi : ab_bfis)
+	      { bfi->CalcElementMatrixAdd(felab, eltrans, bamat, symmetric_so_far, lh); }
+	    symmetric_so_far = true; // will probably be set to false
+	    for (auto bfi : bb_bfis)
+	      { bfi->CalcElementMatrixAdd(felb, eltrans, bbmat, symmetric_so_far, lh); }
+	  });
+
+	CalcInverse(bbmat);
+	elmat = bbmat * bamat;
+
+	Table<DofId> adofs(elclass_inds.Size(), nda*dima),
+          bdofs(elclass_inds.Size(), ndb*dimb);
+        Array<DofId> dnumsa, dnumsb;
+	for (auto i : Range(elclass_inds))
+          {
+            ElementId ei(vb, elclass_inds[i]);
+            space_a->GetDofNrs(ei, dnumsa);
+            space_b->GetDofNrs(ei, dnumsb);
+            for (auto d : dnumsb)
+              { cnt_b[d]++; }
+	    int c = 0;
+	    for (auto l : Range(dnumsa))
+	      for (auto ll : Range(dima))
+		adofs[i][c++] = dima * dnumsa[l] + ll;
+	    c = 0;
+	    for (auto l : Range(dnumsb))
+	      for (auto ll : Range(dimb))
+		bdofs[i][c++] = dimb * dnumsb[l] + ll;
+          }
+
+        auto mat = make_shared<ConstantElementByElementMatrix>
+          (space_b->GetNDof(), space_a->GetNDof(),
+           elmat, std::move(bdofs), std::move(adofs));
+
+        if (op != nullptr)
+          { op = make_shared<SumMatrix>(op, mat); }
+        else
+          { op = mat; }
+      }
+
+    if (op == nullptr) {
+      // dummy op for empty FESpace
+      Table<DofId> xdofs(0, 0), ydofs(0, 0);
+      Matrix<> mat(0,0);
+      op = make_shared<ConstantElementByElementMatrix>
+	(space_b->GetNDof(), space_a->GetNDof(),
+	 mat, std::move(ydofs), std::move(xdofs));
+    }
+
+#ifdef PARALLEL
+    if (space_b->IsParallel() && !localop)
+      { AllReduceDofData (cnt_b, MPI_SUM, space_b->GetParallelDofs()); }
+#endif
+
+    bool multiple = false;
+    for (auto c : cnt_b)
+      if (c > 1)
+	{ multiple = true; break; }
+
+    if (multiple || range_dofs)
+      {
+        VVector<> scaling(cnt_b.Size()*dimb);
+        for (auto dofnr : Range(cnt_b))
+	  if ( range_dofs && !range_dofs->Test(dofnr) )
+	    for (auto ll : Range(dimb))
+	      { scaling(dimb*dofnr+ll) = 0.0; }
+	  else
+	    for (auto ll : Range(dimb))
+	      { scaling(dimb*dofnr+ll) = cnt_b[dofnr] ? 1.0/cnt_b[dofnr] : 0.0; }
+	auto diagmat = make_shared<DiagonalMatrix<>> (scaling);
+        op = make_shared<ProductMatrix> (diagmat, op);
+      }
+
+    if ( parmat && space_a->IsParallel() ) {
+      op = make_shared<ParallelMatrix> (op, space_a->GetParallelDofs(), space_b->GetParallelDofs(),
+					localop ? PARALLEL_OP::C2C : PARALLEL_OP::C2D);
+    }
+
+    return op;
+  } // ConvertOperatorGF
+
+
+
   shared_ptr<BaseMatrix> ConvertOperator (shared_ptr<FESpace> space_a, shared_ptr<FESpace> space_b, VorB vb, LocalHeap & lh,
 					  shared_ptr<DifferentialOperator> diffop, shared_ptr<CoefficientFunction> trial_cf, const Region * reg,
 					  shared_ptr<BitArray> range_dofs, bool localop, bool parmat, bool use_simd,
-					  int bonus_intorder_ab, int bonus_intorder_bb)
+					  int bonus_intorder_ab, int bonus_intorder_bb, bool geom_free)
   {
     if ( space_a->IsComplex() != space_b->IsComplex() ) // b complex and a real could work in principle (?)
       { throw Exception("Cannot convert between complex and non-complex space!"); }
 
     shared_ptr<BaseMatrix> op;
 
-    /** This is a workaround because nested Switch does not work with gcc 7 **/
+    // This is a workaround because nested Switch does not work with gcc 7
     int dima = space_a->GetDimension();
-    Switch<MAX_SYS_DIM>(dima - 1, [&](auto DAMO) {
-	constexpr int DIMA = DAMO + 1;
-	int dimb = space_b->GetDimension();
-	if (dimb == 1) {
-	  if (space_b->IsComplex())
-	    { op = ConvertOperator<Complex, DIMA, 1> (space_a, space_b, diffop, trial_cf, vb, reg, lh, range_dofs, localop, parmat, use_simd,
-						      bonus_intorder_ab, bonus_intorder_bb); }
-	  else
-	    { op = ConvertOperator<double, DIMA, 1> (space_a, space_b, diffop, trial_cf, vb, reg, lh, range_dofs, localop, parmat, use_simd,
-						     bonus_intorder_ab, bonus_intorder_bb); }
-	}
-	else if (dimb == dima) {
-	  if (space_b->IsComplex())
-	    { op = ConvertOperator<Complex, DIMA, DIMA> (space_a, space_b, diffop, trial_cf, vb, reg, lh, range_dofs, localop, parmat, use_simd,
-							 bonus_intorder_ab, bonus_intorder_bb); }
-	  else
-	    { op = ConvertOperator<double, DIMA, DIMA> (space_a, space_b, diffop, trial_cf, vb, reg, lh, range_dofs, localop, parmat, use_simd,
+    if (geom_free) {
+      if (space_b->IsComplex())
+	{ op = nullptr; throw Exception("ConstEBE not availaible for scalar type Complex!"); }
+      else
+	{ op = ConvertOperatorGF<double> (space_a, space_b, diffop, trial_cf, vb, reg, lh, range_dofs, localop, parmat, use_simd,
+					  bonus_intorder_ab, bonus_intorder_bb); }
+    }
+    else { // geom_free
+      Switch<MAX_SYS_DIM>(dima - 1, [&](auto DAMO) {
+	  constexpr int DIMA = DAMO + 1;
+	  int dimb = space_b->GetDimension();
+	  if (dimb == 1) {
+	    if (space_b->IsComplex())
+	      { op = ConvertOperator<Complex, DIMA, 1> (space_a, space_b, diffop, trial_cf, vb, reg, lh, range_dofs, localop, parmat, use_simd,
 							bonus_intorder_ab, bonus_intorder_bb); }
-	}
-      });
+	    else
+	      { op = ConvertOperator<double, DIMA, 1> (space_a, space_b, diffop, trial_cf, vb, reg, lh, range_dofs, localop, parmat, use_simd,
+						       bonus_intorder_ab, bonus_intorder_bb); }
+	  }
+	  else if (dimb == dima) {
+	    if (space_b->IsComplex())
+	      { op = ConvertOperator<Complex, DIMA, DIMA> (space_a, space_b, diffop, trial_cf, vb, reg, lh, range_dofs, localop, parmat, use_simd,
+							   bonus_intorder_ab, bonus_intorder_bb); }
+	    else
+	      { op = ConvertOperator<double, DIMA, DIMA> (space_a, space_b, diffop, trial_cf, vb, reg, lh, range_dofs, localop, parmat, use_simd,
+							  bonus_intorder_ab, bonus_intorder_bb); }
+	  }
+	});
+    } // geom_free
 
     return op;
   } // ConvertOperator
--- comp/fesconvert.hpp
+++ comp/fesconvert.hpp
@@ -7,7 +7,7 @@ namespace ngcomp
   shared_ptr<BaseMatrix> ConvertOperator (shared_ptr<FESpace> spacea, shared_ptr<FESpace> spaceb, VorB vb, LocalHeap & lh,
 					  shared_ptr<DifferentialOperator> diffop = nullptr, shared_ptr<CoefficientFunction> trial_cf = nullptr,
 					  const Region * reg = NULL, shared_ptr<BitArray> range_dofs = nullptr, bool localop = false, bool parmat = true,
-					  bool use_simd = true, int bonus_intorder_ab = 0, int bonus_intorder_bb = 0);
+					  bool use_simd = true, int bonus_intorder_ab = 0, int bonus_intorder_bb = 0, bool geom_free = false);
 
 } // namespace ngcomp
 
