50619279e15765436547b5329e57dd4c6f8a8b3e
Matthias Hochsteger <mhochsteger@cerbsim.com>
2020-11-13 11:12:25 +0100

    gitlab-ci - set NG_BACKTRACE=1 in linux builds

--- tests/gitlab-ci/ubuntu/test.sh
+++ tests/gitlab-ci/ubuntu/test.sh
@@ -7,6 +7,7 @@ docker run \
       -e MKL_DOMAIN_THREADS="MKL_DOMAIN_PARDISO=6" \
       -e LD_LIBRARY_PATH=/opt/intel/mkl/lib/intel64 \
       -e RUN_SLOW_TESTS="$RUN_SLOW_TESTS" \
+      -e NG_BACKTRACE=1 \
       -v /opt/intel:/opt/intel \
       ngsolve_${CI_PIPELINE_ID}_installed:$IMAGE_NAME \
       bash -c 'cd /root/build/ngsolve && make test_ngsolve ARGS="--output-on-failure"'
