9657b3495cea4f9bd7feb7d1a0d64bf3957c0285
Umberto Zerbinati <uzerbinati@me.com>
2020-12-09 05:27:52 +0100

    Added SLEPc support.

--- python/ngs2petsc.py
+++ python/ngs2petsc.py
@@ -1,5 +1,6 @@
 import ngsolve as ngs
 import petsc4py.PETSc as psc
+import slepc4py.SLEPc as spc
 from mpi4py import MPI
 import numpy as np
 
@@ -117,6 +118,49 @@ class PETScPreconditioner(ngs.BaseMatrix):
 def MakePreconditioner(mat, freedofs):
     return PETScPreconditioner(mat, freedofs)
 
+class SLEPcEigenProblem():
+    def __init__(self,PType,SType):
+        self.E = spc.EPS().create()
+        if PType == "GHEP":
+            self.E.setProblemType(spc.EPS.ProblemType.GHEP) 
+        elif PType == "HEP":
+            self.E.setProblemType(spc.EPS.ProblemType.HEP)
+        elif PType == "NHEP":
+            self.E.setProblemType(spc.EPS.ProblemType.NHEP)
+        elif PType == "GNHEP":
+            self.E.setProblemType(spc.EPS.ProblemType.GNHEP)
+        self.E.setType(SType)
+    def setOperators(self,mats,Dofs):
+        if len(mats)==1:
+            self.A = CreatePETScMatrix(mats[0],Dofs);
+            self.E.setOperators(self.A)
+        elif len(mats)==2:
+            self.A = CreatePETScMatrix(mats[0],Dofs);
+            self.M = CreatePETScMatrix(mats[1],Dofs);
+            self.E.setOperators(self.A,self.M)
+    def SpectralTransformation(self,opt):
+        self.ST = self.E.getST();
+        self.ST.setType(opt);
+        self.KSP = self.ST.getKSP();
+    def setWhich(self,n):
+        self.E.setDimensions(n,psc.DECIDE)
+    def getPair(self,fes,s):
+        xr, xi = self.A.createVecs()
+        lam = self.E.getEigenpair(s, xr, xi)
+        vecmap = VectorMapping(fes.ParallelDofs(), fes.FreeDofs())
+        vr =  ngs.GridFunction(fes)
+        vecmap.P2N(xr, vr.vec)
+        vi =  ngs.GridFunction(fes)
+        vecmap.P2N(xi, vi.vec)
+        return lam,vr,vi
+    def Solve(self):
+        self.E.setST(self.ST);
+        self.E.solve();
+        self.Iterations = self.E.getIterationNumber()
+        self.Converged = self.E.getConverged()
+        sol_type = self.E.getType()
+        nev, ncv, mpd = self.E.getDimensions()
+        self.tolerance, Mit = self.E.getTolerances()
 from ngsolve.comp import RegisterPreconditioner
 RegisterPreconditioner ("gamg", MakePreconditioner)
 
