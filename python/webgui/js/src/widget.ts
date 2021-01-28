e81333e6608f0fee7301ac98332a5c2679937a77
Matthias Hochsteger <mhochsteger@cerbsim.com>
2021-01-28 08:05:30 +0100

    webgui - NGSDocuWebGuiWidget to show a preview screenshot and load solution data on demand

--- python/webgui/js/src/widget.ts
+++ python/webgui/js/src/widget.ts
@@ -22,6 +22,7 @@ export class NGSolveView extends DOMWidgetView {
     this.el.appendChild(container);
     setTimeout(()=> {
       this.scene.init(container, render_data);
+      this.scene.render();
     } , 0);
     this.model.on('change:value', this.data_changed, this);
   }
@@ -30,4 +31,43 @@ export class NGSolveView extends DOMWidgetView {
     this.scene.updateRenderData(render_data);
   }
 }
+
+export class NGSolveDocuView extends DOMWidgetView {
+  scene: Scene;
+
+  render() {
+    console.log("Render NGSDocuView");
+    let files = this.model.get("value");
+    console.log("data", files);
+    let container = document.createElement( 'div' );
+    container.setAttribute("style", "height: 50vw;");
+    let img = document.createElement('img');
+    img.setAttribute("src", files['preview']);
+    img.setAttribute("style", "width: 100%");
+    img.onclick = (el) => this.onClickImage(el);
+    container.appendChild(img);
+    this.el.appendChild(container);
+    this.model.on('change:value', this.data_changed, this);
+  }
+  onClickImage(el) {
+      console.log("clicked image", el);
+      document.body.style.cursor = "wait";
+          let files = this.model.get("value");
+          $.get(files['render_data'], (render_data) => {
+              document.body.style.cursor = "";
+              let pel = this.el.children[0];
+              pel.innerHTML = "";
+              console.log("got render data", render_data);
+              let scene = new Scene();
+              scene.init(pel, render_data);
+          });
+  }
+
+  data_changed() {
+    let render_data = this.model.get("value");
+    console.log("got new render data", render_data);
+    this.scene.updateRenderData(render_data);
+  }
+}
+
 
