1b066012daac75caa7b8778461b60f3c373f8b19
Matthias Hochsteger <mhochsteger@cerbsim.com>
2021-01-29 20:14:07 +0100

    some more chromium flags

--- python/webgui/webgui_template.py
+++ python/webgui/webgui_template.py
@@ -189,8 +189,14 @@ class WebGLScene:
         from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
         options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        options.add_argument('no-sandbox')
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        # https://stackoverflow.com/questions/54297559/getting-cannot-activate-web-view
+        options.add_argument("--disable-infobars")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-browser-side-navigation")
+        #options.add_argument("--disable-gpu")
+        options.add_argument("--disable-features=VizDisplayCompositor")
 
         options.add_argument('window-size=1200x600')
 
