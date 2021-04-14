16c62034896c1d83f72eed470c1761dcd830b874
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2021-04-14 21:01:10 +0200

    Prolongation.Operator

--- multigrid/prolongation.hpp
+++ multigrid/prolongation.hpp
@@ -23,7 +23,7 @@ namespace ngmg
   
     ///
     virtual void Update (const FESpace & fes) = 0;
-
+    virtual size_t GetNDofLevel (int level) { throw Exception("Prolongation::GetNDofLevel not overloaded"); }
     ///
     virtual shared_ptr<SparseMatrix< double >> CreateProlongationMatrix( int finelevel ) const = 0;
     ///
@@ -35,6 +35,39 @@ namespace ngmg
   };
 
 
+  class ProlongationOperator : public BaseMatrix
+  {
+    shared_ptr<Prolongation> prol;
+    int level;
+  public:
+    ProlongationOperator (shared_ptr<Prolongation> aprol, int alevel)
+      : prol(aprol), level(alevel) { } 
+
+    virtual bool IsComplex() const override { return false; } 
+
+    virtual int VHeight() const override { return prol->GetNDofLevel(level); }
+    virtual int VWidth() const override { return prol->GetNDofLevel(level-1); }
+
+    virtual void Mult (const BaseVector & x, BaseVector & y) const override
+    {
+      y.Range(0, VWidth()) = x;
+      prol->ProlongateInline (level, y);
+    }
+    virtual void MultTrans (const BaseVector & x, BaseVector & y) const override
+    {
+      auto tmp = x.CreateVector();
+      tmp = x;
+      prol->RestrictInline (level, tmp);
+      y = tmp.Range(0, VWidth());
+    }
+
+    AutoVector CreateRowVector() const override { return make_unique<VVector<double>> (VWidth()); }
+    AutoVector CreateColVector() const override { return make_unique<VVector<double>> (VHeight()); }
+  };
+  
+
+
+  
   /**
      Standard Prolongation.
      Child nodes between 2 parent nodes.
@@ -51,7 +84,8 @@ namespace ngmg
     virtual ~LinearProlongation(); 
 
     virtual void Update (const FESpace & fes) override;
-
+    virtual size_t GetNDofLevel (int level) override { return nvlevel[level]; }
+    
     virtual shared_ptr<SparseMatrix< double >> CreateProlongationMatrix( int finelevel ) const override;
     virtual void ProlongateInline (int finelevel, BaseVector & v) const override;
     virtual void RestrictInline (int finelevel, BaseVector & v) const override;
