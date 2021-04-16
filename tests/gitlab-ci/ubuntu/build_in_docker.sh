9492a02cc479ddd1914f1f21fe76ea0c618454cb
Matthias Hochsteger <mhochsteger@cerbsim.com>
2021-04-16 18:08:53 +0200

    Revert "show output"
    This reverts commit 33f15badd302b08a98ad73e977fd9eaee06815fc.

--- tests/gitlab-ci/ubuntu/build_in_docker.sh
+++ tests/gitlab-ci/ubuntu/build_in_docker.sh
@@ -79,7 +79,7 @@ then
   pip3 install webgui_jupyter_widgets
   jupyter nbextension install --py webgui_jupyter_widgets
   jupyter nbextension enable --py webgui_jupyter_widgets
-  make docs
+  make docs > out
   find ~/src/ngsolve/docs/i-tutorials -name '*.ipynb' -print0 | xargs -0 nbstripout
   cp -r ~/src/ngsolve/docs/i-tutorials docs/html/jupyter-files
   zip -r docs/html/i-tutorials.zip docs/html/jupyter-files
