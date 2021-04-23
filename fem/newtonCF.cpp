41a5dd7e6bd6f75c6bc378de7ebdf194fd8ff335
Matthias Rambausek <matthias.rambausek@gmail.com>
2021-04-23 16:22:33 +0200

    todo notes in newtonCF.cpp

--- fem/newtonCF.cpp
+++ fem/newtonCF.cpp
@@ -1,6 +1,6 @@
 /*********************************************************************/
 /* File:   newtonCF.cpp                                              */
-/* Author: Joachim Schoeberl                                         */
+/* Authors: Joachim Schoeberl, Matthias Rambausek                    */
 /* Date:   Feb 2021                                                  */
 /*********************************************************************/
 
@@ -9,20 +9,31 @@
 namespace ngfem
 {
 
+  // TODO: provide also a mere NewtonCF for general nonlinear systems
+    
+    
   class MinimizationCF : public CoefficientFunction
   {
     shared_ptr<CoefficientFunction> expression;
     shared_ptr<CoefficientFunction> startingpoint;
 
-    ProxyFunction * proxy = nullptr;
+    std::vector<ProxyFunction *> proxies;
     Array<CoefficientFunction*> cachecf;
+    
+    // same parameters as scipy's newton
+    // Alternatively, could one think of ParameterCFs here?
+    double tol{1e-8};
+    double rtol{0.0};
+    int maxiter{10};
+    
   public:
     MinimizationCF (shared_ptr<CoefficientFunction> aexpression,
-                    shared_ptr<CoefficientFunction> astartingpoint)
+                    shared_ptr<CoefficientFunction> astartingpoint,
+                    std::optional<double> atol, 
+                    std::optional<double> artol, 
+                    std::optional<int> amaxiter)
       : expression(aexpression), startingpoint(astartingpoint)
     {
-      SetDimensions (startingpoint->Dimensions());
-      
       expression->TraverseTree
         ( [&] (CoefficientFunction & nodecf)
           {
@@ -31,14 +42,47 @@ namespace ngfem
               {
                 if (!nodeproxy->IsTestFunction())
                   {
-                    if (proxy && proxy != nodeproxy) throw Exception("MinimizeCF: only one proxy allowed");
-                    proxy = nodeproxy;
+                    if (std::find(begin(proxies), end(proxies), nodeproxy) == end(proxies))
+                      proxies.push_back(nodeproxy);
                   }
               }
             else if (nodecf.StoreUserData() && !cachecf.Contains(&nodecf))
               cachecf.Append (&nodecf);
           });
-      if (!proxy) throw Exception("MinimizedCF: don't have a proxy");
+      if (proxies.empty()) throw Exception("MinimizedCF: don't have a proxy");
+      if (proxies.size() > 1) throw Exception("MinimizedCF: only a single proxy is support ATM");
+      
+      // TODO: support case proxies.size() > 1
+      // All proxies must originate from one FE space. However, there is no way to check this.
+      // Available information for proxies:
+      //  - block index: via Proxy Evaluator if this can be cast to a CompoundDifferentialOperator
+      //  - block size: via Proxy Evaluator
+      //  - dimension: should be the same as block size?
+      //  - dims: array with dimensions for each axis
+      
+      // For the CF (starting point):
+      //  - dim, dims (misleading for GF of CompoundFESpace)
+      //  - components (CFs) for GF of CompoundFESpace
+      
+      // Strategy: 
+      //  1. If more that one proxy has been found, sort proxies by block index; only allow each block index appear exactly once.
+      //     IOW, two different proxies with for some reason same block index are forbidden.
+      //  2. Determine cumulated dimensions of collected proxies.
+      //  3. Compare cumulated dimension with dimension of startingpoint.
+      //     '-> In case of mismatch, try whether startingpoint has components
+      //     '-> In case of multiple proxies and startingpoint being a GF, the latter must have 
+      //         components corresponding to the proxies
+      //  4. Call SetDimensions with the appropriate information.
+      
+      // Note: it would be nice, if GF on product space have proper dimensions
+      SetDimensions (startingpoint->Dimensions());
+      
+      if (atol)
+        tol = *atol;
+      if (artol)
+        rtol = *artol;
+      if (amaxiter)
+        maxiter = *amaxiter;
     }
     
 
@@ -70,7 +114,8 @@ namespace ngfem
         ud.AssignMemory (cf, mir.Size(), cf->Dimension(), lh);
       
       const_cast<ElementTransformation&>(trafo).userdata = &ud;
-      // ud.fel = &fel;   // how do I get that ? looks like we don't need it anymore ???
+      
+      auto proxy = proxies[0];
       ud.AssignMemory (proxy, mir.Size(), proxy->Dimension(), lh);
 
       FlatMatrix xk { ud.GetMemory(proxy) };
@@ -78,6 +123,8 @@ namespace ngfem
       startingpoint -> Evaluate (mir, xk);
       // cout << "starting value = " << ud.GetMemory(proxy) << endl;
 
+      
+      // TODO: support multiple proxies (compound spaces)
       
       FlatMatrix<> dderiv(mir.Size(), 1,lh);
       FlatMatrix<AutoDiffDiff<1,double>> ddval(mir.Size(), 1, lh);
@@ -88,16 +135,42 @@ namespace ngfem
       FlatMatrix<> w(mir.Size(), proxy->Dimension(), lh);
       FlatMatrix<> xold(mir.Size(), proxy->Dimension(), lh);
 
-      for (int step = 0; step < 5; step++)
+      auto proxy1 = proxy;
+      auto proxy2 = proxy;
+      FlatTensor<3> proxyvalues(lh, mir.Size(), proxy2->Dimension(), proxy1->Dimension());
+      
+      FlatVector<> rhs(proxy2->Dimension(), lh);
+      FlatVector<> sol(proxy1->Dimension(), lh);
+      FlatMatrix<> mat(proxy2->Dimension(), proxy1->Dimension(), lh);
+      
+      const auto vsemb = proxy->Evaluator()->GetVSEmbedding();
+      const auto proj_dim_1 = vsemb ? vsemb->Width() : 0;
+      const auto proj_dim_2 = vsemb ? vsemb->Width() : 0;
+      FlatVector<> proj_rhs(proj_dim_2, lh);
+      FlatVector<> proj_sol(proj_dim_1, lh);
+      FlatMatrix<> proj_mat(proj_dim_2, proj_dim_1, lh);
+      
+      
+      // TODO: use Linf norm
+      const auto converged = [&](const auto& rhs_vec){
+        const auto res = L2Norm(res);
+        return res <= tol || (res / res_0) <= rtol;
+      }
+      
+      // TODO: Algorithm cleanup: reorder steps
+      //   1. [per quadrature point] Do not compute (off-diagonal)/invert hessian if already converged
+      //   2. Unified algorithm for vsemb true/false; maybe via lambdas returning refs to the
+      //      vectors and matrices that hold the respective results
+      
+      // TODO: exploit symmetry in hessian computation
+      
+      
+      // How to compute norms for tolerance/convergence evaluation?
+      for (int step = 0; step < maxiter; step++)
         {
           // cout << "step = " << step; // << ", starting: " << endl << xk << endl;
       
           double energy = 0;
-
-          auto proxy1 = proxy;
-          auto proxy2 = proxy;
-          FlatTensor<3> proxyvalues(lh, mir.Size(), proxy2->Dimension(), proxy1->Dimension());
-
           
           {
             // RegionTracer regtr1(TaskManager::GetThreadId(), t1); 
@@ -148,16 +221,20 @@ namespace ngfem
           // newton step
           // RegionTracer regtr2(TaskManager::GetThreadId(), t2);    
           // cout << "proxy diffop = " <<  typeid(* (proxy->Evaluator())).name() << endl;
-          if (auto vsemb = proxy->Evaluator()->GetVSEmbedding(); vsemb)
+          if (vsemb)
             {
+              const auto embmat = *vsemb;
               for (int i = 0; i < mir.Size(); i++)
                 {
-                  Matrix mat = proxyvalues(i, STAR,STAR);
-                  Vector rhs = dWdB.Row(i);
-
-                  Vector proj_rhs = Trans(*vsemb) * rhs;
-                  Matrix proj_mat = Trans(*vsemb) * mat * *vsemb;
-                  Vector proj_sol(proj_rhs.Size());
+                  rhs = dWdB.Row(i);
+                  if (converged(rhs))
+                      continue;
+                  
+                  mat = proxyvalues(i, STAR,STAR);
+                  
+                  proj_rhs = Trans(embmat) * rhs;
+                  proj_mat = Trans(embmat) * mat * embmat;
+                  proj_sol(proj_rhs.Size());
 
                   // cout << "proj rhs = " << proj_rhs << endl;
                   // cout << "proj mat = " << proj_mat << endl;
@@ -167,7 +244,7 @@ namespace ngfem
                   proj_sol = proj_mat * proj_rhs;
                   // cout << "proj sol = " << proj_sol << endl;
                   
-                  w.Row(i) = *vsemb * proj_sol;
+                  w.Row(i) = embmat * proj_sol;
                   // cout << "sol = " << w << endl;
                 }
             }
@@ -176,9 +253,12 @@ namespace ngfem
             
             for (int i = 0; i < mir.Size(); i++)
               {
-                Matrix mat = proxyvalues(i, STAR,STAR);
-                Vector rhs = dWdB.Row(i);
-                Vector sol(rhs.Size());
+                rhs = dWdB.Row(i);
+                if (converged(rhs))
+                    continue;
+                
+                mat = proxyvalues(i, STAR,STAR);
+                sol(rhs.Size());
                 CalcInverse (mat);
                 w.Row(i) = mat * rhs;
                 // cout << "sol = " << sol << endl;
@@ -222,6 +302,6 @@ namespace ngfem
   CreateMinimizationCF (shared_ptr<CoefficientFunction> expression,
                         shared_ptr<CoefficientFunction> startingpoint)
   {
-    return make_shared<MinimizationCF> (expression, startingpoint);
+    return make_shared<MinimizationCF> (expression, startingpoint, std::optional<double>{}, std::optional<double>{}, std::optional<int>{});
   }
 }
