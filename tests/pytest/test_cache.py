02786cc36e5951645f5688e79a7c83b19ae47eb2
Matthias Rambausek <matthias.rambausek@gmail.com>
2021-06-07 12:00:47 +0200

    completed (trivial) caching tests for most important cases

--- tests/pytest/test_cache.py
+++ tests/pytest/test_cache.py
@@ -1,9 +1,11 @@
+import numpy as np
+import pathlib
+import pytest
 from ngsolve import *
 from netgen.csg import *
 ngsglobals.msg_level = 0
 from ngsolve.fem import LoggingCF
-
-import pytest
+from ngsolve.fem import NewtonCF
 
 
 def mk_fes():
@@ -11,39 +13,89 @@ def mk_fes():
     return VectorH1(mesh)
 
 
+def mk_cf(fes):
+    return CoefficientFunction((x, y, z))
+
+
+def mk_newton_cf(fes):
+    _cf = mk_cf(fes)
+    return NewtonCF(fes.TrialFunction() - _cf, _cf, maxiter=1)
+
+
 @pytest.fixture
 def fes():
     return mk_fes()
 
 
-def test_cache_in_linear_form_integrator(fes):
-    cf = CacheCF(LoggingCF(CoefficientFunction((1,)*len(fes.components)),
-                           logfile="test_cache_in_linear_form_integrator.log"))
+def line_count(filename):
+    with open(filename, "r") as lf:
+        lines = list(lf.readlines())
+        print(len(lines))
+    return len(lines)
+
+
+def mk_logfile(filename):
+    logfile = pathlib.Path(filename)
+    if logfile.exists():
+        logfile.unlink(missing_ok=True)
+    return logfile
+
+# TODO: add more cases to cover all possible occurrences of caching (a lot!)
+
+
+def mk_linear_form(fes, cf):
     L = LinearForm(fes)
-    L += InnerProduct(cf, CoefficientFunction(tuple(fes.TestFunction()))) * dx
+    test_function = fes.TestFunction()
+    L += InnerProduct(cf, test_function) * dx
+    L += InnerProduct(cf, test_function) * ds(element_boundary=True)
     L.Assemble()
+    return L
+
+
+def test_cache_in_linear_form_integrator(fes):
+    L = mk_linear_form(fes, mk_cf(fes))
+
+    # covers simd version of LFIntegrator :: T_CalcElementVector (also element_vb != VOL)
+    logfile = mk_logfile("test_cache_in_linear_form_integrator_cf.log")
+    cL = mk_linear_form(fes, CacheCF(LoggingCF(mk_cf(fes), logfile=str(logfile))))
+    assert np.allclose(cL.vec.FV().NumPy(), L.vec.FV().NumPy(), atol=1e-15, rtol=0)
+    assert line_count(logfile) == 635
+
+    # covers non-simd version of LFIntegrator :: T_CalcElementVector (also element_vb != VOL)
+    logfile = mk_logfile("test_cache_in_linear_form_integrator_ncf.log")
+    cL = mk_linear_form(fes, CacheCF(LoggingCF(mk_newton_cf(fes), logfile=str(logfile))))
+    assert np.allclose(cL.vec.FV().NumPy(), L.vec.FV().NumPy(), atol=1e-15, rtol=0)
+    assert line_count(logfile) == 773
+
+
+def mk_bilinear_form(fes, cf):
+    a = BilinearForm(fes)
+    trial_function, test_function = fes.TnT()
+    a += InnerProduct(cf, trial_function) * \
+         InnerProduct(cf, test_function) * dx
+    a += InnerProduct(cf, trial_function) * \
+         InnerProduct(cf, test_function) * ds(element_boundary=True)
+    a.Assemble()
+    return a
 
 
 def test_cache_in_bilinear_form_integrator(fes):
-    cf = CacheCF(LoggingCF(CoefficientFunction((1,)*len(fes.components)),
-                           logfile="test_cache_in_bilinear_form_integrator.log"))
-    a = BilinearForm(fes)
-    a += InnerProduct(cf, CoefficientFunction(tuple(fes.TrialFunction()))) * \
-         InnerProduct(cf, CoefficientFunction(tuple(fes.TestFunction()))) * dx
-    a.Assemble()
+    a = mk_bilinear_form(fes, mk_cf(fes))
 
+    # covers simd version of BLFIntegrator :: T_CalcElementMatrixAdd and T_CalcElementMatrixEBAdd
+    logfile = mk_logfile("test_cache_in_bilinear_form_integrator_cf.log")
+    ca = mk_bilinear_form(fes, CacheCF(LoggingCF(mk_cf(fes), logfile=str(logfile))))
+    assert np.allclose(ca.mat.AsVector().FV().NumPy(), a.mat.AsVector().FV().NumPy(), atol=1e-15, rtol=0)
+    assert line_count(logfile) == 527
 
-def test_cache_in_symbolic_energy(fes):
-    cf = CacheCF(LoggingCF(CoefficientFunction((1,)*len(fes.components)),
-                           logfile="test_cache_in_symbolic_energy.log"))
-    u = CoefficientFunction(tuple(fes.TrialFunction()))
-    a = BilinearForm(fes)
-    a += InnerProduct(cf, u) * InnerProduct(cf, u) * dx
-    a.Assemble()
+    # covers non-simd version of BLFIntegrator :: T_CalcElementMatrixAdd and T_CalcElementMatrixEBAdd
+    logfile = mk_logfile("test_cache_in_bilinear_form_integrator_ncf.log")
+    ca = mk_bilinear_form(fes, CacheCF(LoggingCF(mk_newton_cf(fes), logfile=str(logfile))))
+    assert np.allclose(ca.mat.AsVector().FV().NumPy(), a.mat.AsVector().FV().NumPy(), atol=1e-15, rtol=0)
+    assert line_count(logfile) == 779
 
 
 if __name__ == "__main__":
     _fes = mk_fes()
     test_cache_in_linear_form_integrator(_fes)
     test_cache_in_bilinear_form_integrator(_fes)
-    test_cache_in_symbolic_energy(_fes)
