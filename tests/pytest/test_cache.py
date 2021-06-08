a3218cc7a8eaa3fb5758c6feb5cf4dc9e4cb6612
Matthias Rambausek <matthias.rambausek@gmail.com>
2021-06-08 13:07:52 +0200

    added DG test

--- tests/pytest/test_cache.py
+++ tests/pytest/test_cache.py
@@ -9,13 +9,27 @@ from ngsolve.fem import LoggingCF
 from ngsolve.fem import NewtonCF
 
 
+# TODO: add more cases to cover all possible occurrences of caching
+#  still missing:
+#   * SymbolicBilinearFormIntegrator :: T_CalcElementMatrixAddShapeWise and all "Trans" versions
+#   * SymbolicFacetBilinearFormIntegrator :: ApplyFromTraceValues
+
+
 def mk_fes():
     mesh = Mesh(unit_cube.GenerateMesh(maxh=1))
     return VectorH1(mesh)
 
 
+def mk_dg_fes():
+    mesh = Mesh(unit_cube.GenerateMesh(maxh=1))
+    return L2(mesh, dgjumps=True)
+
+
 def mk_cf(fes):
-    return CoefficientFunction((x, y, z))
+    if fes.type == "VectorH1":
+        return CoefficientFunction((x, y, z))
+    else:
+        return CoefficientFunction(x * y * z)
 
 
 def mk_newton_cf(fes):
@@ -26,6 +40,11 @@ def mk_newton_cf(fes):
 @pytest.fixture
 def fes():
     return mk_fes()
+
+
+@pytest.fixture
+def dg_fes():
+    return mk_dg_fes()
 
 
 def eval_count(filename):
@@ -41,9 +60,6 @@ def mk_logfile(filename):
     if logfile.exists():
         logfile.unlink(missing_ok=True)
     return logfile
-
-
-# TODO: add more cases to cover all possible occurrences of caching: still missing: "Trans" versions
 
 
 def mk_linear_form(fes, cf):
@@ -185,9 +201,58 @@ def test_cache_in_facet_bilinear_form_integrator(fes):
     assert eval_count(logfile) == 25
 
 
+def mk_facet_dg_bilinear_form(dg_fes, cf):
+    fes = dg_fes
+    du, dv = fes.TnT()
+    jump_du = du - du.Other()
+    jump_dv = dv - dv.Other()
+
+    a = BilinearForm(fes)
+    a += InnerProduct(cf, cf) * jump_du * jump_dv * dx(skeleton=True)
+    a.Assemble()
+    return a
+
+
+def test_cache_in_facet_bilinear_form_integrator_dg(dg_fes):
+    fes = dg_fes
+    gf = GridFunction(fes)
+    gfvec = gf.vec
+    resvec = gf.vec.Copy()
+    gf.Interpolate(CoefficientFunction(x * y * z))
+
+    logfile = mk_logfile("test_cache_in_facet_bilinear_form_integrator_dg_nocache.log")
+    a = mk_facet_dg_bilinear_form(fes, LoggingCF(mk_cf(fes), logfile=str(logfile)))
+    a.Apply(gfvec, resvec)
+    baseline = eval_count(logfile)
+
+    # covers simd version of FacetBLFIntegrator :: T_CalcFacetMatrix, ApplyFacetMatrix (both "2-element signature")
+    logfile = mk_logfile("test_cache_in_facet_bilinear_form_integrator_dg_cf.log")
+    ca = mk_facet_dg_bilinear_form(fes, CacheCF(LoggingCF(mk_cf(fes), logfile=str(logfile))))
+    assert eval_count(logfile) == 18
+    assert np.allclose(ca.mat.AsVector().FV().NumPy(), a.mat.AsVector().FV().NumPy(), atol=1e-15, rtol=0)
+    cresvec = gfvec.Copy()
+    ca.Apply(gfvec, cresvec)
+    assert np.allclose(cresvec.FV().NumPy(), resvec.FV().NumPy(), atol=1e-15, rtol=0)
+    assert eval_count(logfile) == 36
+
+    # covers non-simd version of FacetBLFIntegrator :: T_CalcFacetMatrix, ApplyFacetMatrix (both "2-element signature")
+    logfile = mk_logfile("test_cache_in_facet_bilinear_form_integrator_ncf.log")
+    ca = mk_facet_dg_bilinear_form(fes, CacheCF(LoggingCF(mk_newton_cf(fes), logfile=str(logfile))))
+    assert np.allclose(ca.mat.AsVector().FV().NumPy(), a.mat.AsVector().FV().NumPy(), atol=1e-15, rtol=0)
+    # NOTE: currently no simd version exists and the count for simd and non.simd are thus the same
+    assert eval_count(logfile) == 18
+    cresvec = gfvec.Copy()
+    ca.Apply(gfvec, cresvec)
+    assert np.allclose(cresvec.FV().NumPy(), resvec.FV().NumPy(), atol=1e-15, rtol=0)
+    assert eval_count(logfile) == 37
+
+
 if __name__ == "__main__":
     _fes = mk_fes()
     test_cache_in_linear_form_integrator(_fes)
     test_cache_in_bilinear_form_integrator(_fes)
     test_cache_in_facet_linear_form_integrator(_fes)
     test_cache_in_facet_bilinear_form_integrator(_fes)
+
+    _dg_fes = mk_dg_fes()
+    test_cache_in_facet_bilinear_form_integrator_dg(_dg_fes)
