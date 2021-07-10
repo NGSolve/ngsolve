240553973b74c0696f6c82e1a1b7e72686972b5e
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2021-07-10 08:22:05 +0200

    [vtkoutput] documentation / some renaming / minors

--- py_tutorials/vtkout.py
+++ py_tutorials/vtkout.py
@@ -6,7 +6,7 @@ from math import pi
 from netgen.geom2d import unit_square
 from netgen.meshing import MeshingParameters
 
-ngsglobals.msg_level = 1
+ngsglobals.msg_level = 4
 
 mesh = Mesh (unit_square.GenerateMesh(maxh=0.3))
 
@@ -14,9 +14,11 @@ v = FESpace(type="hdivho",mesh=mesh, order=1, dirichlet=[1,2,3])
 
 u = GridFunction (space=v)
 
-u.vec[37]=1.0
-
-vtk = VTKOutput(ma=mesh,coefs=[u],names=["sol"],filename="vtkout_",subdivision=3)
-vtk.Do()
+vtk = VTKOutput(mesh=mesh,coefs=[u],names=["sol"],filename="vtkout",subdivision=3,floatsize="single",legacy=False)
+for i in range(13,26):
+    u.vec[:]=0.0
+    u.vec[i]=1.0
+    outputfilename = vtk.Do(time=i/100)
+    print("VTKOutput has been generated in", outputfilename)
 
 Draw (u)
