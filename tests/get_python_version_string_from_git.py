1e3d33247cbd730b724c2c2b26b668caa3b11b8b
Matthias Hochsteger <mhochsteger@cerbsim.com>
2022-04-19 18:12:28 +0200

    fix python versioning for prerelease builds

--- tests/get_python_version_string_from_git.py
+++ tests/get_python_version_string_from_git.py
@@ -10,6 +10,10 @@ git_version = check_output(['git', 'describe', '--tags'], cwd=cwd).decode('utf-8
 version = git_version[1:].split('-')
 if len(version)>2:
     version = version[:2]
-version = '.dev'.join(version)
+if len(version)>1:
+    version = '.post'.join(version) + '.dev'
+else:
+    version = version[0]
 
 print(version)
+
