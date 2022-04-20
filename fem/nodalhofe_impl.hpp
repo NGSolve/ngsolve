f9765f20316b7511b54a263c8e639479231ca225
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2022-04-20 13:49:45 +0200

    nodal tet

--- fem/nodalhofe_impl.hpp
+++ fem/nodalhofe_impl.hpp
@@ -81,4 +81,61 @@ namespace ngfem
             }
       }
   }
+
+
+  template <> template<typename Tx, typename TFA>  
+  void NodalHOFE<ET_TET> :: T_CalcShape (TIP<3,Tx> ip, TFA & shape) const
+  {
+    Tx lam[4] = { ip.x, ip.y, ip.z, 1-ip.x-ip.y-ip.z };
+
+    for (int i = 0; i < 4; i++)
+      shape[i] = NodalShapePower (lam[i], order, order);
+
+
+    int ii = 4;
+    // edge-based shapes
+    for (int i = 0; i < N_EDGE; i++)
+      { 
+        INT<2> e = GetVertexOrientedEdge(i);
+
+        auto ls = lam[e[0]];
+        auto le = lam[e[1]];
+        for (int j = 1; j < order; j++)
+          {
+            shape[ii++] =
+              NodalShapePower(ls, j, order) * 
+              NodalShapePower(le, order-j, order);
+          }
+      }
+
+    for (int i = 0; i < N_FACE; i++)
+      { 
+        INT<4> f = GetVertexOrientedFace(i);
+
+        auto ls = lam[f[0]];
+        auto le = lam[f[1]];
+        auto lo = lam[f[2]];
+        for (int j = 1; j < order; j++)
+          for (int k = 1; j+k < order; k++)
+            {
+              shape[ii++] =
+                NodalShapePower(ls, j, order) * 
+                NodalShapePower(le, k, order) *
+                NodalShapePower(lo, order-j-k, order);              
+            }
+      }
+
+    for (int j = 1; j < order; j++)
+      for (int k = 1; j+k < order; k++)
+        for (int l = 1; j+k+l < order; k++)
+          {
+            shape[ii++] =
+              NodalShapePower(lam[0], j, order) * 
+              NodalShapePower(lam[1], k, order) *
+              NodalShapePower(lam[2], l, order) *
+              NodalShapePower(lam[3], order-j-k-l, order);                          
+          }
+  }
+
+  
 }
