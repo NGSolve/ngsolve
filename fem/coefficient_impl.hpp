6e362c6ff85d381b2e78588920c630451188ead6
Matthias Rambausek <matthias.rambausek@tuwien.ac.at>
2022-04-22 09:07:55 +0200

    move GenericIdentity struct to cf_impl header

--- fem/coefficient_impl.hpp
+++ fem/coefficient_impl.hpp
@@ -120,6 +120,14 @@ namespace ngfem
   
   };
 
+
+  struct GenericIdentity
+  {
+    template <typename T> T operator() (T x) const { return x; }
+    static string Name() { return  " "; }
+    void DoArchive(Archive& ar) {}
+  };
+
 }
 
 #endif
