258e990fbc1deb611ed06ea845ab4923f520297d
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2022-07-15 20:53:56 +0200

    LOBPCG eigenvalue solver

--- python/solvers.py
+++ python/solvers.py
@@ -1,4 +1,4 @@
-from ngsolve.eigenvalues import PINVIT
+from ngsolve.eigenvalues import PINVIT, LOBPCG
 from ngsolve.krylovspace import CG, QMR, MinRes, PreconditionedRichardson, GMRes
 from ngsolve.nonlinearsolvers import Newton, NewtonMinimization
 from ngsolve.bvp import BVP
