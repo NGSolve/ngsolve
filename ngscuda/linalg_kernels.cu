65e443a83745189374313be6a24baaa64b1c243c
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2022-12-12 20:46:25 +0100

    kernels for ebe-const matrix

--- ngscuda/linalg_kernels.cu
+++ ngscuda/linalg_kernels.cu
@@ -41,3 +41,39 @@ void MultAddDiagonal (int n, double alpha, double * D, double * x, double * y)
 
 
 
+
+/* ************** kernels for ConstantEBE Matrix ********************** */
+
+void ConstEBEKernelCopyInKernel (int numblocks, FlatTable<int> row_dnums, double * dev_ux, double * dev_hx)
+{
+  int tid = threadIdx.x;
+  int wm = row_dnums[0].Size();
+  
+  for (int r = 0; r < numblocks; r++)
+    {
+      for (int i = tid; i < wm; i += blockDim.x)
+        dev_hx[r*wm+i] = dev_ux[row_dnums[r][i]];
+    }
+}
+
+void ConstEBEKernelCopyIn (int numblocks, const DevTable<int> & row_dnums, double * dev_ux, double * dev_hx)
+{
+  ConstEBEKernelCopyInKernel<<<1,128>>> (numblocks, row_dnums, dev_ux, dev_hx)
+}
+
+void ConstEBEKernelCopyOutKernel (int numblocks, FlatTable<int> col_dnums, double * dev_hy, double * dev_uy)
+{
+  int tid = threadIdx.x;
+  int hm = col_dnums[0].Size();
+  
+  for (int r = 0; r < numblocks; r++)
+    {
+      for (int i = tid; i < hm; i += blockDim.x)
+        dev_uy[col_dnums[r][i]] += dev_hx[r*hm+i];
+    }
+}
+
+void ConstEBEKernelCopyOut (int numblocks, const DevTable<int> & col_dnums, double * dev_hy, double * dev_uy)
+{
+  ConstEBEKernelCopyOutKernel<<<1,128>>> (numblocks, col_dnums, dev_hy, dev_uy)
+}
