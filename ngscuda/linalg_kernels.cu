2b77a9518f9c62d5841f294d746588cbef8f81cf
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-02-01 22:54:36 +0100

    kernel blocksize

--- ngscuda/linalg_kernels.cu
+++ ngscuda/linalg_kernels.cu
@@ -48,28 +48,30 @@ void MultAddDiagonal (int n, double alpha, double * D, double * x, double * y)
 __global__ void ConstEBEKernelCopyInKernel (int numblocks, int bs, int * row_dnums, double * dev_ux, double * dev_hx)
 {
   int tid = threadIdx.x;
-  for (int r = blockIdx.x; r < numblocks; r+=gridDim.x)
+  for (int r = blockIdx.x*blockDim.y+blockIdx.y; r < numblocks; r+=gridDim.x*blockDim.y)
     for (int i = tid; i < bs; i += blockDim.x)
       dev_hx[r*bs+i] = dev_ux[row_dnums[r*bs+i]];
 }
 
 void ConstEBEKernelCopyIn (int numblocks, int bs, int * row_dnums, double * dev_ux, double * dev_hx)
 {
-  ConstEBEKernelCopyInKernel<<<512,256>>> (numblocks, bs, row_dnums, dev_ux, dev_hx);
+   // ConstEBEKernelCopyInKernel<<<512,256>>> (numblocks, bs, row_dnums, dev_ux, dev_hx);
+   ConstEBEKernelCopyInKernel<<<512,dim3(16,16)>>> (numblocks, bs, row_dnums, dev_ux, dev_hx);
 }
 
 __global__ void ConstEBEKernelCopyOutKernel (int numblocks, int bs, int *  col_dnums, double * dev_hy, double * dev_uy)
 {
   int tid = threadIdx.x;
 
-  for (int r = blockIdx.x; r < numblocks; r+=gridDim.x)
+  // for (int r = blockIdx.x; r < numblocks; r+=gridDim.x)
+  for (int r = blockIdx.x*blockDim.y+blockIdx.y; r < numblocks; r+=gridDim.x*blockDim.y)  
     for (int i = tid; i < bs; i += blockDim.x)
       dev_uy[col_dnums[r*bs+i]] += dev_hy[r*bs+i];
 }
 
 void ConstEBEKernelCopyOut (int numblocks, int bs, int * col_dnums, double * dev_hy, double * dev_uy)
 {
-  ConstEBEKernelCopyOutKernel<<<512,256>>> (numblocks, bs, col_dnums, dev_hy, dev_uy);
+  ConstEBEKernelCopyOutKernel<<<512,dim3(16,16)>>> (numblocks, bs, col_dnums, dev_hy, dev_uy);
 }
 
 
@@ -78,28 +80,30 @@ void ConstEBEKernelCopyOut (int numblocks, int bs, int * col_dnums, double * dev
 __global__ void ConstEBEKernelCopyInIdxKernel (int numblocks, int * idx, int bs, int * row_dnums, double * dev_ux, double * dev_hx)
 {
   int tid = threadIdx.x;
-  for (int r = blockIdx.x; r < numblocks; r+=gridDim.x)
+  // for (int r = blockIdx.x; r < numblocks; r+=gridDim.x)
+  for (int r = blockIdx.x*blockDim.y+blockIdx.y; r < numblocks; r+=gridDim.x*blockDim.y)    
     for (int i = tid; i < bs; i += blockDim.x)
       dev_hx[r*bs+i] = dev_ux[row_dnums[idx[r]*bs+i]];
 }
 
 void ConstEBEKernelCopyInIdx (int numblocks, int * idx, int bs, int * row_dnums, double * dev_ux, double * dev_hx)
 {
-  ConstEBEKernelCopyInIdxKernel<<<512,256>>> (numblocks, idx, bs, row_dnums, dev_ux, dev_hx);
+  ConstEBEKernelCopyInIdxKernel<<<512,dim3(16,16)>>> (numblocks, idx, bs, row_dnums, dev_ux, dev_hx);
 }
 
 __global__ void ConstEBEKernelCopyOutIdxKernel (int numblocks, int * idx, int bs, int *  col_dnums, double * dev_hy, double * dev_uy)
 {
   int tid = threadIdx.x;
 
-  for (int r = blockIdx.x; r < numblocks; r+=gridDim.x)
+  // for (int r = blockIdx.x; r < numblocks; r+=gridDim.x)
+  for (int r = blockIdx.x*blockDim.y+blockIdx.y; r < numblocks; r+=gridDim.x*blockDim.y)    
     for (int i = tid; i < bs; i += blockDim.x)
       dev_uy[col_dnums[idx[r]*bs+i]] += dev_hy[r*bs+i];
 }
 
 void ConstEBEKernelCopyOutIdx (int numblocks, int * idx, int bs, int * col_dnums, double * dev_hy, double * dev_uy)
 {
-  ConstEBEKernelCopyOutIdxKernel<<<512,256>>> (numblocks, idx, bs, col_dnums, dev_hy, dev_uy);
+  ConstEBEKernelCopyOutIdxKernel<<<512,dim3(16,16)>>> (numblocks, idx, bs, col_dnums, dev_hy, dev_uy);
 }
 
 
