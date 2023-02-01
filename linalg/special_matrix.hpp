2e80f8165e9e172338b0ec1f10216c74b9ef7603
Matthias Hochsteger <mhochsteger@cerbsim.com>
2023-02-01 16:33:23 +0100

    cuda EmbeddedMatrix

--- linalg/special_matrix.hpp
+++ linalg/special_matrix.hpp
@@ -267,6 +267,11 @@ namespace ngla
 
     shared_ptr<BaseMatrix> GetMatrix() const { return mat; }
     IntRange GetRange() const { return range; }
+
+    virtual shared_ptr<BaseMatrix> CreateDeviceMatrix() const override
+    {
+      return make_shared<EmbeddedMatrix>(height, range, mat->CreateDeviceMatrix());
+    }
   };
 
 
@@ -338,6 +343,11 @@ namespace ngla
 
     shared_ptr<BaseMatrix> GetMatrix() const { return mat; }
     IntRange GetRange() const { return range; }
+
+    virtual shared_ptr<BaseMatrix> CreateDeviceMatrix() const override
+    {
+      return make_shared<EmbeddedTransposeMatrix>(width, range, mat->CreateDeviceMatrix());
+    }
   };
 
 
