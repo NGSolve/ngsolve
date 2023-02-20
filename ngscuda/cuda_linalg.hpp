d116d130007465804e0d49484706c38231faf6de
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-02-20 10:13:47 +0100

    atomicAdd replacement using CAS (for CUDA_ARCH < 600)

--- ngscuda/cuda_linalg.hpp
+++ ngscuda/cuda_linalg.hpp
@@ -24,6 +24,28 @@ namespace ngla
 #include "unifiedvector.hpp"
 
 
+// from CUDA C++ Programming Guide:
+// https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#atomic-functions
+#if __CUDA_ARCH__ < 600
+__device__ inline double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull =
+                              (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do {
+        assumed = old;
+        old = atomicCAS(address_as_ull, assumed,
+                        __double_as_longlong(val +
+                               __longlong_as_double(assumed)));
+
+    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
+#endif
+
 
 
 namespace ngla
