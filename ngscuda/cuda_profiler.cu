aa70b3b17ea3cb0368cc1083911f239efeca3d37
Matthias Hochsteger <mhochsteger@cerbsim.com>
2023-02-24 12:39:09 +0100

    Separate GPU trace to extra files, group blocks by SM

---
+++ ngscuda/cuda_profiler.cu
@@ -0,0 +1,158 @@
+#include <ngstd.hpp>
+#include <cuda_ngstd.hpp>
+#include "cuda_profiler.hpp"
+
+using namespace std;
+
+namespace ngs_cuda
+{
+
+  int gpu_clock = 0;
+
+  constexpr bool SKIP_BLOCKS_WITH_NO_TRACES = true;
+  constexpr bool CHECK_ALL_TRACE_ENTRIES = true;
+
+#ifdef NGS_CUDA_DEVICE_TIMERS
+  __device__ DevTimerData d_timer_data[N_MAX_BLOCKS * N_MAX_DEVICE_TIMERS];
+  __device__ DevTraceData d_trace_data[N_MAX_TRACER_OBJECTS+1];
+  __device__ DevTraceBlockData d_block_data[N_MAX_BLOCKS];
+
+
+  Array<DevTimerData> timer_data{N_MAX_BLOCKS * N_MAX_DEVICE_TIMERS};
+  Array<DevTraceData> trace_data{N_MAX_TRACER_OBJECTS+1};
+  Array<DevTraceBlockData> block_data{N_MAX_BLOCKS};
+#endif // NGS_CUDA_DEVICE_TIMERS
+
+
+  void CudaRegionTimer :: ProcessTracingData()
+  {
+#ifdef NGS_CUDA_DEVICE_TIMERS
+      static ngcore::Timer<> t_overhead("CUDA Tracing overhead");
+      static ngcore::Timer<> t_memcopy("memcopy");
+      static ngcore::Timer<> t_cleanup("cleanup");
+      cudaDeviceSynchronize();
+      t_overhead.Start();
+      detail::StartGPUTimer(t_overhead);
+      t_memcopy.Start();
+      cudaMemcpyFromSymbol (&trace_data.Last(), d_trace_data, sizeof(DevTraceData), sizeof(DevTraceData)*(trace_data.Size()-1));
+      t_memcopy.Stop();
+      auto nblocks = trace_data.Last().blockNr;
+      if(nblocks>0)
+      {
+      t_memcopy.Start();
+        cudaMemcpyFromSymbol(trace_data.Data(), d_trace_data, sizeof(DevTraceData)*(trace_data.Size()-1));
+      t_memcopy.Stop();
+      t_memcopy.Start();
+        cudaMemcpyFromSymbol(timer_data.Data(), d_timer_data, sizeof(DevTimerData)*timer_data.Size());
+      t_memcopy.Stop();
+      t_memcopy.Start();
+        cudaMemcpyFromSymbol(block_data.Data(), d_block_data, sizeof(DevTraceBlockData)*nblocks);
+      t_memcopy.Stop();
+        cudaDeviceSynchronize();
+
+        auto blocks = block_data.Range(nblocks);
+
+        unsigned max_smid = 0;
+        for(auto b : blocks)
+        {
+          max_smid = std::max(max_smid, b.start_smid);
+          max_smid = std::max(max_smid, b.stop_smid);
+        }
+
+        // cout << "max smid: "  << max_smid << endl;;
+        Array<unsigned long long> smid_start_time(max_smid+1);
+        smid_start_time = std::numeric_limits<unsigned long long>::max();
+        for(auto b : blocks)
+          smid_start_time[b.start_smid] = std::min(smid_start_time[b.start_smid], b.start);
+
+        for(auto & b : blocks)
+        {
+          b.start -= smid_start_time[b.start_smid];
+          b.stop -= smid_start_time[b.stop_smid];
+        }
+
+        if(ngcore::trace)
+        {
+          Array<int> container_sm;
+          for(int i : Range(max_smid+1))
+            container_sm.Append(ngcore::trace->AddUserContainer("SM " + ToString(i)));
+
+          Array<bool> use_block(blocks.Size());
+          use_block = true;
+          if constexpr(SKIP_BLOCKS_WITH_NO_TRACES)
+          {
+            use_block = false;
+            for(auto & tr : trace_data)
+            {
+              if(tr.start[0]==0)
+              {
+                if constexpr(CHECK_ALL_TRACE_ENTRIES)
+                  continue;
+                else
+                  break;
+              }
+              use_block[tr.blockNr] = true;
+            }
+          }
+
+          Array<int> container_block(nblocks);
+          container_block = -1;
+          for(int i : Range(blocks))
+          {
+            if(!use_block[i])
+              continue;
+            auto & b = blocks[i];
+            string name = "Block " + ToString(i);
+            if(b.start_smid == b.stop_smid)
+              container_block[i] = ngcore::trace->AddUserContainer("Block " + ToString(i), container_sm[b.start_smid]);
+            else
+            {
+              name += ", SMIDs: " + ToString(b.start_smid) + ',' + ToString(b.stop_smid);
+              container_block[i] = ngcore::trace->AddUserContainer(name);
+            }
+            ngcore::trace->AddUserEvent({TimeD2H(b.start), TimeD2H(b.stop), "Active", container_block[i], i});
+          }
+
+          for(int id : Range(trace_data))
+          {
+            auto & tr = trace_data[id];
+            if(tr.start[0]==0)
+            {
+              if constexpr(CHECK_ALL_TRACE_ENTRIES)
+                continue;
+              else
+                break;
+            }
+            for(int ti : Range(N_MAX_DEVICE_TIMERS))
+            {
+              if(tr.start[ti]==0)
+                break;
+              auto t0 = smid_start_time[tr.start_smid[ti]];
+              auto t1 = smid_start_time[tr.stop_smid[ti]];
+              stringstream s;
+              s << "Timer " << ti;
+              if(tr.start_smid[ti] != tr.stop_smid[ti])
+                s << "SMIDs: " << tr.start_smid[ti] << ',' << tr.stop_smid[ti];
+
+              ngcore::trace->AddUserEvent({TimeD2H(tr.start[ti]-t0), TimeD2H(tr.stop[ti]-t1), s.str(), container_block[tr.blockNr], id});
+            }
+          }
+        }
+
+        // cleanup for next run
+        t_cleanup.Start();
+        timer_data = DevTimerData{{0}};
+        cudaMemcpyToSymbol (d_timer_data, timer_data.Data(), sizeof(DevTimerData)*timer_data.Size());
+        trace_data = DevTraceData{0,0,0};
+        cudaMemcpyToSymbol (d_trace_data, trace_data.Data(), sizeof(DevTraceData)*trace_data.Size());
+        block_data = DevTraceBlockData{0,0,0,0};
+        cudaMemcpyToSymbol (d_block_data, block_data.Data(), sizeof(DevTraceBlockData)*block_data.Size());
+        t_cleanup.Stop();
+      }
+      
+      detail::StartGPUTimer(t_overhead);
+      t_overhead.Stop();
+#endif // NGS_CUDA_DEVICE_TIMERS
+  }
+
+} // namespace ngs_cuda
---
+++ ngscuda/cuda_profiler.hpp
@@ -0,0 +1,232 @@
+#ifndef NGS_CUDA_PROFILER_HPP
+#define NGS_CUDA_PROFILER_HPP
+
+#include <algorithm>
+#include <core/profiler.hpp>
+#include "cuda_ngstd.hpp"
+
+namespace ngs_cuda
+{
+  using ngcore::Array;
+  extern int gpu_clock;
+
+#ifdef __CUDACC__
+#define NGS_CUDA_DEVICE_TIMERS
+#ifdef NGS_CUDA_DEVICE_TIMERS
+  constexpr int N_MAX_DEVICE_TIMERS = 16;
+  struct DevTraceData {
+    unsigned long long start[N_MAX_DEVICE_TIMERS];
+    unsigned long long stop[N_MAX_DEVICE_TIMERS];
+    unsigned short start_smid[N_MAX_DEVICE_TIMERS];
+    unsigned short stop_smid[N_MAX_DEVICE_TIMERS];
+    union UserData {
+      double d;
+      float f[2];
+      unsigned u[2];
+    } data;
+    unsigned blockNr;
+  };
+
+  struct DevTimerData {
+    unsigned long long time[N_MAX_DEVICE_TIMERS];
+  };
+
+  struct DevTraceBlockData {
+    unsigned long long start;
+    unsigned long long stop;
+    unsigned start_smid;
+    unsigned stop_smid;
+  };
+
+  constexpr int N_MAX_TRACER_OBJECTS = 1024*1024;
+  constexpr int N_MAX_BLOCKS = 65*1024;
+  extern __device__ DevTimerData d_timer_data[];
+  extern __device__ DevTraceData d_trace_data[];
+  extern __device__ DevTraceBlockData d_block_data[];
+  extern Array<DevTimerData> timer_data;
+  extern Array<DevTraceData> trace_data;
+  extern Array<DevTraceBlockData> block_data;
+
+
+  __device__ inline unsigned GetSMID() {
+    unsigned ret;
+    asm("mov.u32 %0, %smid;" : "=r"(ret) );
+    return ret;
+  }
+
+  struct DeviceBlockRegionTracer {
+    int blockNr, threadNr;
+    __device__ DeviceBlockRegionTracer(int nblocks, int blockNr_, int threadNr_)
+      : blockNr(blockNr_), threadNr(threadNr_)
+    {
+      if(threadNr == 0)
+      {
+        d_block_data[blockNr].start = clock64();
+        d_block_data[blockNr].start_smid = GetSMID();
+        if(blockNr == 0)
+        {
+          d_trace_data[N_MAX_TRACER_OBJECTS].blockNr = nblocks;
+        }
+      }
+      __syncwarp();
+    }
+
+    __device__ ~DeviceBlockRegionTracer()
+    {
+      d_block_data[blockNr].stop = clock64();
+      d_block_data[blockNr].stop_smid = GetSMID();
+    }
+  };
+
+  struct DeviceRegionTimer
+  {
+    int timer_nr, id;
+
+    __device__ DeviceRegionTimer( int timer_nr_, int id_=-1)
+      : timer_nr(timer_nr_), id(id_ == -1 ? blockIdx.x : id_)
+    {
+      if(threadIdx.x ==0)
+        atomicAdd(&d_timer_data[id].time[timer_nr], -clock64());
+    }
+
+    __device__ ~DeviceRegionTimer()
+    {
+      if(threadIdx.x ==0)
+        atomicAdd(&d_timer_data[id].time[timer_nr], clock64());
+    }
+
+  };
+
+  struct DeviceRegionTracer
+  {
+    int timer_nr, id;
+    bool active;
+    DevTraceData::UserData & data;
+
+    __device__ DeviceRegionTracer( const DeviceBlockRegionTracer & tr, int timer_nr_, int id_)
+      : active(tr.threadNr == 0), timer_nr(timer_nr_), id(id_), data(d_trace_data[id_].data)
+    {
+      if(active)
+      {
+        d_trace_data[id].start[timer_nr] = clock64();
+        d_trace_data[id].start_smid[timer_nr] = GetSMID();
+        d_trace_data[id].blockNr = tr.blockNr;
+      }
+    }
+
+    __device__ ~DeviceRegionTracer()
+    {
+      if(active)
+      {
+        d_trace_data[id].stop[timer_nr] = clock64();
+        d_trace_data[id].stop_smid[timer_nr] = GetSMID();
+      }
+    }
+
+  };
+#else
+  struct DeviceBlockRegionTracer
+  {
+    __device__ DeviceBlockRegionTracer( int , int, int ) {}    
+  };
+  struct DeviceRegionTimer
+  {
+    __device__ DeviceRegionTimer( int , int ) {}
+  };
+  struct DeviceRegionTracer
+  {
+    // __device__ DeviceRegionTracer( bool active_, int timer_nr_, int id_) {}
+    __device__ DeviceRegionTracer( const DeviceBlockRegionTracer & tr, int timer_nr_, int id_) { }  
+  };
+#endif // NGS_CUDA_DEVICE_TIMERS
+
+#endif // __CUDACC__
+
+
+  namespace detail {
+  struct GPUTimerUserData
+  {
+    int timernr;
+    bool is_start;
+  };
+
+  // encode UserData directly in void* pointer (8 bytes available)
+  inline void * EncodeCallbackData( bool start, int nr )
+  {
+    static_assert(sizeof(GPUTimerUserData) <= sizeof(void*), "cannot encode UserData in void*");
+    GPUTimerUserData data{nr, start};
+    return *reinterpret_cast<void**>(&data);
+  }
+
+  inline void CUDART_CB Callback(cudaStream_t stream, cudaError_t status, void *userData)
+  {
+    const GPUTimerUserData & ud = *reinterpret_cast<GPUTimerUserData*>(&userData);
+    if(!ngcore::trace) return;
+    if(ud.is_start)
+      ngcore::trace->StartGPU(ud.timernr);
+    else
+      ngcore::trace->StopGPU(ud.timernr);
+  }
+
+  inline void CUDART_CB CallbackKernelStart(cudaStream_t stream, cudaError_t status, void *userData)
+  {
+    ngcore::TTimePoint & t = *reinterpret_cast<ngcore::TTimePoint*>(userData);
+    t = ngcore::GetTimeCounter();
+  }
+
+  inline void StartGPUTimer(int nr) {
+    cudaStreamAddCallback(0, Callback, EncodeCallbackData(true, nr), 0);
+  }
+
+  inline void StopGPUTimer(int nr) {
+    cudaStreamAddCallback(0, Callback, EncodeCallbackData(false, nr), 0);
+  }
+  } // namspace detail
+
+  class CudaRegionTimer
+  {
+    const ngcore::Timer<> & timer;
+    bool is_already_stopped = false;
+    ngcore::TTimePoint t_kernel_start;
+
+
+
+  public:
+    CudaRegionTimer (ngcore::Timer<> & atimer) : timer(atimer) {
+      timer.Start();
+      detail::StartGPUTimer(timer);
+#ifdef NGS_CUDA_DEVICE_TIMERS
+      cudaStreamAddCallback(0, detail::CallbackKernelStart, &t_kernel_start, 0);
+#endif // NGS_CUDA_DEVICE_TIMERS
+    }
+    ~CudaRegionTimer ()
+    {
+      if(!is_already_stopped)
+        Stop();
+    }
+    CudaRegionTimer() = delete;
+    CudaRegionTimer(const CudaRegionTimer &) = delete;
+    CudaRegionTimer(CudaRegionTimer &&) = delete;
+    void operator=(const CudaRegionTimer &) = delete;
+    void operator=(CudaRegionTimer &&) = delete;
+
+    void Stop() {
+      detail::StopGPUTimer(timer);
+#ifdef NGS_CUDA_DEVICE_TIMERS
+      ProcessTracingData();
+#endif // NGS_CUDA_DEVICE_TIMERS
+      timer.Stop();
+      is_already_stopped = true;
+    }
+
+    void ProcessTracingData();
+
+    ngcore::TTimePoint TimeD2H( long long ticks )
+    {
+      double t_seconds = 1.0*ticks/gpu_clock + 1e-6; // add 1us for kernel startup
+      return t_kernel_start + t_seconds / ngcore::seconds_per_tick;
+    }
+  };
+}
+
+#endif // NGS_CUDA_PROFILER_HPP
