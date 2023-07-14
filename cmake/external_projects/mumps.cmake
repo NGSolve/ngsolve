7d5556ea9eae0a94f8b26cd5b2a3f4cdca95b59d
Matthias Hochsteger <mhochsteger@cerbsim.com>
2023-07-14 15:07:42 +0200

    Fix MUMPS build

--- cmake/external_projects/mumps.cmake
+++ cmake/external_projects/mumps.cmake
@@ -19,13 +19,10 @@ configure_file(${CMAKE_CURRENT_LIST_DIR}/mumps.inc ${CMAKE_CURRENT_BINARY_DIR}/d
 
 ExternalProject_Add(project_mumps
   DEPENDS project_parmetis
-  URL "http://mumps.enseeiht.fr/MUMPS_5.2.0.tar.gz"
-  URL_MD5 cd6d06f27ce2689eb0436e41fcc9caed
-#   URL "http://mumps.enseeiht.fr/MUMPS_5.0.2.tar.gz"
-#   URL_MD5 591bcb2c205dcb0283872608cdf04927
-# URL_MD5 b477573fdcc87babe861f62316833db0
-#  URL "http://mumps.enseeiht.fr/MUMPS_5.1.2.tar.gz"
-#  URL_MD5 6ac4f52380ce4d74126be2d7c530e533
+  URL 
+  "http://graal.ens-lyon.fr/MUMPS/MUMPS_5.0.2.tar.gz"
+  "https://src.fedoraproject.org/lookaside/extras/MUMPS/MUMPS_5.0.2.tar.gz/591bcb2c205dcb0283872608cdf04927/MUMPS_5.0.2.tar.gz"
+  URL_MD5 591bcb2c205dcb0283872608cdf04927
   ${SUBPROJECT_ARGS}
   DOWNLOAD_DIR ${CMAKE_CURRENT_SOURCE_DIR}/external_dependencies
   BUILD_IN_SOURCE 1
