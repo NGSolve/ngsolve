79a1ca33c70942ff75d776120133aeee93a4cc1f
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-07-18 23:05:04 +0200

    PrivateSpace: all dofs hidden + Compress

--- python/__init__.py
+++ python/__init__.py
@@ -65,7 +65,7 @@ from .comp import VOL, BND, BBND, BBBND, COUPLING_TYPE, ElementId, \
 from .solve import BVP, CalcFlux, Draw, DrawFlux, \
     SetVisualization
 from .utils import x, y, z, dx, ds, grad, Grad, curl, div, Deviator, PyId, PyTrace, \
-    PyDet, PyCross, PyCof, PyInv, PySym, PySkew, OuterProduct, TimeFunction, Normalize, printmaster
+    PyDet, PyCross, PyCof, PyInv, PySym, PySkew, OuterProduct, PrivateSpace, TimeFunction, Normalize, printmaster
 from . import solvers
 
 
