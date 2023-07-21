8dd280e960b43906728fd5b30d69e2408cbd4549
Christopher Lackner <clackner@cerbsim.com>
2023-07-21 14:25:01 +0200

    Clean up ExportStdMathFunction code for numpy vectorization
    This allows to also give numpy arrays to our std math functions.
    Overloads will be tried in order from pybind11, since
    double and complex can be cast to CoefficientFunction implicitly
    this natively also supports combinations of double/Complex + CF like
    
    ```
    atan(0.5, x)
    ```

--- fem/python_fem.hpp
+++ fem/python_fem.hpp
@@ -38,20 +38,18 @@ namespace ngfem
   {
     static RegisterClassForArchive<cl_UnaryOpCF<FUNC>, CoefficientFunction> reguopcf;
 
-    m.def (name.c_str(), [name] (py::object x) -> py::object
-              {
-                FUNC func;
-                py::extract<double> ed(x);
-                if (ed.check()) return py::cast(func(ed()));
-                if (py::extract<Complex> (x).check())
-                  return py::cast(func(py::extract<Complex> (x)()));
-                if (py::extract<shared_ptr<CoefficientFunction>>(x).check())
-                  {
-                    auto coef = py::extract<shared_ptr<CoefficientFunction>>(x)();
-                    return py::cast(func(coef));
-                  }
-                throw py::type_error (string("can't compute math-function, type = ") + name);
-              }, py::arg("x"), description.c_str());
+    m.def(name.c_str(), [](double x) {
+        FUNC func;
+        return func(x);
+      }, py::arg("x"), description.c_str());
+    m.def(name.c_str(), [](Complex x) {
+        FUNC func;
+        return func(x);
+      }, py::arg("x"), description.c_str());
+    m.def(name.c_str(), [] (shared_ptr<CoefficientFunction> x) -> shared_ptr<CoefficientFunction> {
+        FUNC func;
+        return func(x);
+      }, py::arg("x"), description.c_str());
   }
   
 
@@ -60,22 +58,24 @@ namespace ngfem
   {
     static RegisterClassForArchive<cl_BinaryOpCF<FUNC>, CoefficientFunction> regbinopcf;
 
-    m.def (name.c_str(),
-           [name] (py::object x, py::object y) -> py::object
-           {
-             FUNC func;
-             py::extract<double> dx(x), dy(y);
-             if (dx.check() && dy.check()) return py::cast(func(dx(), dy()));
-             py::extract<Complex> cx(x), cy(y);
-             if (cx.check() && cy.check()) return py::cast(func(cx(), cy()));
-             if (py::extract<shared_ptr<CoefficientFunction>>(x).check() || py::extract<shared_ptr<CoefficientFunction>>(y).check())
-               {
-                 shared_ptr<CoefficientFunction> cx = py::cast<shared_ptr<CoefficientFunction>>(x);
-                 shared_ptr<CoefficientFunction> cy = py::cast<shared_ptr<CoefficientFunction>>(y);
-                 return py::cast(BinaryOpCF(cx, cy, func, FUNC::Name()));
-               }
-             throw py::type_error (string("can't compute binary math-function")+typeid(FUNC).name());
-           }, py::arg(arg0.c_str()), py::arg(arg1.c_str()), description.c_str());
+    m.def(name.c_str(), py::vectorize([](double x, double y)
+    {
+        FUNC func;
+        return func(x,y);
+    }), py::arg(arg0.c_str()), py::arg(arg1.c_str()), description.c_str());
+    m.def(name.c_str(), py::vectorize([](Complex x, Complex y)
+    {
+        FUNC func;
+        return func(x,y);
+    }), py::arg(arg0.c_str()), py::arg(arg1.c_str()), description.c_str());
+    m.def(name.c_str(),
+          [](shared_ptr<CoefficientFunction> x,
+             shared_ptr<CoefficientFunction> y)
+          -> shared_ptr<CoefficientFunction>
+    {
+      FUNC func;
+      return BinaryOpCF(x, y, func, FUNC::Name());
+    }, py::arg(arg0.c_str()), py::arg(arg1.c_str()), description.c_str());
   }
 
 
