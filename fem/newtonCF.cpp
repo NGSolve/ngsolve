20453fe7fa24353b62b0a901b348774202373e28
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-08-25 01:58:27 +0200

    non-const assign and rvalue assign

--- fem/newtonCF.cpp
+++ fem/newtonCF.cpp
@@ -945,9 +945,9 @@ public:
           int offset2 = offset1;
           for (size_t block2 : Range(block1, nblocks)) {
             const auto proxy2 = proxies[block2];
-            const auto &linb =
+            auto linb =
                 lin_blocks[block1 * nblocks + block2](qi, STAR, STAR);
-            const auto &lhsb =
+            auto lhsb =
                 lhs_blocks[block1 * nblocks + block2](qi, STAR, STAR);
 
             if (auto vsemb1 = get_vs_embedding(proxy1); vsemb1)
