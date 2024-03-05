27f681e46f5f8ee5a7fb5a022c2aed9a8edb7e8d
Matthias Hochsteger <mhochsteger@cerbsim.com>
2024-03-05 08:42:38 +0100

    Update Netgen, deploy pyodide

--- tests/gitlab-ci/pyodide/build.sh
+++ tests/gitlab-ci/pyodide/build.sh
@@ -4,4 +4,4 @@ USERID=`id -u`
 GROUPID=`id -g`
 # docker run -u `id -u` -v `pwd`/dist:/dist ngsolve_pyodide bash -c 'cd /root/output && cp ngsolve*.tar.bz2 /dist/'
 docker run -v `pwd`/dist:/dist ngsolve_pyodide bash -c "cd /root/output && cp ngsolve*.tar.bz2 /dist/ && chown -R ${USERID}:${GROUPID} /dist/"
-# scp dist/*.tar.bz2 mhochsteger@aurora:public_html/ngsolve/
+scp dist/*.tar.bz2 mhochsteger@aurora:public_html/ngsolve/pyodide-0.26.0a3
