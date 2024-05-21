64332c286c82eb91d34c186ad18c1853f1b60f99
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-05-21 10:57:45 +0200

    added plotrates to iterative solers

--- python/solvers.py
+++ python/solvers.py
@@ -1,5 +1,6 @@
 from ngsolve.eigenvalues import PINVIT, LOBPCG
 from ngsolve.krylovspace import CG, QMR, MinRes, PreconditionedRichardson, GMRes
+from ngsolve.krylovspace import CGSolver
 from ngsolve.nonlinearsolvers import Newton, NewtonMinimization
 from ngsolve.bvp import BVP
 from ngsolve.directsolvers import SuperLU
