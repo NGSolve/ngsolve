c390ce4760cea42151b4093a31cb8993d195cfc0
Matthias Hochsteger <mhochsteger@cerbsim.com>
2024-06-13 11:37:30 +0200

    Scikit-build-core plugin to generate ngsolve dependency version dynamically

---
+++ python/_scikit_build_core_dependencies.py
@@ -0,0 +1,23 @@
+def dynamic_metadata(field, settings):
+
+    if field != "dependencies":
+        msg = "Only the 'dependencies' field is supported"
+        raise ValueError(msg)
+
+    if settings:
+        msg = "No inline configuration is supported"
+        raise ValueError(msg)
+
+    import toml
+    import importlib.metadata
+
+    config = toml.load("pyproject.toml")
+    dependencies = config["project"].get("dependencies", [])
+
+    ngs_version = importlib.metadata.version("ngsolve")
+    dependencies = dependencies + [f"ngsolve=={ngs_version}"]
+    return dependencies
+
+
+def get_requires_for_dynamic_metadata(_settings):
+    return ["toml", "ngsolve"]
