39b06c645c160005d39ca630b7653034f6be27c3
Christopher Lackner <clackner@cerbsim.com>
2024-06-13 10:47:31 +0200

    use extract proxies in ImplicitEuler

--- python/timestepping.py
+++ python/timestepping.py
@@ -7,21 +7,24 @@ class ImplicitEuler:
             equation: ngs.comp.SumOfIntegrals,
             time: ngs.Parameter,
             dt: Union[float, ngs.Parameter],
-            u: ngs.comp.ProxyFunction,
             pc_cls: Type = ngs.preconditioners.MultiGrid,
             pc_args: Optional[dict] = None,
             lin_solver_cls: Type = ngs.solvers.CGSolver,
             lin_solver_args: Optional[dict] = None):
 
         self.time = time
-        self.gfu_old = ngs.GridFunction(u.space)
+        proxies = equation.GetProxies()
+        udt = list(filter(lambda u: u.dt_order > 0, proxies))
+        if len(udt) != 1:
+            raise ValueError("Only du/dt allowed in implicit euler!")
+        udt = udt[0]
+        self.gfu_old = ngs.GridFunction(udt.space)
         self.dt = ngs.Parameter(dt) if (isinstance(dt, float) or isinstance(dt, int)) else dt
-        self.bfmstar = ngs.BilinearForm(u.space)
-        self.bfmstar += equation.Replace({ u.dt : 1/self.dt * (u-self.gfu_old) })
-        print(equation.Replace({ u.dt : 1/self.dt * (u-self.gfu_old) }))
+        self.bfmstar = ngs.BilinearForm(udt.space)
+        self.bfmstar += equation.Replace({ udt : 1/self.dt * (udt.anti_dt-self.gfu_old) })
         self.bfmstar.Assemble()
         self.c = pc_cls(self.bfmstar, **(pc_args or {}))
-        self.lin_solver = lin_solver_cls(mat=self.bfmstar, pre=self.c, **(lin_solver_args or {}))
+        self.lin_solver = lin_solver_cls(mat=self.bfmstar.mat, pre=self.c, **(lin_solver_args or {}))
 
     def Integrate(self, u_start: ngs.GridFunction,
                   end_time: float,
@@ -43,8 +46,7 @@ class ImplicitEuler:
         if dt is not None:
             self.dt.Set(dt)
         self.gfu_old.vec.data = u.vec
-        self.bfmstar.AssembleLinearization(u.vec)
-        newton = ngs.nonlinearsolvers.NewtonSolver(a=self.bfmstar.mat,
+        newton = ngs.nonlinearsolvers.NewtonSolver(a=self.bfmstar,
                                                    u=u,
                                                    solver=self.lin_solver)
         newton.Solve(**(newton_args or {}))
