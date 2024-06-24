2d21b658086170ae24f3a5e57e36d7bad5d68733
Matthias Hochsteger <mhochsteger@cerbsim.com>
2024-06-24 18:01:21 +0200

    Merge windows jobs in one stage for better caching

--- tests/gitlab-ci/win/set_vars.bat
+++ tests/gitlab-ci/win/set_vars.bat
@@ -1,12 +1,12 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64"
 set CMAKE_GENERATOR=Ninja
-set NETGEN_BUILD_DIR=%CI_DIR%\build
-set CMAKE_INSTALL_PREFIX=%CI_DIR%\install
-set SRC_DIR=%CI_DIR%\src
+set NETGEN_BUILD_DIR=%CI_PROJECT_DIR%\build
+set CMAKE_INSTALL_PREFIX=%CI_PROJECT_DIR%\install
+set SRC_DIR=%CI_PROJECT_DIR%
 set NETGENDIR=%CMAKE_INSTALL_PREFIX%\bin
 set PATH=%NETGENDIR%;C:\python312;C:\python312\bin;C:\python312\Scripts;C:\tools\;%PATH%
 set PYTHONPATH=%CMAKE_INSTALL_PREFIX%\lib\site-packages
-set CCACHE_BASEDIR=%CI_DIR%
+REM set CCACHE_BASEDIR=%CI_DIR%
 set HOME=%USERPROFILE%
 call ccache -M 20G
 call ccache -s
