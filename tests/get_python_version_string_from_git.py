308945c7f7bafff9dcd4ea8f9a18674ad06287ba
Matthias Hochsteger <mhochsteger@cerbsim.com>
2024-07-02 13:01:32 +0200

    Build Post Release pip packages on release branch

--- tests/get_python_version_string_from_git.py
+++ tests/get_python_version_string_from_git.py
@@ -8,13 +8,18 @@ if len(sys.argv)>1:
 
 git_version = check_output(['git', 'describe', '--tags'], cwd=cwd).decode('utf-8').strip()
 
+def is_dev_build():
+    if 'NG_NO_DEV_PIP_VERSION' in os.environ:
+        return False
+    if 'CI_COMMIT_REF_NAME' in os.environ and os.environ['CI_COMMIT_REF_NAME'] == 'release':
+        return False
+    return True
+
 version = git_version[1:].split('-')
 if len(version)>2:
     version = version[:2]
 if len(version)>1:
     version = '.post'.join(version)
-    if len(sys.argv)>1 or not 'NGS_NO_DEV_PIP_VERSION' in os.environ:
-        version += '.dev'
 else:
     version = version[0]
 
