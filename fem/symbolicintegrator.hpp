9631618dec251a57130acb3b628203c84cbdb377
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-07-11 11:12:44 +0200

    wrap primary(-proxy) to Python

--- fem/symbolicintegrator.hpp
+++ fem/symbolicintegrator.hpp
@@ -72,6 +72,7 @@ public:
 
   NGS_DLL_HEADER shared_ptr<ProxyFunction> Deriv() const;
   NGS_DLL_HEADER shared_ptr<ProxyFunction> Trace() const;
+  NGS_DLL_HEADER shared_ptr<ProxyFunction> Primary() const { return primaryproxy; }
   
   NGS_DLL_HEADER shared_ptr<ProxyFunction> Dt() const;  
   NGS_DLL_HEADER shared_ptr<ProxyFunction> AntiDt() const;
