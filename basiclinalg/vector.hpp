c2b2c5c46d538a59bae9887ba62a2127d00d76f5
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-07-22 10:47:49 +0200

    alternative check for index-type

--- basiclinalg/vector.hpp
+++ basiclinalg/vector.hpp
@@ -314,7 +314,8 @@ namespace ngbla
 
     
     /// constant element access
-    template<typename I, typename std::enable_if<std::is_integral<I>::value, int>::type = 0>    
+    // template<typename I, typename std::enable_if<std::is_integral<I>::value, int>::type = 0>
+    template<typename I, typename std::enable_if<std::is_convertible<I,size_t>::value, int>::type = 0>
     INLINE TELEM & operator() (I i) const
     {
       NETGEN_CHECK_RANGE(i,0,Size());
