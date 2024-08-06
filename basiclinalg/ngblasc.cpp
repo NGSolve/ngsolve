d11f5e2681883a826b54661c3e43557443613cd8
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-08-06 21:05:01 +0200

    better simding of real-mat * complex-vec

--- basiclinalg/ngblasc.cpp
+++ basiclinalg/ngblasc.cpp
@@ -1,14 +1,12 @@
 #include <bla.hpp>
 
 
-/*
 // do we have 32 vector-registers ?
 #if defined(__AVX512F__) || defined(__arm64__)
 constexpr bool reg32 = true;
 #else
 constexpr bool reg32 = false;
 #endif
-*/
 
 namespace ngbla
 {
@@ -196,56 +194,66 @@ namespace ngbla
 
 
   
-  template <typename TM, size_t SX, typename FUNC>
-  void NgGEMV_Short (Complex s, BareSliceMatrix<TM,ColMajor> a, SliceVector<Complex> x, SliceVector<Complex> y, FUNC func) NETGEN_NOEXCEPT  
+  template <size_t SX, typename FUNC>
+  void NgGEMV_Short (Complex s, BareSliceMatrix<double,ColMajor> a, SliceVector<Complex> x, SliceVector<Complex> y, FUNC func) NETGEN_NOEXCEPT  
   {
-    Vec<SX,Complex> hx;
-    for (size_t j = 0; j < SX; j++)
-      hx(j) = x(j);
-    
-    size_t i = 0;
     /*
-    for ( ; i+2 <= y.Size(); i+=2)
+    Vec<SX> hxr, hxi;
+    for (size_t j = 0; j < SX; j++)
       {
-        Complex sum0 = 0;
-        Complex sum1 = 0;
-
-        for (size_t j = 0; j < SX; j++)
-          {
-            Complex xj = hx(j);
-            sum0 += a(i  ,j) * xj;
-            sum1 += a(i+1,j) * xj;
-          }
-        
-        func(y(i  ), s*sum0);
-        func(y(i+1), s*sum1);
+        hxr(j) = x(j).real();
+        hxi(j) = x(j).imag();
       }
     */
+    size_t i = 0;
+    constexpr size_t SW = SIMD<double>::Size();
+    for ( ; i+SW <= y.Size(); i+= SW)
+      {
+        SIMD<double,SW> sumr = 0.0;
+        SIMD<double,SW> sumi = 0.0;
+        for (size_t j = 0; j < SX; j++)
+          {
+            SIMD<double,SW> aij(a.Addr(i,j));
+            SIMD<double,SW> xr(x(j).real());
+            SIMD<double,SW> xi(x(j).imag());
+            sumr += aij*xr;
+            sumi += aij*xi;
+          }
+        SIMD<double,SW> s_sumr = s.real()*sumr - s.imag()*sumi;
+        SIMD<double,SW> s_sumi = s.real()*sumi + s.imag()*sumr;
+
+        for (size_t k = 0; k < SW; k++)
+          func(y(i+k), Complex(s_sumr[k], s_sumi[k]));
+      }
+           
     for ( ; i < y.Size(); i++)
       {
         Complex sum = 0;
         for (size_t j = 0; j < SX; j++)
-          sum += a(i,j) * hx(j);
+          sum += a(i,j) * x(j);
         func(y(i), s*sum);
       }
   }
 
   void Test1 (Complex s, BareSliceMatrix<double,ColMajor> a, SliceVector<Complex> x, SliceVector<Complex> y)
   {
-    NgGEMV_Short<double,2> (s, a, x, y,[](Complex & y, Complex sum) { y+=sum; });
+    NgGEMV_Short<2> (s, a, x, y,[](Complex & y, Complex sum) { y+=sum; });
   }
   
-  template <typename TM, typename FUNC>
-  void NgGEMV (Complex s, BareSliceMatrix<TM,ColMajor> a, SliceVector<Complex> x, SliceVector<Complex> y, FUNC func) NETGEN_NOEXCEPT  
+  template <typename FUNC>
+  void NgGEMV (Complex s, BareSliceMatrix<double,ColMajor> a, SliceVector<Complex> x, SliceVector<Complex> y, FUNC func) NETGEN_NOEXCEPT  
   {
+    // static Timer t("Complex MatVec, ColMajor, SliceVec"); RegionTimer reg(t);
+    // t.AddFlops (2*x.Size()*y.Size());
+
     /*
-      // dit not improve ...
-    typedef void (*pmv)(Complex, BareSliceMatrix<TM,ColMajor>, SliceVector<Complex>, SliceVector<Complex>, FUNC) NETGEN_NOEXCEPT;
+    // dit not improve ...
+    typedef void (*pmv)(Complex, BareSliceMatrix<double,ColMajor>, SliceVector<Complex>, SliceVector<Complex>, FUNC) NETGEN_NOEXCEPT;
     static pmv dispatch_matvec[12];
     static auto init_matvec = [] ()
     {
       Iterate<std::size(dispatch_matvec)> ([&] (auto i)
-      { dispatch_matvec[i] = &NgGEMV_Short<TM,i,FUNC>; });
+      { dispatch_matvec[i] = &NgGEMV_Short<i,FUNC>; });
       return 1;
     }();
 
@@ -255,8 +263,9 @@ namespace ngbla
         return;
       }
     */
-    // static Timer t("Complex MatVec, ColMajor, SliceVec"); RegionTimer reg(t);
-    // t.AddFlops (2*sizeof(TM)/8*x.Size()*y.Size());
+
+
+    
     /*
     for (size_t i = 0; i < y.Size(); i++)
       {
@@ -290,8 +299,177 @@ namespace ngbla
         func(y(i+3), s*sum3);
       }
     */
+    /*
     size_t i = 0;
+    for ( ; i+8 <= y.Size(); i+=8)
+      {
+        Complex sum0 = 0;
+        Complex sum1 = 0;
+        Complex sum2 = 0;
+        Complex sum3 = 0;
+        Complex sum4 = 0;
+        Complex sum5 = 0;
+        Complex sum6 = 0;
+        Complex sum7 = 0;
+        
+        for (size_t j = 0; j < x.Size(); j++)
+          {
+            Complex xj = x(j);
+            sum0 += a(i  ,j) * xj;
+            sum1 += a(i+1,j) * xj;
+            sum2 += a(i+2,j) * xj;
+            sum3 += a(i+3,j) * xj;
+            sum4 += a(i+4,j) * xj;
+            sum5 += a(i+5,j) * xj;
+            sum6 += a(i+6,j) * xj;
+            sum7 += a(i+7,j) * xj;
+          }
+        
+        func(y(i  ), s*sum0);
+        func(y(i+1), s*sum1);
+        func(y(i+2), s*sum2);
+        func(y(i+3), s*sum3);
+        func(y(i+4), s*sum4);
+        func(y(i+5), s*sum5);
+        func(y(i+6), s*sum6);
+        func(y(i+7), s*sum7);
+      }
 
+    for ( ; i+2 <= y.Size(); i+=2)
+      {
+        Complex sum0 = 0;
+        Complex sum1 = 0;
+
+        for (size_t j = 0; j < x.Size(); j++)
+          {
+            Complex xj = x(j);
+            sum0 += a(i  ,j) * xj;
+            sum1 += a(i+1,j) * xj;
+          }
+        
+        func(y(i  ), s*sum0);
+        func(y(i+1), s*sum1);
+      }
+
+    for ( ; i < y.Size(); i++)
+      {
+        Complex sum = 0;
+        for (size_t j = 0; j < x.Size(); j++)
+          sum += a(i,j) * x(j);
+        func(y(i), s*sum);
+      }
+    */
+
+    constexpr int SW = SIMD<double>::Size();
+    constexpr int MSW = (reg32 ? 8 : 4)*SIMD<double>::Size();    
+    size_t i = 0;
+    for ( ; i+MSW <= y.Size(); i+= MSW)
+      {
+        SIMD<double,MSW> sumr = 0.0;
+        SIMD<double,MSW> sumi = 0.0;
+        for (size_t j = 0; j < x.Size(); j++)
+          {
+            SIMD<double,MSW> aij(a.Addr(i,j));
+            SIMD<double,MSW> xr(x(j).real());
+            SIMD<double,MSW> xi(x(j).imag());
+            sumr += aij*xr;
+            sumi += aij*xi;
+          }
+        SIMD<double,MSW> s_sumr = s.real()*sumr - s.imag()*sumi;
+        SIMD<double,MSW> s_sumi = s.real()*sumi + s.imag()*sumr;
+
+        for (size_t k = 0; k < MSW; k++)
+          func(y(i+k), Complex(s_sumr[k], s_sumi[k]));
+      }
+    
+    for ( ; i+SW <= y.Size(); i+= SW)
+      {
+        SIMD<double,SW> sumr = 0.0;
+        SIMD<double,SW> sumi = 0.0;
+        for (size_t j = 0; j < x.Size(); j++)
+          {
+            SIMD<double,SW> aij(a.Addr(i,j));
+            SIMD<double,SW> xr(x(j).real());
+            SIMD<double,SW> xi(x(j).imag());
+            sumr += aij*xr;
+            sumi += aij*xi;
+          }
+        SIMD<double,SW> s_sumr = s.real()*sumr - s.imag()*sumi;
+        SIMD<double,SW> s_sumi = s.real()*sumi + s.imag()*sumr;
+
+        for (size_t k = 0; k < SW; k++)
+          func(y(i+k), Complex(s_sumr[k], s_sumi[k]));
+      }
+           
+    for ( ; i < y.Size(); i++)
+      {
+        Complex sum = 0;
+        for (size_t j = 0; j < x.Size(); j++)
+          sum += a(i,j) * x(j);
+        func(y(i), s*sum);
+      }
+  }
+
+
+
+  template <typename FUNC>
+  void NgGEMV (Complex s, BareSliceMatrix<Complex,ColMajor> a, SliceVector<Complex> x, SliceVector<Complex> y, FUNC func) NETGEN_NOEXCEPT  
+  {
+    static Timer t("Complex MatVec, ColMajor, SliceVec"); RegionTimer reg(t);
+    t.AddFlops (4*x.Size()*y.Size());
+    
+    /*
+      // dit not improve ...
+    typedef void (*pmv)(Complex, BareSliceMatrix<TM,ColMajor>, SliceVector<Complex>, SliceVector<Complex>, FUNC) NETGEN_NOEXCEPT;
+    static pmv dispatch_matvec[12];
+    static auto init_matvec = [] ()
+    {
+      Iterate<std::size(dispatch_matvec)> ([&] (auto i)
+      { dispatch_matvec[i] = &NgGEMV_Short<TM,i,FUNC>; });
+      return 1;
+    }();
+
+    if (x.Size() < std::size(dispatch_matvec))
+      {
+        (*dispatch_matvec[x.Size()])(s, a, x, y, func);
+        return;
+      }
+    */
+    /*
+    for (size_t i = 0; i < y.Size(); i++)
+      {
+        Complex sum = 0;
+        for (size_t j = 0; j < x.Size(); j++)
+          sum += a(i,j) * x(j);
+        func(y(i), s*sum);
+      }
+    */
+
+    /*
+    size_t i = 0;
+    for ( ; i+4 <= y.Size(); i+=4)
+      {
+        Complex sum0 = 0;
+        Complex sum1 = 0;
+        Complex sum2 = 0;
+        Complex sum3 = 0;
+        
+        for (size_t j = 0; j < x.Size(); j++)
+          {
+            sum0 += a(i  ,j) * x(j);
+            sum1 += a(i+1,j) * x(j);
+            sum2 += a(i+2,j) * x(j);
+            sum3 += a(i+3,j) * x(j);
+          }
+        
+        func(y(i  ), s*sum0);
+        func(y(i+1), s*sum1);
+        func(y(i+2), s*sum2);
+        func(y(i+3), s*sum3);
+      }
+    */
+
+    size_t i = 0;
     for ( ; i+8 <= y.Size(); i+=8)
       {
         Complex sum0 = 0;
@@ -351,6 +529,13 @@ namespace ngbla
       }
   }
 
+
+
+
+
+
+
+  
   template <>
   NGS_DLL_HEADER    
   void NgGEMV<false> (Complex s, BareSliceMatrix<double,RowMajor> a, SliceVector<Complex> x, SliceVector<Complex> y) NETGEN_NOEXCEPT
