2781f8ce70c808fc3ee6db2eb871bf395a675959
Matthias Hochsteger <mhochsteger@cerbsim.com>
2024-10-07 18:05:08 +0200

    Pyodide fixes

--- tests/gitlab-ci/pyodide/build.sh
+++ tests/gitlab-ci/pyodide/build.sh
@@ -1,7 +1,8 @@
 docker build -t ngsolve_pyodide -f tests/gitlab-ci/pyodide/Dockerfile .
+rm -rf dist
 mkdir -p dist
 USERID=`id -u`
 GROUPID=`id -g`
 # docker run -u `id -u` -v `pwd`/dist:/dist ngsolve_pyodide bash -c 'cd /root/output && cp ngsolve*.tar.bz2 /dist/'
 docker run -v `pwd`/dist:/dist ngsolve_pyodide bash -c "cd /root/output && cp ngsolve*.tar.bz2 /dist/ && chown -R ${USERID}:${GROUPID} /dist/"
-scp dist/*.tar.bz2 mhochsteger@aurora:public_html/ngsolve/pyodide-0.26.0a3
+scp -P 692 dist/*.tar.bz2 deploy@ngsolve.org:/opt/data/files/
