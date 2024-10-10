7b6eb148cf059f1bb240f6d4e74ed785232ae10e
Matthias Hochsteger <mhochsteger@cerbsim.com>
2024-10-10 11:54:31 +0200

    Push pyodide image only from master

--- tests/gitlab-ci/pyodide/build.sh
+++ tests/gitlab-ci/pyodide/build.sh
@@ -5,4 +5,9 @@ USERID=`id -u`
 GROUPID=`id -g`
 # docker run -u `id -u` -v `pwd`/dist:/dist ngsolve_pyodide bash -c 'cd /root/output && cp ngsolve*.tar.bz2 /dist/'
 docker run -v `pwd`/dist:/dist ngsolve_pyodide bash -c "cd /root/output && cp ngsolve*.tar.bz2 /dist/ && chown -R ${USERID}:${GROUPID} /dist/"
+
+# push only if $CI_COMMIT_REF_NAME is "master"
+if [ "$CI_COMMIT_REF_NAME" != "master" ]; then
+  exit 0
+fi
 scp -P 692 dist/*.tar.bz2 deploy@ngsolve.org:/opt/data/files/
