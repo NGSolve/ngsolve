ac3efd7daea5ea073594c0c759e8f5891fbddb6b
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-11-07 09:46:29 +0100

    updated poission_mpi

--- docs/i-tutorials/unit-5a.1-mpi/poisson_mpi.py
+++ docs/i-tutorials/unit-5a.1-mpi/poisson_mpi.py
@@ -1,14 +1,11 @@
-from mpi4py import MPI
 from ngsolve import *
-from netgen.geom2d import unit_square
+from mpi4py import MPI
+from netgen.occ import unit_square
 
 comm = MPI.COMM_WORLD
 
-if comm.rank == 0:
-    ngmesh = unit_square.GenerateMesh(maxh=0.1).Distribute(comm)
-else:
-    ngmesh = netgen.meshing.Mesh.Receive(comm)
-
+ngmesh = unit_square.GenerateMesh(maxh=0.1, comm=comm)
+    
 for l in range(3):
     ngmesh.Refine()
 mesh = Mesh(ngmesh)
@@ -17,19 +14,17 @@ fes = H1(mesh, order=3, dirichlet=".*")
 u,v = fes.TnT()
 
 a = BilinearForm(grad(u)*grad(v)*dx)
-pre = Preconditioner(a, "local")
+pre = preconditioners.Local(a)
 a.Assemble()
 
 f = LinearForm(1*v*dx).Assemble()
 gfu = GridFunction(fes)
 
-
 inv = CGSolver(a.mat, pre.mat)
 gfu.vec.data = inv*f.vec
 
 ip = InnerProduct(gfu.vec, f.vec)
-if comm.rank == 0:
-    print ("(u,f) =", ip)
+printonce("(u,f) =", ip)
 # (u,f) = 0.03514425357822445
 
     
