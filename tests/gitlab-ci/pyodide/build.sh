7001ac9fab39c0fa0b65c4f12017edb2d373ab1f
Matthias Hochsteger <mhochsteger@cerbsim.com>
2024-12-03 20:44:17 +0100

    Upload pyodide build from other branches, append branch name to result file

--- tests/gitlab-ci/pyodide/build.sh
+++ tests/gitlab-ci/pyodide/build.sh
@@ -7,8 +7,9 @@ GROUPID=`id -g`
 # docker run -u `id -u` -v `pwd`/dist:/dist ngsolve_pyodide bash -c 'cd /root/output && cp ngsolve*.tar.bz2 /dist/'
 docker run -v `pwd`/dist:/dist ngsolve_pyodide bash -c "cd /root/output && cp ngsolve*.tar.bz2 /dist/ && chown -R ${USERID}:${GROUPID} /dist/"
 
-# push only if $CI_COMMIT_REF_NAME is "master"
+# append branch name to file name if it's other than master
+cd dist
 if [ "$CI_COMMIT_REF_NAME" != "master" ]; then
-  exit 0
+  rename "s/ngsolve_pyodide_/ngsolve_pyodide_${CI_COMMIT_REF_NAME}_/" *.tar.bz2
 fi
-scp -P 692 dist/*.tar.bz2 deploy@ngsolve.org:/opt/data/files/
+scp -P 692 *.tar.bz2 deploy@ngsolve.org:/opt/data/files/
