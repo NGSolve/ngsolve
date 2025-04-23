693acc5d27ae32386a0878b7c69bddbd1edc016d
Matthias Hochsteger <mhochsteger@cerbsim.com>
2025-04-23 10:49:19 +0200

    Don't build mac bundle anymore

--- tests/gitlab-ci/mac/upload.sh
+++
@@ -1,4 +0,0 @@
-rsync -ztrl --del -e ssh \
-      --rsync-path="mkdir -p deploy/builds/$CI_PIPELINE_ID/macos && rsync" \
-      *.dmg \
-      gitlab-runner@vector.asc.tuwien.ac.at:deploy/builds/$CI_PIPELINE_ID/macos/
