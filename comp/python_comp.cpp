c469e0aa1495ff9c2fef030b08eb58d190ebb194
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2025-06-04 19:29:14 +0200

    replace scaling by rtyp

--- comp/python_comp.cpp
+++ comp/python_comp.cpp
@@ -46,7 +46,7 @@
 #include "postproc.hpp"
 #include "interpolate.hpp"
 #include "fesconvert.hpp"
-#include "potentialtools.hpp"
+// #include "potentialtools.hpp"
 #include "vtkoutput.hpp"
 #include "webgui.hpp"
 
