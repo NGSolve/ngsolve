d509c63bee5262fe83203d12eee15665dd8946be
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2025-07-20 17:51:33 +0200

    specify all tempalte args

--- basiclinalg/ngblasc.cpp
+++ basiclinalg/ngblasc.cpp
@@ -785,22 +785,22 @@ namespace ngbla
 
 
   template <>
-  void NgGEMMBare<true,false>(size_t ah, size_t aw, size_t bw,
+  void NgGEMMBare<true,false,RowMajor,RowMajor>(size_t ah, size_t aw, size_t bw,
                               BareSliceMatrix<Complex,RowMajor> a, BareSliceMatrix<Complex,RowMajor> b, BareSliceMatrix<Complex,RowMajor> c)
   { NgGEMMFunc<true,false> (ah, aw, bw, a, b, c); }
   
   template <>
-  void NgGEMMBare<true,false>(size_t ah, size_t aw, size_t bw,
+  void NgGEMMBare<true,false,ColMajor,RowMajor>(size_t ah, size_t aw, size_t bw,
                               BareSliceMatrix<Complex,ColMajor> a, BareSliceMatrix<Complex,RowMajor> b, BareSliceMatrix<Complex,RowMajor> c)
   { NgGEMMFunc<true,false> (ah, aw, bw, a, b, c); }
   
   template <>
-  void NgGEMMBare<true,false>(size_t ah, size_t aw, size_t bw,
+  void NgGEMMBare<true,false,RowMajor,ColMajor>(size_t ah, size_t aw, size_t bw,
                               BareSliceMatrix<Complex,RowMajor> a, BareSliceMatrix<Complex,ColMajor> b, BareSliceMatrix<Complex,RowMajor> c)
   { NgGEMMFunc<true,false> (ah, aw, bw, a, b, c); }
 
   template <>
-  void NgGEMMBare<true,false>(size_t ah, size_t aw, size_t bw,
+  void NgGEMMBare<true,false,ColMajor,ColMajor>(size_t ah, size_t aw, size_t bw,
                               BareSliceMatrix<Complex,ColMajor> a, BareSliceMatrix<Complex,ColMajor> b, BareSliceMatrix<Complex,RowMajor> c)
   { NgGEMMFunc<true,false> (ah, aw, bw, a, b, c); }
 
