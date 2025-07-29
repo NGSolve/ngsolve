8e69a41c8ef8ea0b6545a03ab1fbac46df7defb5
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2025-07-29 16:18:49 +0200

    remove timers

--- bem/mptools.cpp
+++ bem/mptools.cpp
@@ -206,9 +206,9 @@ namespace ngsbem
   {
     LocalHeap lh(8*6*sqr(order) + 8*15*order + 2*sizeof(entry_type)*(order+3) + 500);
       
-    static Timer t("mptool sh RotateY"+ToString(sizeof(entry_type)/sizeof(Complex)));
-    // RegionTimer rg(t);
-    RegionTracer rt(TaskManager::GetThreadId(), t, order);
+    // static Timer t("mptool sh RotateY"+ToString(sizeof(entry_type)/sizeof(Complex)));
+    // RegionTimer rg(t, order);
+    
     
     /*
       static std::map<int, unique_ptr<Timer<>>> timers;
@@ -1214,8 +1214,8 @@ namespace ngsbem
   template <typename RADIAL, typename entry_type> template <typename TARGET>
   void MultiPole<RADIAL,entry_type> :: ShiftZ (double z, MultiPole<TARGET,entry_type> & target)
   {
-    static Timer t("mptool ShiftZ"+ToString(typeid(RADIAL).name())+ToString(typeid(TARGET).name()));
-    RegionTimer rg(t);
+    // static Timer t("mptool ShiftZ"+ToString(typeid(RADIAL).name())+ToString(typeid(TARGET).name()));
+    // RegionTimer rg(t);
       
     int os = sh.Order();
     int ot = target.SH().Order();
