7d149b2e16a35251fe333799fd61b624102a6f3f
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2025-10-10 20:15:25 +0200

    adjust for ng-statushandler in ngcore

--- ngstd/statushandler.cpp
+++ ngstd/statushandler.cpp
@@ -1,47 +1,53 @@
-
-#include <nginterface.h>
 #include <ngstd.hpp>
+#include <core/statushandler.hpp>
 
 namespace ngstd
 {
   static mutex m;
-  void BaseStatusHandler::PushStatus (const char * str)
+  void BaseStatusHandler::PushStatus (const std::string & str)
   {
     lock_guard lock(m);
-    Ng_PushStatus(str);
+    // Ng_PushStatus(str);
+    ngcore::PushStatus(str);
   }
 
   void BaseStatusHandler::PopStatus ()
   {
     lock_guard lock(m);
-    Ng_PopStatus();
+    // Ng_PopStatus();
+    ngcore::PopStatus();    
   }
 
   void BaseStatusHandler::SetThreadPercentage (double percent)
   {
-    Ng_SetThreadPercentage(percent);
+    // Ng_SetThreadPercentage(percent);
+    ngcore::SetThreadPercent(percent);
   }
 
   void BaseStatusHandler::GetStatus (string & str, double & percent)
   {
-    char * s;
-    Ng_GetStatus(&s, percent);
-    str = s;
+    // Ng_GetStatus(str, percent);
+    ngcore::GetStatus(str, percent);
   }
 
   void BaseStatusHandler::SetTerminate(void)
   {
-    Ng_SetTerminate();
+    // Ng_SetTerminate();
+    // ngcore::SetTerminate();
+    ngcore::multithread.terminate = 1;    
   }
 
   void BaseStatusHandler::UnSetTerminate(void)
   {
-    Ng_UnSetTerminate();
+    // Ng_UnSetTerminate();
+    // ngcore::UnSetTerminate();
+    ngcore::multithread.terminate = 1;    
   }
 
   bool BaseStatusHandler::ShouldTerminate(void)
   {
-    return (Ng_ShouldTerminate() != 0);
+    // return (Ng_ShouldTerminate() != 0);
+    return ngcore::multithread.terminate != 0;
   }
 
 } // namespace ngstd
