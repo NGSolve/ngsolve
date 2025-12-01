c875929826a8496d312448e58c9ec808a82d524a
erdieee <58039191+erdieee@users.noreply.github.com>
2025-12-01 11:22:31 +0100

    bem tests compute RR translation, perform test for different orders

--- tests/pytest/test_bem.py
+++ tests/pytest/test_bem.py
@@ -5,27 +5,37 @@ from netgen.occ import *
 from ngsolve import *
 from ngsolve.bem import *
 
-def test_transform():
+
+@pytest.mark.parametrize(
+    "order_source_1, order_source_2, order_target_1, order_target_2",
+    [
+        (80, 80, 80, 80),
+        (80, 82, 79, 83),
+    ],
+)
+def test_transform(order_source_1, order_source_2, order_target_1, order_target_2):
     box = Box((-10,-10,-10), (10,10,10))
     mesh = Mesh(OCCGeometry(box).GenerateMesh(maxh=5))
 
     kappa = 20
-    order = 80
-    S = SingularExpansionCF(order, kappa, (0,0,0), rad=1)
+    S = SingularExpansionCF(order_source_1, kappa, (0,0,0), rad=1)
     S.AddCharge((0.3, -0.1,0.5), 1)
 
     dx,dy,dz = 0.1, 0.2, 0.3
-    S2 = SingularExpansionCF(order, kappa, (dx,dy,dz), rad=2)
+    S2 = SingularExpansionCF(order_source_2, kappa, (dx,dy,dz), rad=2)
     S.Transform(S2)
 
     meshpnt = mesh(1,1,1)
     assert S(meshpnt) == pytest.approx(S2(meshpnt), rel=1e-10)
 
-    R = RegularExpansionCF(order, kappa, (3,3,3), rad=2)
+    R = RegularExpansionCF(order_target_1, kappa, (3,3,3), rad=2)
     S2.Transform(R)
     meshpnt = mesh(2,2,2)
     assert S(meshpnt) == pytest.approx(R(meshpnt), rel=1e-10)
     
+    R2 = RegularExpansionCF(order_target_2, kappa, (3+dx,3+dy,3+dz), rad=2)
+    R.Transform(R2)
+    assert S(meshpnt) == pytest.approx(R2(meshpnt), rel=1e-10)
 
     
 
@@ -47,4 +57,3 @@ def test_singularmltransform():
     val2 = S(mesh(1,1,4))
 
     assert val1 == pytest.approx(val2)
-
