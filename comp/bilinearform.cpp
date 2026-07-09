68ea0300b46c9a7f66915865cdfe34b2e3c18968
Christopher Lackner <clackner@cerbsim.com>
2026-07-09 12:36:27 +0200

    fix matrix shape, matrixfreebdb call reassemble

--- comp/bilinearform.cpp
+++ comp/bilinearform.cpp
@@ -1539,7 +1539,7 @@ namespace ngcomp
                     const_cast<ElementTransformation&>(trafo).userdata = &ud;
                     
                     FlatTensor<3> proxyvalues(lh, ir.Size(), dimx, dimy);
-                    FlatMatrix<> val(1, ir.Size(), lh);
+                    FlatMatrix<> val(ir.Size(), 1, lh);
 
                     int k1 = 0;
                     for (auto proxy1 : trialproxies)
@@ -1753,13 +1753,13 @@ namespace ngcomp
 
   void BilinearForm :: ReAssemble (LocalHeap & lh, bool reallocate)
   {
-    if (nonlinear_matrix_free_bdb)
+    if (nonlinear_matrix_free_bdb || matrix_free_bdb)
       {
-        AssembleBDB (lh, false);
+        AssembleBDB (lh, !nonlinear_matrix_free_bdb);
         return;
       }
 
-    if (nonassemble || matrix_free_bdb)
+    if (nonassemble)
       {
         Assemble(lh);
         return;
