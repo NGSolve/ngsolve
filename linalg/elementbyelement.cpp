9b0ac5867a82e1f11b742d6a0988a28714a4b94c
Christopher Lackner <clackner@cerbsim.com>
2026-09-25 18:09:56 +0200

    after ngsolve NgGEMM fix, we can reduce require again

--- linalg/elementbyelement.cpp
+++ linalg/elementbyelement.cpp
@@ -984,8 +984,7 @@ namespace ngla
     std::visit([&](auto vx, auto vy) {
     typedef decltype(vx) TX;
     typedef decltype(vy) TY;
-    if constexpr (requires (TY & ey, TSCAL64 es, SCAL em, TX ex) { ey += es * (em * ex); } &&
-                  (is_same_v<TY,Complex> || (is_same_v<TX,SCAL> && is_same_v<TY,SCAL>)))
+    if constexpr (requires (TY & ey, TSCAL64 es, SCAL em, TX ex) { ey += es * (em * ex); })
     {
     auto fx = x.FV<TX>();
     auto fy = y.FV<TY>();
